30fe875a277e0529a4e8dfabfba4fcf3a8e4d2ab
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-06-12 22:19:54 +0000

    initial contribution

---
+++ tags/0.1.0/org.openhealthtools.mdht.uml.cda.ccd/src/org/openhealthtools/mdht/uml/cda/ccd/CCDFactory.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2009 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ccd;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see org.openhealthtools.mdht.uml.cda.ccd.CCDPackage
+ * @generated
+ */
+public interface CCDFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	CCDFactory eINSTANCE = org.openhealthtools.mdht.uml.cda.ccd.impl.CCDFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Problem Act</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Problem Act</em>'.
+	 * @generated
+	 */
+	ProblemAct createProblemAct();
+
+	/**
+	 * Returns a new object of class '<em>Problem Observation</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Problem Observation</em>'.
+	 * @generated
+	 */
+	ProblemObservation createProblemObservation();
+
+	/**
+	 * Returns a new object of class '<em>A</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>A</em>'.
+	 * @generated
+	 */
+	A createA();
+
+	/**
+	 * Returns a new object of class '<em>B</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>B</em>'.
+	 * @generated
+	 */
+	B createB();
+
+	/**
+	 * Returns a new object of class '<em>C</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>C</em>'.
+	 * @generated
+	 */
+	C createC();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	CCDPackage getCCDPackage();
+
+} //CCDFactory
---
+++ tags/0.1.0/org.openhealthtools.mdht.uml.cda.ccd/src/org/openhealthtools/mdht/uml/cda/ccd/tests/Main.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2009 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ccd.tests;
+
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.openhealthtools.mdht.uml.cda.ccd.C;
+import org.openhealthtools.mdht.uml.cda.ccd.CCDFactory;
+import org.openhealthtools.mdht.uml.cda.ccd.ProblemObservation;
+import org.openhealthtools.mdht.uml.cda.CDAFactory;
+import org.openhealthtools.mdht.uml.cda.CDAPackage;
+import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
+import org.openhealthtools.mdht.uml.cda.Entry;
+import org.openhealthtools.mdht.uml.cda.Section;
+import org.openhealthtools.mdht.uml.cda.resource.CDAResource;
+import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
+import org.w3c.dom.Document;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		ProblemObservation problemObservation = CCDFactory.eINSTANCE.createProblemObservation().init();
+		
+		Entry entry = CDAFactory.eINSTANCE.createEntry();
+		entry.setObservation(problemObservation);
+		
+		C c = CCDFactory.eINSTANCE.createC().init();
+		entry.setAct(c);
+		
+		Section sect = CDAFactory.eINSTANCE.createSection();
+		sect.getEntry().add(entry);
+		
+		ClinicalDocument doc = CDAFactory.eINSTANCE.createClinicalDocument();
+		doc.addSection(sect);
+		
+		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(doc);
+		System.out.println(diagnostic);
+		if (diagnostic.getSeverity() == Diagnostic.OK) {
+			System.out.println("Document is valid CCD!");
+		} else {
+			System.out.println("Document is invalid CCD!");
+		}
+
+		Resource.Factory factory = CDAResource.Factory.INSTANCE;
+		XMLResource resource = (XMLResource) factory.createResource(URI.createURI(CDAPackage.eNS_URI));
+		resource.getContents().add(doc);
+
+		Document document = resource.save(null, null, null);
+		CDAUtil.printDocument(document, System.out);
+
+		resource = (XMLResource) factory.createResource(URI.createURI(CDAPackage.eNS_URI));
+		resource.load(document, null);
+		CDAUtil.printDocument(document, System.out);
+		
+		ClinicalDocument clinicalDocument = (ClinicalDocument) resource.getContents().get(0);
+		System.out.println(clinicalDocument.getComponent().getStructuredBody().getComponent().get(0).getSection().getEntry().get(0).getObservation());
+	}
+}
