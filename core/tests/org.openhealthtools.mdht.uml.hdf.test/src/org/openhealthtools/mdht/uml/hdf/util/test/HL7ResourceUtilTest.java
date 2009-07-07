6d48310c8907364c526d6b7dbd9bf5dc2c6afa67
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-07-07 19:51:32 +0000

    Initial version of the HDF Test Fragment

---
+++ core/tests/org.openhealthtools.mdht.uml.hdf.test/src/org/openhealthtools/mdht/uml/hdf/util/test/HL7ResourceUtilTest.java
@@ -0,0 +1,55 @@
+package org.openhealthtools.mdht.uml.hdf.util.test;
+
+import junit.framework.TestCase;
+
+public class HL7ResourceUtilTest extends TestCase {
+
+	public void testGetBooleanPreference() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetStringPreference() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetLongPreference() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAppliedHDFStereotype() {
+		fail("Not yet implemented");
+	}
+
+	public void testApplyHDFStereotype() {
+		fail("Not yet implemented");
+	}
+
+	public void testUnapplyHDFStereotype() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAppliedHDFProfile() {
+		fail("Not yet implemented");
+	}
+
+	public void testApplyHDFProfile() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAppliedRIMStereotype() {
+		fail("Not yet implemented");
+	}
+
+	public void testApplyRIMStereotype() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAppliedRIMProfile() {
+		fail("Not yet implemented");
+	}
+
+	public void testApplyRIMProfile() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ core/tests/org.openhealthtools.mdht.uml.hdf.test/src/org/openhealthtools/mdht/uml/hdf/util/test/MultiplicityElementUtilTest.java
@@ -0,0 +1,11 @@
+package org.openhealthtools.mdht.uml.hdf.util.test;
+
+import junit.framework.TestCase;
+
+public class MultiplicityElementUtilTest extends TestCase {
+
+	public void testGetMultiplicityAsString() {
+		fail("Not yet implemented");
+	}
+
+}
