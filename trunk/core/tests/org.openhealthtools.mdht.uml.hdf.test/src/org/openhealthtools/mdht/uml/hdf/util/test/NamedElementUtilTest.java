b335e8179a9dc95f8015694e1e761e9aad932ad8
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-07-07 19:51:32 +0000

    Initial version of the HDF Test Fragment

---
+++ trunk/core/tests/org.openhealthtools.mdht.uml.hdf.test/src/org/openhealthtools/mdht/uml/hdf/util/test/NamedElementUtilTest.java
@@ -0,0 +1,15 @@
+package org.openhealthtools.mdht.uml.hdf.util.test;
+
+import junit.framework.TestCase;
+
+public class NamedElementUtilTest extends TestCase {
+
+	public void testGetQualifiedNameMaxDepth() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetVisibilityAsSign() {
+		fail("Not yet implemented");
+	}
+
+}
