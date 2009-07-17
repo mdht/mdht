77c835fcb5319a99b72d3ac44966a8c70146e5a0
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-07-17 18:25:21 +0000

    Initial import.

---
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/EcoreTransformer.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.transform;
+
+import java.util.Collections;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.util.UMLSwitch;
+
+public class EcoreTransformer {
+	
+	private EcoreTransformerOptions transformerOptions;
+
+	public EcoreTransformer() {
+		transformerOptions = new EcoreTransformerOptions();
+	}
+
+	public EcoreTransformer(EcoreTransformerOptions options) {
+		transformerOptions = options;
+	}
+	
+	public void transformElement(Element element) {
+
+		UMLSwitch transformTemplateIdentifier = 
+			new TransformTemplateIdentifier(transformerOptions);
+		UMLSwitch transformVocabConstraint = 
+			new TransformVocabConstraint(transformerOptions);
+
+		try {
+			TreeIterator iterator = EcoreUtil.getAllContents(
+					Collections.singletonList(element));
+			while (iterator != null && iterator.hasNext()) {
+				EObject child = (EObject) iterator.next();
+
+				transformTemplateIdentifier.doSwitch(child);
+				transformVocabConstraint.doSwitch(child);
+			}
+		}
+		catch (IndexOutOfBoundsException e) {
+			//TODO use logger
+			e.printStackTrace();
+		}
+
+	}
+	
+}
---
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/EcoreTransformerOptions.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.transform;
+
+import org.eclipse.core.runtime.Preferences;
+import org.openhealthtools.mdht.uml.cda.transform.internal.Activator;
+
+/**
+ *
+ */
+public class EcoreTransformerOptions {
+
+	public static final String INCLUDE_VOCABULARY_CONSTRAINTS = "includeVocabularyConstraints";
+	
+	private boolean includeVocabularyConstraints;
+	
+	public EcoreTransformerOptions() {
+		initializePreferences();
+	}
+	
+	private void initializePreferences() {
+		Preferences store = Activator.getDefault().getPluginPreferences();
+
+		includeVocabularyConstraints = 
+			store.getBoolean(INCLUDE_VOCABULARY_CONSTRAINTS);
+	}
+
+	public boolean isIncludeVocabularyConstraints() {
+		return includeVocabularyConstraints;
+	}
+
+	public void setIncludeVocabularyConstraints(boolean includeVocabularyConstraints) {
+		this.includeVocabularyConstraints = includeVocabularyConstraints;
+	}
+
+}
