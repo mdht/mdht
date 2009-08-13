fb5bfa16ac5e76897f8a88d0089a8e47e2eef69e
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-08-13 14:15:58 +0000

    ICDAProfileConstants was created to be consistent with IHDFProfileConstants.

---
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.resources/src/org/openhealthtools/mdht/uml/cda/resources/util/ICDAProfileConstants.java
@@ -0,0 +1,13 @@
+package org.openhealthtools.mdht.uml.cda.resources.util;
+
+public interface ICDAProfileConstants {
+
+	public static final String CDA_PROFILE_NAME = "CDA";
+
+	/*
+	 * Validation Support Stereotype and Properties
+	 */
+	public static final String VALIDATION_SUPPORT = "ValidationSupport";
+	public static final String VALIDATION_SUPPORT_MESSAGE = "message";
+	public static final String VALIDATION_SUPPORT_SEVERITY = "severity";
+}
