5de61fff08f4903e54d5cec59a2f4cd834b13192
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-08-14 23:39:49 +0000

    Modified TransformAssociation to use the typeCode property from the HasClinicalStatementTemplate stereotype during OCl constraint generation.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/TransformAssociation.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/TransformAssociation.java
@@ -13,6 +13,7 @@
 package org.openhealthtools.mdht.uml.cda.transform;
 
 import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Constraint;
 import org.eclipse.uml2.uml.EnumerationLiteral;
 import org.eclipse.uml2.uml.OpaqueExpression;
@@ -32,15 +33,15 @@ public class TransformAssociation extends TransformAbstract {
 			return null;
 		}
 		
-		org.eclipse.uml2.uml.Class sourceClass = null;
-		org.eclipse.uml2.uml.Class targetClass = null;
+		Class sourceClass = null;
+		Class targetClass = null;
 		
 		Property targetProperty = null;
 		
 		for (Property property : association.getMemberEnds()) {
 			if (property.isNavigable()) {
 				sourceClass = property.getClass_();
-				targetClass = (org.eclipse.uml2.uml.Class) property.getType();
+				targetClass = (Class) property.getType();
 				targetProperty = property;
 				break;
 			}
@@ -50,14 +51,17 @@ public class TransformAssociation extends TransformAbstract {
 			return null;
 		}
 
-		org.eclipse.uml2.uml.Class cdaTargetClass = getCDAClass(targetClass);
+		Class cdaSourceClass = getCDAClass(sourceClass);
+		Class cdaTargetClass = getCDAClass(targetClass);
 		
-		if (cdaTargetClass == null) {
+		if (cdaSourceClass == null || cdaTargetClass == null) {
 			return null;
 		}
 		
 		// TODO: Add support to validate target and source classes to allow only valid relationships:
 		//       Document -> Section, Section -> Section, Section -> { Act, Encounter, ... }, { Act, Encounter, ... } -> { Act, Encounter, ... }
+
+		String cdaSourceName = cdaSourceClass.getName();
 		
 		String cdaTargetName = cdaTargetClass.getName();
 		String cdaTargetLowerName = cdaTargetName.substring(0, 1).toLowerCase() + cdaTargetName.substring(1);
@@ -75,6 +79,22 @@ public class TransformAssociation extends TransformAbstract {
 		body.append(cdaTargetLowerName);
 		body.append(".oclIsTypeOf(" + targetQName + "))");
 		
+		Stereotype hasClinicalStatementTemplate = EcoreTransformUtil.getAppliedCDAStereotype(association, ICDAProfileConstants.HAS_CLINICAL_STATEMENT_TEMPLATE);
+		if (hasClinicalStatementTemplate != null) {
+			EnumerationLiteral literal = (EnumerationLiteral) association.getValue(hasClinicalStatementTemplate, ICDAProfileConstants.HAS_CLINICAL_STATEMENT_TEMPLATE_TYPE_CODE);
+			
+			// TODO: Validate that the EnumerationLiteral comes from an Enumeration that is compatible with the Type of typeCode
+			
+			if (literal != null) {
+				if ("Section".equals(cdaSourceName)) {
+					body.append(" and self.entry.typeCode = ");
+				} else {
+					body.append(" and self.entryRelationship.typeCode = ");
+				}
+				body.append(literal.getQualifiedName());
+			}
+		}
+		
 		String constraintName = sourceClass.getName() + "_" + targetLowerName;
 		Constraint constraint = sourceClass.createOwnedRule(constraintName, UMLPackage.eINSTANCE.getConstraint());
 		constraint.getConstrainedElements().add(sourceClass);
@@ -82,7 +102,7 @@ public class TransformAssociation extends TransformAbstract {
 		OpaqueExpression expression = (OpaqueExpression) constraint.createSpecification(null, null, UMLPackage.eINSTANCE.getOpaqueExpression());
 		expression.getLanguages().add("OCL");
 		expression.getBodies().add(body.toString());
-		
+
 		Stereotype validationSupport = EcoreTransformUtil.getAppliedCDAStereotype(association, ICDAProfileConstants.VALIDATION_SUPPORT);
 		if (validationSupport != null) {
 			String message = (String) association.getValue(validationSupport, ICDAProfileConstants.VALIDATION_SUPPORT_MESSAGE);
