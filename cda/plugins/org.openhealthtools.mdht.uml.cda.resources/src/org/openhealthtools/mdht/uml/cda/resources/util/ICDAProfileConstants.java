b4ccfd95955495b844cc52e81999f7babd1c15ad
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-08-14 23:37:19 +0000

    Added HasClinicalStatementTemplate stereotype with typeCode property.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.resources/src/org/openhealthtools/mdht/uml/cda/resources/util/ICDAProfileConstants.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.resources/src/org/openhealthtools/mdht/uml/cda/resources/util/ICDAProfileConstants.java
@@ -10,4 +10,10 @@ public interface ICDAProfileConstants {
 	public static final String VALIDATION_SUPPORT = "ValidationSupport";
 	public static final String VALIDATION_SUPPORT_MESSAGE = "message";
 	public static final String VALIDATION_SUPPORT_SEVERITY = "severity";
+	
+	/*
+	 * Has Clinical Statement Template Stereotype and Properties
+	 */
+	public static final String HAS_CLINICAL_STATEMENT_TEMPLATE = "HasClinicalStatementTemplate";
+	public static final String HAS_CLINICAL_STATEMENT_TEMPLATE_TYPE_CODE = "typeCode";
 }
