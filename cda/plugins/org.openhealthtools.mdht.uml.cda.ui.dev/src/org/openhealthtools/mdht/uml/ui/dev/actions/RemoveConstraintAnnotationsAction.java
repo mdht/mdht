969c146f856f11b30612f47f671d7be8abbbd483
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-09-14 23:06:27 +0000

    Initial import.

---
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.ui.dev/src/org/openhealthtools/mdht/uml/ui/dev/actions/RemoveConstraintAnnotationsAction.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Copyright (c) 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.ui.dev.actions;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.IUndoableOperation;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.eclipse.emf.workspace.AbstractEMFOperation;
+import org.eclipse.emf.workspace.IWorkspaceCommandStack;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.util.UMLUtil;
+import org.openhealthtools.mdht.uml.cda.transform.AnnotationsUtil;
+import org.openhealthtools.mdht.uml.cda.transform.EcoreTransformUtil;
+
+public class RemoveConstraintAnnotationsAction implements IObjectActionDelegate {
+	public static final String VALIDATION_ERROR = "constraints.validation.error";
+	public static final String VALIDATION_WARNING = "constraints.validation.warning";
+	public static final String VALIDATION_INFO = "constraints.validation.info";
+
+	private NamedElement namedElement;
+	
+	public RemoveConstraintAnnotationsAction() {
+		super();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		try {
+			TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(namedElement);
+			IUndoableOperation operation = new AbstractEMFOperation(
+					editingDomain, "Remove Constraint Annotations") {
+			    protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) {
+
+					TreeIterator iterator = EcoreUtil.getAllContents(
+							Collections.singletonList(namedElement));
+					
+					while (iterator != null && iterator.hasNext()) {
+						Object child = iterator.next();
+						if (child instanceof Class) {
+							Class childElement = (Class) child;
+							AnnotationsUtil annotationsUtil = new AnnotationsUtil(childElement);
+							annotationsUtil.removeAnnotation(VALIDATION_ERROR);
+							annotationsUtil.removeAnnotation(VALIDATION_WARNING);
+							annotationsUtil.removeAnnotation(VALIDATION_INFO);
+							annotationsUtil.saveAnnotations();
+
+							Stereotype eClass = EcoreTransformUtil.getEcoreStereotype(childElement, UMLUtil.STEREOTYPE__E_CLASS);
+							if (eClass != null) {
+								List<String> annotations = (List<String>) childElement.getValue(eClass, "annotations");
+								if (annotations.isEmpty()) {
+									childElement.unapplyStereotype(eClass);
+								}
+							}
+						}
+					}
+
+			        return Status.OK_STATUS;
+			    }};
+
+		    try {
+				IWorkspaceCommandStack commandStack = (IWorkspaceCommandStack) editingDomain.getCommandStack();
+				operation.addContext(commandStack.getDefaultUndoContext());
+		        commandStack.getOperationHistory().execute(operation, new NullProgressMonitor(), null);
+
+		    } catch (ExecutionException ee) {
+		    	ee.printStackTrace();
+		    }
+
+		} catch (Exception e) {
+			throw new RuntimeException(e.getCause());
+		}
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		namedElement = null;
+		
+		if (((IStructuredSelection)selection).size() == 1) {
+			Object selected = ((IStructuredSelection)selection).getFirstElement();
+			if (selected instanceof IAdaptable) {
+				selected = (EObject) ((IAdaptable) selected).getAdapter(EObject.class);
+			}
+			if (selected instanceof NamedElement) {
+				namedElement = (NamedElement) selected;
+			}
+		}
+		
+		action.setEnabled(namedElement != null);
+	}
+
+}
