64b8ba1ae38dfdee63a1e81bc9e7a9b225184ca0
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-09-28 19:40:05 +0000

    Create new filters for Entry and EntryRelationship.

---
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/filters/EntryRelationshipFilter.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.ui.filters;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Type;
+
+/**
+ * Selects an object if it is an association applicable to <<EntryRelationship>> stereotype.
+ */
+public class EntryRelationshipFilter extends CDAFilter {
+	
+	public boolean select(Object object) {
+		Element element = null;
+		if (object instanceof Element)
+			element = (Element) object;
+		else if (object instanceof IAdaptable)
+			element = (Element) ((IAdaptable)object).getAdapter(Element.class);
+		
+		if (element instanceof Association && isEntryRelationship((Association) element)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isEntryRelationship(Association association) {
+		Type srcType = null;
+		Type targetType = null;
+		for (Property property : association.getMemberEnds()) {
+			if (property.isNavigable())
+				targetType = property.getType();
+			else
+				srcType = property.getType();
+		}
+		
+		return isClinicalStatement(srcType) && isClinicalStatement(targetType);
+	}
+
+}
