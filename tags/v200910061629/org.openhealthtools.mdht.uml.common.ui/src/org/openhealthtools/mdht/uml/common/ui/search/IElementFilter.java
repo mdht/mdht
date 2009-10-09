669b0c19e31873487ec24be55298e02492565233
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-10-09 13:02:53 +0000

    CDA M1 Release on October 6, 2009

---
+++ tags/v200910061629/org.openhealthtools.mdht.uml.common.ui/src/org/openhealthtools/mdht/uml/common/ui/search/IElementFilter.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.common.ui.search;
+
+import org.eclipse.uml2.uml.Element;
+
+/**
+ * Used by ModelSearch to filter search results.
+ */
+public interface IElementFilter {
+
+	/**
+	 * Returns true if this element is acceptable to the filter.
+	 * 
+	 * @param element
+	 * @return true if accepted
+	 */
+	public boolean accept(Element element);
+	
+}
