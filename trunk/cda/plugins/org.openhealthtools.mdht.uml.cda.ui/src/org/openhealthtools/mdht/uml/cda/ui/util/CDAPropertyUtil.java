825a7c46b927deaeb99484ceb1a346a8c359d9d4
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2009-10-30 21:41:26 +0000

    Refactored to move shared classes into uml.common

--- trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/util/CDAPropertyUtil.java
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/util/CDAPropertyUtil.java
@@ -1,17 +1,15 @@
-/*****************************************************************************
- * Copyright (c) 2009 CEA LIST and others.
- *
- *    
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 David A Carlson.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *  Yann TANGUY (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
- *  David Carlson (XMLmodeling.com) - modified from Eclipse Papyrus source
- *  
- *****************************************************************************/
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.ui.util;
 
 import org.eclipse.uml2.uml.Property;
--- trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.ui/src/org/openhealthtools/mdht/uml/hdf/ui/util/HDFPropertyUtil.java
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.ui/src/org/openhealthtools/mdht/uml/hdf/ui/util/HDFPropertyUtil.java
@@ -1,157 +1,34 @@
-/*****************************************************************************
- * Copyright (c) 2009 CEA LIST.
- *
- *    
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 David A Carlson.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *  Yann TANGUY (CEA LIST) yann.tanguy@cea.fr - Initial API and implementation
- *  David Carlson (XMLmodeling.com) - modified from Eclipse Papyrus source
- *  
- *****************************************************************************/
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.hdf.ui.util;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EEnumLiteral;
 import org.eclipse.uml2.uml.Enumeration;
 import org.eclipse.uml2.uml.EnumerationLiteral;
-import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.Stereotype;
 import org.openhealthtools.mdht.uml.common.util.MultiplicityElementUtil;
 import org.openhealthtools.mdht.uml.common.util.NamedElementUtil;
+import org.openhealthtools.mdht.uml.common.util.PropertyNotationUtil;
 import org.openhealthtools.mdht.uml.hdf.util.HL7ResourceUtil;
 import org.openhealthtools.mdht.uml.hdf.util.IHDFProfileConstants;
 
 /**
  * Utility class for <code>org.eclipse.uml2.uml.Property</code><BR>
  */
-public class PropertyUtil {
-
-	public final static String UNDEFINED_TYPE_NAME = "<Undefined>";
-
-	/**
-	 * Get all properties that can be subset by this {@link Property} checks the
-	 * type and the multiplicity.
-	 * 
-	 * @return all properties that can be subset
-	 */
-	public static List<Property> getSubsettablesProperties(Property property) {
-		List<Property> list = new ArrayList<Property>();
-
-		// subset properties:
-		Iterator<NamedElement> it = property.getClass_().getMembers().iterator();
-		while (it.hasNext()) {
-
-			NamedElement element = it.next();
-			if (element instanceof Property) {
-				boolean isValid = true;
-				Property subsettableProperty = (Property) element;
-
-				// check it is not itself....
-				if (subsettableProperty.equals(property)) {
-					isValid = false;
-				}
-
-				// check types conformity
-				if (property.getType() != null && subsettableProperty.getType() != null) {
-					if (!property.getType().conformsTo(subsettableProperty.getType())) {
-						isValid = false;
-					}
-				} else {
-					isValid = false;
-				}
-
-				// check multiplicity (only upper bound has an OCL rule)
-				if ((subsettableProperty.getUpper() != -1) && (property.getUpper() > subsettableProperty.getUpper())) {
-					isValid = false;
-				}
-
-				if (isValid) {
-					list.add(subsettableProperty);
-				}
-			}
-		}
-		return list;
-	}
-
-	/**
-	 * Get all properties that can be redefined by this {@link Property}.
-	 * 
-	 * @return all properties that can be redefined
-	 */
-	public static List<Property> getRedefinableProperties(Property property) {
-		List<Property> list = new ArrayList<Property>();
-
-		// redefine-able properties:
-		Iterator<NamedElement> it = property.getClass_().getMembers().iterator();
-		while (it.hasNext()) {
-			NamedElement element = it.next();
-			if (element instanceof Property) {
-				Property redefinableProperty = (Property) element;
-				if (!redefinableProperty.equals(property)) {
-					list.add(redefinableProperty);
-				}
-			}
-		}
-
-		return list;
-	}
-
-	/**
-	 * return the full label of the property, given UML2 specification.
-	 * 
-	 * @return the string corresponding to the label of the property
-	 */
-	public static String getLabel(Property property) {
-		StringBuffer buffer = new StringBuffer();
-		// visibility
-		buffer.append(" ");
-		buffer.append(NamedElementUtil.getVisibilityAsSign(property));
-
-		// derived property
-		if (property.isDerived()) {
-			buffer.append("/");
-		}
-
-		// name
-		buffer.append(" ");
-		buffer.append(property.getName());
-
-		// type
-		if (property.getType() != null) {
-			buffer.append(": " + property.getType().getName());
-		} else {
-			buffer.append(": " + UNDEFINED_TYPE_NAME);
-		}
-
-		// multiplicity -> do not display [1]
-		String multiplicity = MultiplicityElementUtil.getMultiplicityAsString(property);
-		if (!multiplicity.trim().equals("[1]")) {
-			buffer.append(multiplicity);
-		}
-
-		// default value
-		if (property.getDefault() != null) {
-			buffer.append(" = ");
-			buffer.append(property.getDefault());
-		}
-
-		// property modifiers
-		String modifiers = PropertyUtil.getModifiersAsString(property, false);
-		if (!modifiers.equals("")) {
-			buffer.append(" {");
-			buffer.append(modifiers);
-			buffer.append("}");
-		}
-		return buffer.toString();
-	}
+public class HDFPropertyUtil extends PropertyNotationUtil {
 
 	/**
 	 * return the custom label of the property, given UML2 specification and a
@@ -214,7 +91,7 @@ public class PropertyUtil {
 		if ((style & IHL7Appearance.DISP_MOFIFIERS) != 0) {
 			boolean multiLine = ((style & IHL7Appearance.DISP_MULTI_LINE) != 0);
 			// property modifiers
-			String modifiers = PropertyUtil.getModifiersAsString(property, multiLine);
+			String modifiers = HDFPropertyUtil.getModifiersAsString(property, multiLine);
 			if (!modifiers.equals("")) {
 				if (multiLine) {
 					buffer.append("\n");
@@ -474,80 +351,4 @@ public class PropertyUtil {
 		return value.toString();
 	}
 		
-	/**
-	 * Returns the modifier of the property, separated by a comma, as as single
-	 * line if <code>multiline</code> is <code>false</code> or as a multiline
-	 * string if <code>multiline</code> is <code>false</code>.
-	 * 
-	 * @param multiLine
-	 *            boolean that indicates if the string should have several lines
-	 *            when set to <code>true</code> or only one line when set to
-	 *            <code>false</code>.
-	 * 
-	 * @return a string giving all modifiers for the property
-	 */
-	private static String getModifiersAsString(Property property, boolean multiLine) {
-		StringBuffer buffer = new StringBuffer();
-		boolean needsComma = false;
-		String NL = (multiLine) ? "\n" : "";
-
-		// Return property modifiers
-		if (property.isReadOnly()) {
-			buffer.append("readOnly");
-			needsComma = true;
-		}
-		if (property.isDerivedUnion()) {
-			if (needsComma) {
-				buffer.append(",");
-				buffer.append(NL);
-			}
-			buffer.append("union");
-			needsComma = true;
-		}
-		if (property.isOrdered()) {
-			if (needsComma) {
-				buffer.append(",");
-				buffer.append(NL);
-			}
-			buffer.append("ordered");
-			needsComma = true;
-		}
-		if (property.isUnique()) {
-			if (needsComma) {
-				buffer.append(",");
-				buffer.append(NL);
-			}
-			buffer.append("unique");
-			needsComma = true;
-		}
-
-		// is the property redefining another property ?
-		Iterator<org.eclipse.uml2.uml.Property> it;
-		it = property.getRedefinedProperties().iterator();
-		while (it.hasNext()) {
-			org.eclipse.uml2.uml.Property current = it.next();
-			if (needsComma) {
-				buffer.append(",");
-				buffer.append(NL);
-			}
-			buffer.append("redefines ");
-			buffer.append(current.getName());
-			needsComma = true;
-		}
-
-		// is the property subsetting another property ?
-		it = property.getSubsettedProperties().iterator();
-		while (it.hasNext()) {
-			Property current = it.next();
-			if (needsComma) {
-				buffer.append(",");
-				buffer.append(NL);
-			}
-			buffer.append("subsets ");
-			buffer.append(current.getName());
-			needsComma = true;
-		}
-
-		return buffer.toString();
-	}
 }
