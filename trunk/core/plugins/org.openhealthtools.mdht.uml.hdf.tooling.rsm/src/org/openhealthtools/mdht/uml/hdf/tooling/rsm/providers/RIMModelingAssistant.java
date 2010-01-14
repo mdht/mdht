d29458c07050fd68ab9b0b4dd9a1729f54942dba
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-01-14 23:17:56 +0000

    Initial import from sourceforge

---
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.tooling.rsm/src/org/openhealthtools/mdht/uml/hdf/tooling/rsm/providers/RIMModelingAssistant.java
@@ -0,0 +1,213 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.hdf.tooling.rsm.providers;
+
+import com.ibm.xtools.uml.ui.internal.dialogs.selectelement.ISelectElementFilter;
+import com.ibm.xtools.uml.ui.internal.dialogs.selectelement.SelectElementDialog;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.gmf.runtime.emf.type.core.IMetamodelType;
+import org.eclipse.gmf.runtime.emf.type.core.ISpecializationType;
+
+import org.eclipse.gmf.runtime.emf.ui.services.modelingassistant.ModelingAssistantProvider;
+
+import org.eclipse.jface.viewers.IFilter;
+
+import org.eclipse.jface.window.Window;
+
+import org.openhealthtools.mdht.uml.hdf.tooling.rsm.types.RIMElementTypes;
+
+/**
+ * @generated
+ */
+public class RIMModelingAssistant extends ModelingAssistantProvider {
+
+    /**
+     * @generated
+     */
+    public List<IElementType> getRelTypesOnSource(IAdaptable source) {
+        EObject eObject = (EObject)source.getAdapter(EObject.class);
+        if (eObject != null) {
+            List<IElementType> result = new ArrayList<IElementType>();
+            for (int i = 0; i < RIMElementTypes.RELATIONSHIP_TYPES.length; ++i) {
+                IElementType elementType = RIMElementTypes.RELATIONSHIP_TYPES[i];
+                if (RIMElementTypes.matchesSource(elementType, eObject)) {
+                    result.add(elementType);
+                }
+            }
+            return result;
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * @generated
+     */
+    public List<IElementType> getRelTypesOnSourceAndTarget(IAdaptable source, IAdaptable target) {
+        EObject sourceEObject = (EObject)source.getAdapter(EObject.class);
+        EObject targetEObject = (EObject)target.getAdapter(EObject.class);
+        if (sourceEObject != null && targetEObject != null) {
+            List<IElementType> result = new ArrayList<IElementType>();
+            for (int i = 0; i < RIMElementTypes.RELATIONSHIP_TYPES.length; ++i) {
+                IElementType elementType = RIMElementTypes.RELATIONSHIP_TYPES[i];
+                if (RIMElementTypes.matchesSource(elementType, sourceEObject) &&
+                        RIMElementTypes.matchesTarget(elementType, targetEObject)) {
+                    result.add(elementType);
+                }
+            }
+            return result;
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * @generated
+     */
+    public List<IElementType> getRelTypesOnTarget(IAdaptable target) {
+        EObject eObject = (EObject)target.getAdapter(EObject.class);
+        if (eObject != null) {
+            List<IElementType> result = new ArrayList<IElementType>();
+            for (int i = 0; i < RIMElementTypes.RELATIONSHIP_TYPES.length; ++i) {
+                IElementType elementType = RIMElementTypes.RELATIONSHIP_TYPES[i];
+                if (RIMElementTypes.matchesTarget(elementType, eObject)) {
+                    result.add(elementType);
+                }
+            }
+            return result;
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * @generated
+     */
+    public List<IElementType> getTypesForSource(IAdaptable target, IElementType relationshipType) {
+        EObject eObject = (EObject)target.getAdapter(EObject.class);
+        if (eObject != null) {
+            return removeAbstractTypes(RIMElementTypes.getSources(relationshipType), eObject);
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * @generated
+     */
+    public List<IElementType> getTypesForTarget(IAdaptable source, IElementType relationshipType) {
+        EObject eObject = (EObject)source.getAdapter(EObject.class);
+        if (eObject != null) {
+            return removeAbstractTypes(RIMElementTypes.getTargets(relationshipType), eObject);
+        }
+        return Collections.emptyList();
+    }
+    
+    /**
+     * @generated
+     */
+    private List<IElementType> removeAbstractTypes(IElementType[] elementTypes, EObject eObject) {
+        if (elementTypes == null || elementTypes.length == 0) {
+            return Collections.emptyList();
+        }
+        List<IElementType> result = new ArrayList<IElementType>();
+        for (int i = 0; i < elementTypes.length; ++i) {
+            if (elementTypes[i].getEClass() == null || !elementTypes[i].getEClass().isAbstract()) {
+                result.add(elementTypes[i]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @generated
+     */
+    public EObject selectExistingElementForSource(IAdaptable target,
+            IElementType relationshipType) {
+        EObject eObject = (EObject)target.getAdapter(EObject.class);
+        if (eObject != null) {
+            List<IElementType> sourceTypes = Arrays.asList(RIMElementTypes.getSources(relationshipType));
+            if (!sourceTypes.isEmpty()) {
+                return openSelectElementDialog(target, sourceTypes);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @generated
+     */
+    public EObject selectExistingElementForTarget(IAdaptable source,
+            IElementType relationshipType) {
+        EObject eObject = (EObject)source.getAdapter(EObject.class);
+        if (eObject != null) {
+            List<IElementType> targetTypes = Arrays.asList(RIMElementTypes.getTargets(relationshipType));
+            if (!targetTypes.isEmpty()) {
+                return openSelectElementDialog(source, targetTypes);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @generated
+     */
+    private EObject openSelectElementDialog(IAdaptable context, final List<IElementType> elementTypes) {
+        Object eObject = context.getAdapter(EObject.class);
+        
+        final IFilter filter = new IFilter() {
+
+            public boolean select(Object toTest) {
+                if (toTest instanceof EObject) {
+                    EObject eObject = (EObject)toTest;
+                    for (IElementType elementType : elementTypes) {
+                        if (elementType instanceof IMetamodelType) {
+                            if (elementType.getEClass().isSuperTypeOf(eObject.eClass())) {
+                                return true;
+                            }
+                        } else if (elementType instanceof ISpecializationType) {
+                            if (((ISpecializationType)elementType).getMatcher() == null) {
+                                if (elementType.getEClass().isSuperTypeOf(eObject.eClass())) {
+                                    return true;
+                                }
+                            } else if (((ISpecializationType)elementType).getMatcher().matches(eObject)) {
+                                return true;
+                            }
+                        } 
+                    }
+                }
+                return false;
+            }
+            
+        };
+        
+        SelectElementDialog dialog = new SelectElementDialog((EObject) eObject, (ISelectElementFilter)filter) {
+            protected boolean isValidSelection(List currentSelectedElements) {
+                return filter.select(currentSelectedElements.get(0));
+            }
+        };
+
+        if (dialog.open() != Window.OK) {
+            // user canceled gesture
+            return null;
+        }
+        
+        return (EObject) dialog.getSelectedElements().get(0);
+    }
+}
---
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.tooling.rsm/src/org/openhealthtools/mdht/uml/hdf/tooling/rsm/types/RIMElementTypes.java
@@ -0,0 +1,190 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.hdf.tooling.rsm.types;
+
+import com.ibm.xtools.uml.type.IStereotypedElementType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.eclipse.gmf.runtime.emf.type.core.AbstractElementTypeEnumerator;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.gmf.runtime.emf.type.core.IMetamodelType;
+import org.eclipse.gmf.runtime.emf.type.core.ISpecializationType;
+
+/**
+ * @generated
+ */
+public class RIMElementTypes extends AbstractElementTypeEnumerator {
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _ACT__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.ActClass"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _ENTITY__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.EntityClass"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _ROLE__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.RoleClass"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _ROLELINK__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.RoleLinkClass"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _ACTRELATIONSHIP__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.ActRelationshipClass"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _PARTICIPATION__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.ParticipationClass"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IStereotypedElementType _LANGUAGECOMMUNICATION__CLASS = (IStereotypedElementType)getElementType("org.openhealthtools.mdht.uml.hdf.tooling.rsm.LanguageCommunication"); //$NON-NLS-1$
+
+    /**
+     * @generated
+     */
+    public static final IElementType[] NODE_TYPES = {
+            _ACT__CLASS,
+            _ENTITY__CLASS,
+            _ROLE__CLASS,
+            _ROLELINK__CLASS,
+            _ACTRELATIONSHIP__CLASS,
+            _PARTICIPATION__CLASS,
+            _LANGUAGECOMMUNICATION__CLASS
+        };
+
+    /**
+     * @generated
+     */
+    public static final IElementType[] RELATIONSHIP_TYPES = {
+            
+        };
+    
+    /**
+     * @generated
+     */
+    private static Map<IElementType, IElementType[]> sources = new HashMap<IElementType, IElementType[]>();
+    
+    /**
+     * @generated
+     */
+    static {
+        };
+    
+    /**
+     * @generated
+     */
+    private static Map<IElementType, IElementType[]> targets = new HashMap<IElementType, IElementType[]>();
+    
+    /**
+     * @generated
+     */
+    static {
+        };
+    
+    /**
+     * @generated
+     */
+    public static final IElementType[] getSources(IElementType elementType) {
+        return sources.get(elementType);
+    }
+    
+    /**
+     * @generated
+     */
+    public static final IElementType[] getTargets(IElementType elementType) {
+        return targets.get(elementType);
+    }
+    
+    /**
+     * @generated
+     */
+    public static IElementType getMatchingSource(IElementType elementType, EObject source) {
+        return matches(getSources(elementType), source);
+    }
+    
+    /**
+     * @generated
+     */
+    public static IElementType getMatchingTarget(IElementType elementType, EObject target) {
+        return matches(getTargets(elementType), target);
+    }
+    
+    /**
+     * @generated
+     */
+    public static boolean matchesSource(IElementType elementType, EObject source) {
+        IElementType[] sourceTypes = getSources(elementType);
+        if (sourceTypes != null && sourceTypes.length == 0) {
+            return true;
+        }
+        return matches(sourceTypes, source) != null;
+    }
+    
+    /**
+     * @generated
+     */
+    public static boolean matchesTarget(IElementType elementType, EObject target) {
+        IElementType[] targetTypes = getTargets(elementType);
+        if (targetTypes != null && targetTypes.length == 0) {
+            return true;
+        }
+        return matches(targetTypes, target) != null;
+    }
+    
+    /**
+     * @generated
+     */
+    private static IElementType matches(IElementType[] elementTypes, EObject eObject) {
+        if (elementTypes == null) {
+            return null;
+        }
+        for (int i = 0; i < elementTypes.length; ++i) {
+            IElementType elementType = elementTypes[i];
+            if (elementType instanceof ISpecializationType) {
+                if (((ISpecializationType)elementType).getMatcher() != null &&
+                        ((ISpecializationType)elementType).getMatcher().matches(eObject)) {
+                    return elementType;
+                }
+            } 
+        }
+        for (int i = 0; i < elementTypes.length; ++i) {
+            IElementType elementType = elementTypes[i];
+            if (elementType instanceof ISpecializationType) {
+                if (((ISpecializationType)elementType).getMatcher() == null &&
+                        elementType.getEClass().isSuperTypeOf(eObject.eClass())) {
+                    return elementType;
+                }
+            } else if (elementType instanceof IMetamodelType) {
+                if (elementType.getEClass().isSuperTypeOf(eObject.eClass())) {
+                    return elementType;
+                }
+            }
+        }
+        return null;
+    }
+}
