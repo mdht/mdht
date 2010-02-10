d7fa2ed788766c54ff4470b514b4e4b38f7d1d3d
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-02-10 23:17:33 +0000

    updated CDARegistry to load from EPackage.Registry removing dependency on Extension Registry and OSGi

--- cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/internal/registry/CDARegistry.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/internal/registry/CDARegistry.java
@@ -14,57 +14,35 @@ package org.openhealthtools.mdht.uml.cda.internal.registry;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 public class CDARegistry {
 	public static final CDARegistry INSTANCE = new CDARegistry();
 	private Map<String, EClass> classes = null;
-	
-	private static final String CDA_EXTENSION = "org.openhealthtools.mdht.uml.cda.extension";
-	private static final String ATT_NSURI = "nsURI";
-	private static final String ATT_ECLASS = "eClass";
-	private static final String ATT_ID = "id";
+	private static final String CDA_ANNOTATION_SOURCE = "http://www.openhealthtools.org/mdht/uml/cda/annotation";
+	private static final String TEMPLATE_ID_ROOT = "templateId.root";
 	
 	private CDARegistry() {
 		classes = new HashMap<String, EClass>();
+		load();
 	}
 	
 	private void load() {
-		classes.clear();
-		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		if (registry != null) {
-			IExtensionPoint point = registry.getExtensionPoint(CDA_EXTENSION);
-			if (point != null) {
-				for (IExtension extension : point.getExtensions()) {
-					for (IConfigurationElement element : extension.getConfigurationElements()) {
-						if (validate(element)) {
-							EPackage ePackage = EPackage.Registry.INSTANCE.getEPackage(element.getAttribute(ATT_NSURI));
-							if (ePackage != null) {
-								EClass eClass = (EClass) ePackage.getEClassifier(element.getAttribute(ATT_ECLASS));
-								if (eClass != null) {
-									classes.put(element.getAttribute(ATT_ID), eClass);
-								}
-							}
-						}
-					}
+		for (String key : EPackage.Registry.INSTANCE.keySet()) {
+			EPackage ePackage = EPackage.Registry.INSTANCE.getEPackage(key);
+			for (EClassifier eClassifier : ePackage.getEClassifiers()) {
+				String templateId = EcoreUtil.getAnnotation(eClassifier, CDA_ANNOTATION_SOURCE, TEMPLATE_ID_ROOT);
+				if (templateId != null) {
+					classes.put(templateId, (EClass) eClassifier);
 				}
 			}
 		}
 	}
 	
-	private boolean validate(IConfigurationElement element) {
-		return (element.getAttribute(ATT_NSURI) != null && element.getAttribute(ATT_ECLASS) != null && element.getAttribute(ATT_ID) != null);
-	}
-	
 	public EClass getEClass(String id) {
-		load();
 		return classes.get(id);
 	}
 }
