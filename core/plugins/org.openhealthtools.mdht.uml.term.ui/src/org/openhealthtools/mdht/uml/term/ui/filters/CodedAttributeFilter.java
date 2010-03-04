afcbe99e13f225559a77cae28bf839b95e813573
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-03-04 15:53:16 +0000

    Refactor CTS to Term

---
+++ core/plugins/org.openhealthtools.mdht.uml.term.ui/src/org/openhealthtools/mdht/uml/term/ui/filters/CodedAttributeFilter.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2010 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.term.ui.filters;
+
+import java.util.List;
+
+import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Property;
+import org.openhealthtools.mdht.uml.common.util.UMLUtil;
+
+/**
+ * Selects an object if it is a UML Property where type is specialization of HL7 CD.
+ */
+public class CodedAttributeFilter extends CTSFilter {
+
+	public boolean select(Object object) {
+		Element element = getElement(object);
+		
+		if (element instanceof Property
+				&& ((Property)element).getType() instanceof Classifier) {
+			Classifier type = (Classifier) ((Property)element).getType();
+			if (UMLUtil.isTemplateBinding(type)) {
+				List<Classifier> params = UMLUtil.getTemplateBindingParameters(type);
+				if (params.size() == 1)
+					type = params.get(0);
+			}
+			
+			List<String> allParentNames = UMLUtil.getAllParentNames(type);
+			return allParentNames.contains("CD")
+					|| allParentNames.contains("SC");
+		}
+		return false;
+	}
+
+}
---
+++ core/plugins/org.openhealthtools.mdht.uml.term.ui/src/org/openhealthtools/mdht/uml/term/ui/filters/ValueSetVersionFilter.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.term.ui.filters;
+
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Enumeration;
+import org.eclipse.uml2.uml.Stereotype;
+import org.openhealthtools.mdht.uml.term.core.util.TermProfileUtil;
+import org.openhealthtools.mdht.uml.term.core.util.ITermProfileConstants;
+
+/**
+ * Selects an object if it is a UML Property with 
+ * ValueSetVersion stereotype applied.
+ */
+public class ValueSetVersionFilter extends CTSFilter {
+
+	public boolean select(Object object) {
+		Element element = getElement(object);
+		
+		if (element instanceof Enumeration) {
+			Stereotype stereotype = TermProfileUtil.getAppliedStereotype(
+					element, ITermProfileConstants.VALUE_SET_VERSION);
+			return stereotype != null;
+		}
+		return false;
+	}
+
+}
