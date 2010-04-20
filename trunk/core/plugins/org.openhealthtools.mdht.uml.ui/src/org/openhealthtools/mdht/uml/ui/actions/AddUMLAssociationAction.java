322c11b6c7e229fdc8fbc48c4cf4387b06d2146b
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-04-20 15:40:14 +0000

    Change default of new association end to be 0..1

--- trunk/core/plugins/org.openhealthtools.mdht.uml.ui/src/org/openhealthtools/mdht/uml/ui/actions/AddUMLAssociationAction.java
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.ui/src/org/openhealthtools/mdht/uml/ui/actions/AddUMLAssociationAction.java
@@ -73,6 +73,7 @@ public class AddUMLAssociationAction extends UML2AbstractAction {
 							property.setName(propertyName);
 							source.getOwnedAttributes().add(property);
 							property.setType(target);
+							property.setLower(0);
 							property.setAggregation(AggregationKind.NONE_LITERAL);
 
 							Association association = (Association) source.getNearestPackage()
