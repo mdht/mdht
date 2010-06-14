67873151e94cc9d69e652b549bc37bdacbec2e24
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-06-14 18:20:18 +0000

    Updated to junit4

--- trunk/core/tests/org.openhealthtools.mdht.uml.common.test/src/org/openhealthtools/mdht/uml/common/util/test/AllTests.java
+++ trunk/core/tests/org.openhealthtools.mdht.uml.common.test/src/org/openhealthtools/mdht/uml/common/util/test/AllTests.java
@@ -1,8 +1,13 @@
 package org.openhealthtools.mdht.uml.common.util.test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {UMLUtilTest.class })
 public class AllTests {
 
 	public static Test suite() {
