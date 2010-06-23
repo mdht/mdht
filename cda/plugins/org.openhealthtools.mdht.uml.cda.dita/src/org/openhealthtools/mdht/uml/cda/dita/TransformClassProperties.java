3788bd6ccf9cf32b85afc795461776b31fc50b0e
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-06-23 20:11:50 +0000

    Support for publishing developer doc.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.dita/src/org/openhealthtools/mdht/uml/cda/dita/TransformClassProperties.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.dita/src/org/openhealthtools/mdht/uml/cda/dita/TransformClassProperties.java
@@ -4,18 +4,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.Constraint;
 import org.eclipse.uml2.uml.Generalization;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.Property;
 import org.openhealthtools.mdht.uml.cda.core.util.CDAModelUtil;
 import org.openhealthtools.mdht.uml.cda.core.util.InstanceGenerator;
 import org.openhealthtools.mdht.uml.cda.dita.internal.Logger;
+import org.openhealthtools.mdht.uml.common.util.NamedElementComparator;
 import org.openhealthtools.mdht.uml.common.util.UMLUtil;
 
 public class TransformClassProperties extends TransformAbstract {
@@ -92,6 +99,7 @@ public class TransformClassProperties extends TransformAbstract {
 		writer.println("<body>");
 		
 		appendConformanceRules(writer, umlClass);
+		appendAggregateRules(writer, umlClass);
 		appendExample(writer, umlClass);
 		
 		writer.println("</body>");
@@ -103,16 +111,33 @@ public class TransformClassProperties extends TransformAbstract {
 		
 		boolean hasRules = false;
 		for (Generalization generalization : umlClass.getGeneralizations()) {
-			String rule = CDAModelUtil.computeConformanceMessage(generalization, true);
-			if (rule.length() > 0) {
+			String message = CDAModelUtil.computeConformanceMessage(generalization, true);
+			if (message.length() > 0) {
 				hasRules = true;
-				writer.println("<li>" + rule + "</li>");
+				writer.println("<li>" + message + "</li>");
 			}
 		}
-		for (Property property : umlClass.getOwnedAttributes()) {
+
+		List<Property> allProperties = new ArrayList<Property>(umlClass.getOwnedAttributes());
+		List<Property> allAttributes = new ArrayList<Property>();
+		for (Property property : allProperties) {
+			if (CDAModelUtil.isXMLAttribute(property)) {
+				allAttributes.add(property);
+			}
+		}
+		allProperties.removeAll(allAttributes);
+		Collections.sort(allAttributes, new NamedElementComparator());
+		// XML attributes
+		for (Property property : allAttributes) {
 			hasRules = true;
 			writer.println("<li>" + CDAModelUtil.computeConformanceMessage(property, true) + "</li>");
 		}
+		// XML elements
+		for (Property property : allProperties) {
+			hasRules = true;
+			writer.println("<li>" + CDAModelUtil.computeConformanceMessage(property, true) + "</li>");
+		}
+
 		for (Constraint constraint : umlClass.getOwnedRules()) {
 			hasRules = true;
 			writer.println("<li>" + CDAModelUtil.computeConformanceMessage(constraint, true) + "</li>");
@@ -125,6 +150,98 @@ public class TransformClassProperties extends TransformAbstract {
 		writer.println("</ol>");
 	}
 
+	private void appendAggregateRules(PrintWriter writer, Class umlClass) {
+		Package xrefSource = UMLUtil.getTopPackage(umlClass);
+		List<Classifier> allParents = new ArrayList<Classifier>(umlClass.allParents());
+		allParents.add(0, umlClass);
+
+		List<Property> allProperties = new ArrayList<Property>();
+		List<Property> allAttributes = new ArrayList<Property>();
+		List<Constraint> allConstraints = new ArrayList<Constraint>();
+		
+		// process parents in reverse order, CDA base class first
+		for (int i=allParents.size()-1; i>=0; i--) {
+			Class parent = (Class) allParents.get(i);
+			
+			for (Property property : parent.getOwnedAttributes()) {
+				// if list contains this property name, replace it; else append
+				int index = findProperty(allProperties, property.getName());
+				if (index >= 0) {
+					allProperties.set(index, property);
+				}
+				else {
+					allProperties.add(property);
+				}
+			}
+		}
+
+		for (int i=allParents.size()-1; i>=0; i--) {
+			Class parent = (Class) allParents.get(i);
+			
+			for (Constraint constraint : parent.getOwnedRules()) {
+				allConstraints.add(constraint);
+			}
+		}
+
+		writer.println("<ol id=\"aggregate\">");
+		
+		// use i>0 to omit this class
+		for (int i=allParents.size()-1; i>0; i--) {
+			Class parent = (Class) allParents.get(i);
+			String message = CDAModelUtil.computeGeneralizationConformanceMessage(parent, true, xrefSource);
+			if (message.length() > 0) {
+				writer.println("<li>" + message + "</li>");
+			}
+		}
+
+		for (Property property : allProperties) {
+			if (CDAModelUtil.isXMLAttribute(property)) {
+				allAttributes.add(property);
+			}
+		}
+		allProperties.removeAll(allAttributes);
+		Collections.sort(allAttributes, new NamedElementComparator());
+		// XML attributes
+		for (Property property : allAttributes) {
+			writer.println("<li>" + modelPrefix(property) +
+					CDAModelUtil.computeConformanceMessage(property, true, xrefSource) + "</li>");
+		}
+		// XML elements
+		for (Property property : allProperties) {
+			writer.println("<li>" + modelPrefix(property) +
+					CDAModelUtil.computeConformanceMessage(property, true, xrefSource) + "</li>");
+		}
+
+		for (Constraint constraint : allConstraints) {
+			writer.println("<li>" + modelPrefix(constraint) +
+					CDAModelUtil.computeConformanceMessage(constraint, true) + "</li>");
+		}
+
+		writer.println("</ol>");
+	}
+	
+	private int findProperty(List<Property> properties, String name) {
+		if (name != null) {
+			for (int i=0; i<properties.size(); i++) {
+				if (name.equals(properties.get(i).getName()))
+					return i;
+			}
+		}
+		return -1;
+	}
+
+	private String modelPrefix(NamedElement element) {
+		StringBuffer prefix = new StringBuffer();
+		String modelPrefix = CDAModelUtil.getModelPrefix(element);
+		if (modelPrefix != null && modelPrefix.length() > 0) {
+			prefix.append("[");
+			prefix.append(modelPrefix);
+			prefix.append("] ");
+		}
+		
+		return prefix.toString();
+	}
+	
 	private void appendExample(PrintWriter writer, Class umlClass) {
 		writer.print("<codeblock id=\"example\"><![CDATA[");
 
@@ -140,5 +257,5 @@ public class TransformClassProperties extends TransformAbstract {
 		
 		writer.println("]]></codeblock>");
 	}
-
+	
 }
