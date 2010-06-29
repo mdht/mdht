21bd67a1edf0afd0c1cbeccd624be8f2c73d0014
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-06-29 18:53:05 +0000

    Initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.test/src/org/openhealthtools/mdht/uml/cda/operations/ClinicalStatementOperationsTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class is a JUnit 4 test case for ClinicalStatementOperations.
+ */
+@SuppressWarnings("nls")
+public class ClinicalStatementOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.operations.ClinicalStatementOperations#getEntryRelationshipTargets(org.openhealthtools.mdht.uml.cda.ClinicalStatement, org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship, java.lang.Object)}.
+	 */
+	@Test
+	public void testGetEntryRelationshipTargetsClinicalStatementX_ActRelationshipEntryRelationshipObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.operations.ClinicalStatementOperations#getEntryRelationshipTargets(org.openhealthtools.mdht.uml.cda.ClinicalStatement, java.lang.Object)}.
+	 */
+	@Test
+	public void testGetEntryRelationshipTargetsClinicalStatementObject() {
+		fail("Not yet implemented");
+	}
+
+} // ClinicalStatementOperationsTest
