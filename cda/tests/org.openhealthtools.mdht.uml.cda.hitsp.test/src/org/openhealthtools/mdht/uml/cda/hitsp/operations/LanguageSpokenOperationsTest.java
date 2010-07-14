2db51644b104972f7601d82a580a1a4e5e61f220
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-14 23:12:11 +0000

    added LanguageSpokenOperationsTest

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/LanguageSpokenOperationsTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.hitsp.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ihe.operations.LanguageCommunicationOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class LanguageSpokenOperationsTest extends LanguageCommunicationOperationsTest{
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.LanguageSpokenOperations#validateLanguageSpokenTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.LanguageSpoken, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateLanguageSpokenTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // LanguageSpokenOperationsTest
