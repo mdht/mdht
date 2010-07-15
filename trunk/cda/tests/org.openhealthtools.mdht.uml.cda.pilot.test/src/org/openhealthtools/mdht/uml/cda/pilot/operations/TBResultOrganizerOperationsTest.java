a41a0e0c2199814a2bacea6b636a9b79e5bcd183
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 19:50:45 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.pilot.test/src/org/openhealthtools/mdht/uml/cda/pilot/operations/TBResultOrganizerOperationsTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.pilot.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ccd.operations.ResultOrganizerOperationsTest;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class TBResultOrganizerOperationsTest extends ResultOrganizerOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultOrganizerOperations#validateTBResultOrganizerTBResultObservation(org.openhealthtools.mdht.uml.cda.pilot.TBResultOrganizer, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTBResultOrganizerTBResultObservation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultOrganizerOperations#validateResultOrganizerTemplateId(org.openhealthtools.mdht.uml.cda.pilot.TBResultOrganizer, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultOrganizerTemplateIdTBResultOrganizerDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultOrganizerOperations#validateResultOrganizerCode(org.openhealthtools.mdht.uml.cda.pilot.TBResultOrganizer, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultOrganizerCodeTBResultOrganizerDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+} // TBResultOrganizerOperationsTest
---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.pilot.test/src/org/openhealthtools/mdht/uml/cda/pilot/operations/TBResultsSectionOperationsTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.pilot.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ccd.operations.ResultsSectionOperationsTest;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class TBResultsSectionOperationsTest extends ResultsSectionOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultsSectionOperations#validateTBResultsSectionText(org.openhealthtools.mdht.uml.cda.pilot.TBResultsSection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTBResultsSectionText() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultsSectionOperations#validateTBResultsSectionTitle(org.openhealthtools.mdht.uml.cda.pilot.TBResultsSection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTBResultsSectionTitle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultsSectionOperations#validateTBResultsSectionTBResultOrganizer(org.openhealthtools.mdht.uml.cda.pilot.TBResultsSection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTBResultsSectionTBResultOrganizer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultsSectionOperations#validateTBResultsSectionTBResultObservation(org.openhealthtools.mdht.uml.cda.pilot.TBResultsSection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTBResultsSectionTBResultObservation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultsSectionOperations#validateResultsSectionTemplateId(org.openhealthtools.mdht.uml.cda.pilot.TBResultsSection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultsSectionTemplateIdTBResultsSectionDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultsSectionOperations#validateResultsSectionCode(org.openhealthtools.mdht.uml.cda.pilot.TBResultsSection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultsSectionCodeTBResultsSectionDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+} // TBResultsSectionOperationsTest
