c35095b22461ca8c3e540bb0138b73f8be2878ba
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 18:50:05 +0000

    initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/CommentOperationsTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.hitsp.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class CommentOperationsTest extends org.openhealthtools.mdht.uml.cda.ihe.operations.CommentOperationsTest{
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.CommentOperations#validateHITSPCommentTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.Comment, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateHITSPCommentTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // CommentOperationsTest
