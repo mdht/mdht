9e54111bae6e347edd129f00aa23c5538bc0a6dd
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 15:44:47 +0000

    initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/CoverageEntryOperationsTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ihe.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ccd.operations.CoverageActivityOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class CoverageEntryOperationsTest extends CoverageActivityOperationsTest{
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.CoverageEntryOperations#validateCoverageEntryTemplateId(org.openhealthtools.mdht.uml.cda.ihe.CoverageEntry, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateCoverageEntryTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // CoverageEntryOperationsTest
