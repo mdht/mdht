344d3e65df0d4fec07b048a350c9a9b18a998515
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 17:40:07 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/MedicalSummaryOperationsTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ihe.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class MedicalSummaryOperationsTest extends MedicalDocumentOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalSummaryOperations#validateMedicalSummaryProblemConcernEntry(org.openhealthtools.mdht.uml.cda.ihe.MedicalSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateMedicalSummaryProblemConcernEntry() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalSummaryOperations#validateMedicalSummaryAllergyConcernEntry(org.openhealthtools.mdht.uml.cda.ihe.MedicalSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateMedicalSummaryAllergyConcernEntry() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalSummaryOperations#validateMedicalSummaryMedications(org.openhealthtools.mdht.uml.cda.ihe.MedicalSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateMedicalSummaryMedications() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalSummaryOperations#validateMedicalSummaryTemplateId(org.openhealthtools.mdht.uml.cda.ihe.MedicalSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateMedicalSummaryTemplateId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalSummaryOperations#validateMedicalSummaryCode(org.openhealthtools.mdht.uml.cda.ihe.MedicalSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateMedicalSummaryCode() {
+		fail("Not yet implemented");
+	}
+
+}
+// MedicalSummaryOperationsTest
