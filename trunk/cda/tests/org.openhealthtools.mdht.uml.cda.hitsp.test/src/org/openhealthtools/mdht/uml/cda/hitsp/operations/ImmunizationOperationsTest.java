91d0365c0d8015c003be638b32b12284fc7a8d95
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 19:06:11 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/ImmunizationOperationsTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.hitsp.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class ImmunizationOperationsTest extends org.openhealthtools.mdht.uml.cda.ihe.operations.ImmunizationOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.ImmunizationOperations#validateHITSPImmunizationTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.Immunization, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateHITSPImmunizationTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // ImmunizationOperationsTest
