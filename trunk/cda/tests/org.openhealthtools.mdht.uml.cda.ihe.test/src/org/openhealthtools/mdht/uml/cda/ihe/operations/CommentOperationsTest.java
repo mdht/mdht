655d84676b087f8b808be49bb9e4bd21f0bc7d7d
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 18:45:10 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/CommentOperationsTest.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ihe.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.operations.ActOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class CommentOperationsTest extends ActOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.CommentOperations#validateCommentTemplateId(org.openhealthtools.mdht.uml.cda.ihe.Comment, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateCommentTemplateId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.CommentOperations#validateCommentClassCode(org.openhealthtools.mdht.uml.cda.ihe.Comment, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateCommentClassCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.CommentOperations#validateCommentCode(org.openhealthtools.mdht.uml.cda.ihe.Comment, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateCommentCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.CommentOperations#validateCommentStatusCode(org.openhealthtools.mdht.uml.cda.ihe.Comment, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateCommentStatusCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.CommentOperations#validateCommentText(org.openhealthtools.mdht.uml.cda.ihe.Comment, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateCommentText() {
+		fail("Not yet implemented");
+	}
+
+} // CommentOperationsTest
