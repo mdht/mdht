1564e44d392992733cf625e43a09a7556962507b
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 19:50:45 +0000

    initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.pilot.test/src/org/openhealthtools/mdht/uml/cda/pilot/operations/TBResultObservationOperationsTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.pilot.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.hitsp.operations.ResultOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class TBResultObservationOperationsTest extends ResultOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultObservationOperations#validateTBResultObservationClassCode(org.openhealthtools.mdht.uml.cda.pilot.TBResultObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTBResultObservationClassCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultObservationOperations#validateResultObservationTemplateId(org.openhealthtools.mdht.uml.cda.pilot.TBResultObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultObservationTemplateIdTBResultObservationDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultObservationOperations#validateResultObservationCode(org.openhealthtools.mdht.uml.cda.pilot.TBResultObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultObservationCodeTBResultObservationDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TBResultObservationOperations#validateResultObservationStatusCode(org.openhealthtools.mdht.uml.cda.pilot.TBResultObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateResultObservationStatusCodeTBResultObservationDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+}
+// TBResultObservationOperationsTest
---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.pilot.test/src/org/openhealthtools/mdht/uml/cda/pilot/operations/TuberculosisFollowUpProgressNoteOperationsTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.pilot.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.operations.ClinicalDocumentOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class TuberculosisFollowUpProgressNoteOperationsTest extends ClinicalDocumentOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TuberculosisFollowUpProgressNoteOperations#validateTuberculosisFollowUpProgressNotePatientNames(org.openhealthtools.mdht.uml.cda.pilot.TuberculosisFollowUpProgressNote, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTuberculosisFollowUpProgressNotePatientNames() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TuberculosisFollowUpProgressNoteOperations#validateTuberculosisFollowUpProgressNoteTemplateId(org.openhealthtools.mdht.uml.cda.pilot.TuberculosisFollowUpProgressNote, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTuberculosisFollowUpProgressNoteTemplateId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.pilot.operations.TuberculosisFollowUpProgressNoteOperations#validateTuberculosisFollowUpProgressNoteTBResultsSection(org.openhealthtools.mdht.uml.cda.pilot.TuberculosisFollowUpProgressNote, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateTuberculosisFollowUpProgressNoteTBResultsSection() {
+		fail("Not yet implemented");
+	}
+
+} // TuberculosisFollowUpProgressNoteOperationsTest
