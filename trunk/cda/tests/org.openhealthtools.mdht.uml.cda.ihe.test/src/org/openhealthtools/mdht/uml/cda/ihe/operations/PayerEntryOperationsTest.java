848833ece5c6c428865442a4b62bd5d1789e2807
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 18:36:30 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/PayerEntryOperationsTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ihe.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ccd.operations.PolicyActivityOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class PayerEntryOperationsTest extends PolicyActivityOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.PayerEntryOperations#validatePolicyActivityTemplateId(org.openhealthtools.mdht.uml.cda.ihe.PayerEntry, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidatePolicyActivityTemplateIdPayerEntryDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+} // PayerEntryOperationsTest
