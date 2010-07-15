2fd078c547d9e2210fadbddff4d2c56270fa7586
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 18:38:25 +0000

    initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProcedureEntryPlanOfCareActivityProcedureOperationsTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ihe.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ccd.operations.PlanOfCareActivityProcedureOperationsTest;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class ProcedureEntryPlanOfCareActivityProcedureOperationsTest extends PlanOfCareActivityProcedureOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.ProcedureEntryPlanOfCareActivityProcedureOperations#validateProcedureEntryPlanOfCareActivityProcedureTemplateId(org.openhealthtools.mdht.uml.cda.ihe.ProcedureEntryPlanOfCareActivityProcedure, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateProcedureEntryPlanOfCareActivityProcedureTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // ProcedureEntryPlanOfCareActivityProcedureOperationsTest
