7a4523fc1aaf230cef526a68a436c922191a7aa1
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 20:10:17 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ncr.test/src/org/openhealthtools/mdht/uml/cda/ncr/operations/NeonatalCareReportOperationsTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ncr.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.operations.ClinicalDocumentOperationsTest;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class NeonatalCareReportOperationsTest  extends ClinicalDocumentOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ncr.operations.NeonatalCareReportOperations#validateNeonatalCareReportTemplateId(org.openhealthtools.mdht.uml.cda.ncr.NeonatalCareReport, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateNeonatalCareReportTemplateId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ncr.operations.NeonatalCareReportOperations#validateNeonatalCareReportPatientDataSectionNCR(org.openhealthtools.mdht.uml.cda.ncr.NeonatalCareReport, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateNeonatalCareReportPatientDataSectionNCR() {
+		fail("Not yet implemented");
+	}
+
+} // NeonatalCareReportOperationsTest
---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ncr.test/src/org/openhealthtools/mdht/uml/cda/ncr/operations/PatientDataSectionNCROperationsTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ncr.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class PatientDataSectionNCROperationsTest extends PatientDataSectionOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ncr.operations.PatientDataSectionNCROperations#validatePatientDataSectionNCREncountersSection(org.openhealthtools.mdht.uml.cda.ncr.PatientDataSectionNCR, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidatePatientDataSectionNCREncountersSection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ncr.operations.PatientDataSectionNCROperations#validatePatientDataSectionNCRAcuityDataSection(org.openhealthtools.mdht.uml.cda.ncr.PatientDataSectionNCR, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidatePatientDataSectionNCRAcuityDataSection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ncr.operations.PatientDataSectionNCROperations#validatePatientDataSectionNCRBirthWeight(org.openhealthtools.mdht.uml.cda.ncr.PatientDataSectionNCR, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidatePatientDataSectionNCRBirthWeight() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ncr.operations.PatientDataSectionNCROperations#validatePatientDataSectionTemplateId(org.openhealthtools.mdht.uml.cda.ncr.PatientDataSectionNCR, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidatePatientDataSectionTemplateIdPatientDataSectionNCRDiagnosticChainMapOfObjectObject() {
+		fail("Not yet implemented");
+	}
+
+}
+// PatientDataSectionNCROperationsTest
