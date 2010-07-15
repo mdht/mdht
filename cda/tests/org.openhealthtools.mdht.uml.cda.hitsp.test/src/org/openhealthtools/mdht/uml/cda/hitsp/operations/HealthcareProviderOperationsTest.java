1c103dd2bdae95583b6f7a0c2e3292300a843772
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 19:03:54 +0000

    initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/HealthcareProviderOperationsTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.hitsp.operations;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ihe.operations.HealthcareProvidersPharmaciesOperationsTest;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class HealthcareProviderOperationsTest extends HealthcareProvidersPharmaciesOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.HealthcareProviderOperations#validateHealthcareProviderTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.HealthcareProvider, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateHealthcareProviderTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // HealthcareProviderOperationsTest
