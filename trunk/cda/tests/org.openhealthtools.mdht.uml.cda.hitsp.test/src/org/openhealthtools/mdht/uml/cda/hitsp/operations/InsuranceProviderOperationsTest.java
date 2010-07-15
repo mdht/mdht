32d3fdff575752cf7b934808d7ac7ff180e086bb
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-15 19:07:48 +0000

    initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/InsuranceProviderOperationsTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.hitsp.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ihe.operations.CoverageEntryOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class InsuranceProviderOperationsTest extends CoverageEntryOperationsTest{
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.InsuranceProviderOperations#validateInsuranceProviderTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.InsuranceProvider, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateInsuranceProviderTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // InsuranceProviderOperationsTest
