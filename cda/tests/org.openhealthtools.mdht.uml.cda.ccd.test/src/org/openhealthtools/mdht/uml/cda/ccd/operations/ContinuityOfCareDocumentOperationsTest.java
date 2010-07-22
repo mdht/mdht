ca46b30d4e7bbc1d3b97d65a958443551d4b9dcd
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-22 19:06:12 +0000

    modified getTestCases() to include test cases from super class.

--- cda/tests/org.openhealthtools.mdht.uml.cda.ccd.test/src/org/openhealthtools/mdht/uml/cda/ccd/operations/ContinuityOfCareDocumentOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ccd.test/src/org/openhealthtools/mdht/uml/cda/ccd/operations/ContinuityOfCareDocumentOperationsTest.java
@@ -13,7 +13,7 @@
 package org.openhealthtools.mdht.uml.cda.ccd.operations;
 
 import static org.junit.Assert.assertTrue;
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -210,7 +210,9 @@ public class ContinuityOfCareDocumentOperationsTest extends CCDValidationTest {
 	protected List<CDATestCase> getTestCases() {
 		// Return a new List because the one returned by Arrays.asList is
 		// unmodifiable so a sub-class can't append their test cases.
-		return new ArrayList<CDATestCase>(Arrays.asList(TEST_CASE_ARRAY));
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
 	}
 
 	@Override
--- cda/tests/org.openhealthtools.mdht.uml.cda.ccd.test/src/org/openhealthtools/mdht/uml/cda/ccd/operations/FunctionalStatusSectionOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ccd.test/src/org/openhealthtools/mdht/uml/cda/ccd/operations/FunctionalStatusSectionOperationsTest.java
@@ -12,7 +12,6 @@
  */
 package org.openhealthtools.mdht.uml.cda.ccd.operations;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -120,7 +119,9 @@ public class FunctionalStatusSectionOperationsTest extends CCDValidationTest {
 	protected List<CDATestCase> getTestCases() {
 		// Return a new List because the one returned by Arrays.asList is
 		// unmodifiable so a sub-class can't append their test cases.
-		return new ArrayList<CDATestCase>(Arrays.asList(TEST_CASE_ARRAY));
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
 	}
 
 	@Override
