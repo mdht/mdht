ae6c60d6a2138d84d0cdd2c420b951df323363e3
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-22 18:14:44 +0000

    derives from CDAValidationTest added empty test case array implemented getTestCases()

--- cda/tests/org.openhealthtools.mdht.uml.cda.test/src/org/openhealthtools/mdht/uml/cda/operations/ClinicalStatementOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.test/src/org/openhealthtools/mdht/uml/cda/operations/ClinicalStatementOperationsTest.java
@@ -12,7 +12,10 @@
  */
 package org.openhealthtools.mdht.uml.cda.operations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -20,10 +23,23 @@ import org.junit.Test;
  * This class is a JUnit 4 test case for ClinicalStatementOperations.
  */
 @SuppressWarnings("nls")
-public class ClinicalStatementOperationsTest {
+public abstract class ClinicalStatementOperationsTest extends CDAValidationTest {
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {};
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
+	}
 
 	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.operations.ClinicalStatementOperations#getEntryRelationshipTargets(org.openhealthtools.mdht.uml.cda.ClinicalStatement, org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship, java.lang.Object)}.
+	 * Test method for
+	 * {@link org.openhealthtools.mdht.uml.cda.operations.ClinicalStatementOperations#getEntryRelationshipTargets(org.openhealthtools.mdht.uml.cda.ClinicalStatement, org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship, java.lang.Object)}
+	 * .
 	 */
 	@Test
 	public void testGetEntryRelationshipTargetsClinicalStatementX_ActRelationshipEntryRelationshipObject() {
@@ -31,7 +47,9 @@ public class ClinicalStatementOperationsTest {
 	}
 
 	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.operations.ClinicalStatementOperations#getEntryRelationshipTargets(org.openhealthtools.mdht.uml.cda.ClinicalStatement, java.lang.Object)}.
+	 * Test method for
+	 * {@link org.openhealthtools.mdht.uml.cda.operations.ClinicalStatementOperations#getEntryRelationshipTargets(org.openhealthtools.mdht.uml.cda.ClinicalStatement, java.lang.Object)}
+	 * .
 	 */
 	@Test
 	public void testGetEntryRelationshipTargetsClinicalStatementObject() {
