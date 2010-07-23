7b609eaf7999d45c50b02a903f70b26a59604624
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:51:36 +0000

    implemented test for validateProcedureEntryPlanOfCareActivityProcedureTemplateId()

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProcedureEntryPlanOfCareActivityProcedureOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProcedureEntryPlanOfCareActivityProcedureOperationsTest.java
@@ -12,23 +12,67 @@
  */
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
-import org.junit.Test;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.openhealthtools.mdht.uml.cda.ccd.operations.PlanOfCareActivityProcedureOperationsTest;
+import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
+import org.openhealthtools.mdht.uml.cda.ihe.ProcedureEntryPlanOfCareActivityProcedure;
 
 /**
- * This class
+ * This class is a JUnit 4 test case.
  */
 @SuppressWarnings("nls")
-public class ProcedureEntryPlanOfCareActivityProcedureOperationsTest extends PlanOfCareActivityProcedureOperationsTest {
+public class ProcedureEntryPlanOfCareActivityProcedureOperationsTest extends
+		PlanOfCareActivityProcedureOperationsTest {
+
+	@SuppressWarnings("hiding")
+	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.4.19";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+
+	// Template ID
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return ProcedureEntryPlanOfCareActivityProcedureOperations
+					.validateProcedureEntryPlanOfCareActivityProcedureTemplateId(
+							(ProcedureEntryPlanOfCareActivityProcedure) objectToTest,
+							diagnostician, map);
+		}
+	} };
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
+	}
 
 	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.ProcedureEntryPlanOfCareActivityProcedureOperations#validateProcedureEntryPlanOfCareActivityProcedureTemplateId(org.openhealthtools.mdht.uml.cda.ihe.ProcedureEntryPlanOfCareActivityProcedure, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 * @see org.openhealthtools.mdht.uml.cda.operations.MutualExclusionValidationTest#getObjectToTest()
 	 */
-	@Test
-	public void testValidateProcedureEntryPlanOfCareActivityProcedureTemplateId() {
-		fail("Not yet implemented");
+	@Override
+	protected EObject getObjectToTest() {
+		return IHEFactory.eINSTANCE
+				.createProcedureEntryPlanOfCareActivityProcedure();
+	}
+
+	/**
+	 * @see org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalDocumentOperationsTest#getObjectInitToTest()
+	 */
+	@Override
+	protected EObject getObjectInitToTest() {
+		return IHEFactory.eINSTANCE
+				.createProcedureEntryPlanOfCareActivityProcedure().init();
 	}
 
 } // ProcedureEntryPlanOfCareActivityProcedureOperationsTest
