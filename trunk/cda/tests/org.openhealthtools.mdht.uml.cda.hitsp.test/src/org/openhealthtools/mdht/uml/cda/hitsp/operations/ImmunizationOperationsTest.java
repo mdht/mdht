c386a03ab1c3d90ed2b0c9868b58d543cac94d9c
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:11:25 +0000

    implemented test for validateHITSPImmunizationTemplateId()

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/ImmunizationOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/ImmunizationOperationsTest.java
@@ -12,22 +12,57 @@
  */
 package org.openhealthtools.mdht.uml.cda.hitsp.operations;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
-import org.junit.Test;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.openhealthtools.mdht.uml.cda.hitsp.HITSPFactory;
+import org.openhealthtools.mdht.uml.cda.hitsp.Immunization;
 
 /**
  * This class is a JUnit 4 test case.
  */
 @SuppressWarnings("nls")
-public class ImmunizationOperationsTest extends org.openhealthtools.mdht.uml.cda.ihe.operations.ImmunizationOperationsTest {
+public class ImmunizationOperationsTest
+		extends
+		org.openhealthtools.mdht.uml.cda.ihe.operations.ImmunizationOperationsTest {
 
-	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.ImmunizationOperations#validateHITSPImmunizationTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.Immunization, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
-	 */
-	@Test
-	public void testValidateHITSPImmunizationTemplateId() {
-		fail("Not yet implemented");
+	protected static final String TEMPLATE_ID = "2.16.840.1.113883.3.88.11.83.13";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return ImmunizationOperations.validateHITSPImmunizationTemplateId(
+					(Immunization) objectToTest, diagnostician, map);
+		}
+	} }; // TEST_CASE_ARRAY
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		retValue.addAll(super.getTestCases());
+		return retValue;
+	}
+
+	@Override
+	protected EObject getObjectToTest() {
+		return HITSPFactory.eINSTANCE.createImmunization();
+	}
+
+	@Override
+	protected EObject getObjectInitToTest() {
+		return HITSPFactory.eINSTANCE.createImmunization().init();
 	}
 
 } // ImmunizationOperationsTest
