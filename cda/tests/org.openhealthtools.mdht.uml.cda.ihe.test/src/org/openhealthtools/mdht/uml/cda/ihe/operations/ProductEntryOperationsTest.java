fb2bb163623f4c08204511124c5eec2e3db6bafe
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:57:47 +0000

    implemented test for validateProductEntryTemplateId()

--- cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProductEntryOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProductEntryOperationsTest.java
@@ -12,10 +12,15 @@
  */
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
-import org.junit.Test;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.openhealthtools.mdht.uml.cda.ccd.operations.ProductOperationsTest;
+import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
+import org.openhealthtools.mdht.uml.cda.ihe.ProductEntry;
 
 /**
  * This class is a JUnit 4 test case.
@@ -23,14 +28,41 @@ import org.openhealthtools.mdht.uml.cda.ccd.operations.ProductOperationsTest;
 @SuppressWarnings("nls")
 public class ProductEntryOperationsTest extends ProductOperationsTest {
 
-	/**
-	 * Test method for
-	 * {@link org.openhealthtools.mdht.uml.cda.ihe.operations.ProductEntryOperations#validateProductEntryTemplateId(org.openhealthtools.mdht.uml.cda.ihe.ProductEntry, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
-	 * .
-	 */
-	@Test
-	public void testValidateProductEntryTemplateId() {
-		fail("Not yet implemented");
+	@SuppressWarnings("hiding")
+	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.4.7.2";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return ProductEntryOperations.validateProductEntryTemplateId(
+					(ProductEntry) objectToTest, diagnostician, map);
+		}
+
+	} }; // TEST_CASE_ARRAY
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
+	}
+
+	@Override
+	protected EObject getObjectToTest() {
+		return IHEFactory.eINSTANCE.createProductEntry();
+	}
+
+	@Override
+	protected EObject getObjectInitToTest() {
+		return IHEFactory.eINSTANCE.createProductEntry().init();
 	}
 
 } // ProductEntryOperationsTest
