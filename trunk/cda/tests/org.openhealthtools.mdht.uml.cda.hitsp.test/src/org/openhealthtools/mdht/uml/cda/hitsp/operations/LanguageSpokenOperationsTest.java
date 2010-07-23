655d1f4ab1452f1956f6a54b5717715f9be7a36b
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:19:41 +0000

    add commented test implementation stub

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/LanguageSpokenOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/LanguageSpokenOperationsTest.java
@@ -12,7 +12,7 @@
  */
 package org.openhealthtools.mdht.uml.cda.hitsp.operations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.openhealthtools.mdht.uml.cda.ihe.operations.LanguageCommunicationOperationsTest;
@@ -23,6 +23,42 @@ import org.openhealthtools.mdht.uml.cda.ihe.operations.LanguageCommunicationOper
 @SuppressWarnings("nls")
 public class LanguageSpokenOperationsTest extends LanguageCommunicationOperationsTest{
 
+//	protected static final String TEMPLATE_ID = "2.16.840.1.113883.3.88.11.83.11";
+//
+//	private static final CDATestCase TEST_CASE_ARRAY[] = {
+//	// Template ID
+//	// -------------------------------------------------------------
+//	new TemplateIDValidationTest(TEMPLATE_ID) {
+//
+//		@Override
+//		protected boolean validate(final EObject objectToTest,
+//				final BasicDiagnostic diagnostician,
+//				final Map<Object, Object> map) {
+//			return CommentOperations.validateHITSPCommentTemplateId(
+//					(Comment) objectToTest, diagnostician, map);
+//		}
+//	} }; // TEST_CASE_ARRAY
+//
+//	@Override
+//	protected List<CDATestCase> getTestCases() {
+//		// Return a new List because the one returned by Arrays.asList is
+//		// unmodifiable so a sub-class can't append their test cases.
+//		final List<CDATestCase> retValue = super.getTestCases();
+//		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+//		retValue.addAll(super.getTestCases());
+//		return retValue;
+//	}
+//
+//	@Override
+//	protected EObject getObjectToTest() {
+//		return HITSPFactory.eINSTANCE.createLanguageSpoken();
+//	}
+//
+//	@Override
+//	protected EObject getObjectInitToTest() {
+//		return HITSPFactory.eINSTANCE.createLanguageSpoken().init();
+//	}
+	
 	/**
 	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.LanguageSpokenOperations#validateLanguageSpokenTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.LanguageSpoken, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
 	 */
