2073d2c0b10ca7d187b16b37bd06561859c744b9
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:55:34 +0000

    implemented test for validateProcedureEntryProcedureActivityProcedureTemplateId()

--- cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProcedureEntryProcedureActivityProcedureOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ProcedureEntryProcedureActivityProcedureOperationsTest.java
@@ -12,23 +12,62 @@
  */
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
-import org.junit.Test;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.openhealthtools.mdht.uml.cda.ccd.operations.ProcedureActivityProcedureOperationsTest;
+import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
+import org.openhealthtools.mdht.uml.cda.ihe.ProcedureEntryProcedureActivityProcedure;
 
 /**
  * This class is a JUnit 4 test case.
  */
 @SuppressWarnings("nls")
-public class ProcedureEntryProcedureActivityProcedureOperationsTest extends ProcedureActivityProcedureOperationsTest {
+public class ProcedureEntryProcedureActivityProcedureOperationsTest extends
+		ProcedureActivityProcedureOperationsTest {
 
-	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.ProcedureEntryProcedureActivityProcedureOperations#validateProcedureEntryProcedureActivityProcedureTemplateId(org.openhealthtools.mdht.uml.cda.ihe.ProcedureEntryProcedureActivityProcedure, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
-	 */
-	@Test
-	public void testValidateProcedureEntryProcedureActivityProcedureTemplateId() {
-		fail("Not yet implemented");
+	@SuppressWarnings("hiding")
+	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.4.19";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return ProcedureEntryProcedureActivityProcedureOperations
+					.validateProcedureEntryProcedureActivityProcedureTemplateId(
+							(ProcedureEntryProcedureActivityProcedure) objectToTest,
+							diagnostician, map);
+		}
+
+	} }; // TEST_CASE_ARRAY
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
+	}
+
+	@Override
+	protected EObject getObjectToTest() {
+		return IHEFactory.eINSTANCE
+				.createProcedureEntryProcedureActivityProcedure();
+	}
+
+	@Override
+	protected EObject getObjectInitToTest() {
+		return IHEFactory.eINSTANCE
+				.createProcedureEntryProcedureActivityProcedure().init();
 	}
 
 } // ProcedureEntryProcedureActivityProcedureOperationsTest
