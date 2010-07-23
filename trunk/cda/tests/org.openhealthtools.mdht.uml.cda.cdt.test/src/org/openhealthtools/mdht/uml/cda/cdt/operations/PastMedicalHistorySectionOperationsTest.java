3b6c81e2d120aa1852781b2651fac45ccc2cc047
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 18:29:29 +0000

    implemented test for validatePastMedicalHistorySectionTemplateId()

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/PastMedicalHistorySectionOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/PastMedicalHistorySectionOperationsTest.java
@@ -16,10 +16,13 @@ import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.openhealthtools.mdht.uml.cda.cdt.CDTFactory;
+import org.openhealthtools.mdht.uml.cda.cdt.PastMedicalHistorySection;
 import org.openhealthtools.mdht.uml.cda.operations.SectionOperationsTest;
 
 /**
@@ -29,7 +32,24 @@ import org.openhealthtools.mdht.uml.cda.operations.SectionOperationsTest;
 public class PastMedicalHistorySectionOperationsTest extends
 		SectionOperationsTest {
 
-	private static final CDATestCase TEST_CASE_ARRAY[] = {};
+	protected static final String TEMPLATE_ID = "2.16.840.1.113883.10.20.2.9";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return PastMedicalHistorySectionOperations
+					.validatePastMedicalHistorySectionTemplateId(
+							(PastMedicalHistorySection) objectToTest,
+							diagnostician, map);
+		}
+
+	} }; // TEST_CASE_ARRAY
 
 	@Override
 	protected List<CDATestCase> getTestCases() {
@@ -55,16 +75,6 @@ public class PastMedicalHistorySectionOperationsTest extends
 	 */
 	@Test
 	public void testValidatePastMedicalHistorySectionClinicalStatements() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link org.openhealthtools.mdht.uml.cda.cdt.operations.PastMedicalHistorySectionOperations#validatePastMedicalHistorySectionTemplateId(org.openhealthtools.mdht.uml.cda.cdt.PastMedicalHistorySection, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
-	 * .
-	 */
-	@Test
-	public void testValidatePastMedicalHistorySectionTemplateId() {
 		fail("Not yet implemented");
 	}
 
