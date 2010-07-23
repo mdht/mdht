04dd5a3f64b50b24e9c608a1162dfeaa3ae911b1
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 19:18:33 +0000

    implemented test for validateReasonForVisitSectionConsultTemplateId()

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/ReasonForVisitSectionConsultOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/ReasonForVisitSectionConsultOperationsTest.java
@@ -16,20 +16,40 @@ import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.openhealthtools.mdht.uml.cda.cdt.CDTFactory;
+import org.openhealthtools.mdht.uml.cda.cdt.ReasonForVisitSectionConsult;
 import org.openhealthtools.mdht.uml.cda.operations.SectionOperationsTest;
 
 /**
- * This class
+ * This class is a JUnit 4 test case.
  */
 @SuppressWarnings("nls")
 public class ReasonForVisitSectionConsultOperationsTest extends
 		SectionOperationsTest {
 
-	private static final CDATestCase TEST_CASE_ARRAY[] = {};
+	protected static final String TEMPLATE_ID = "2.16.840.1.113883.10.20.4.8";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return ReasonForVisitSectionConsultOperations
+					.validateReasonForVisitSectionConsultTemplateId(
+							(ReasonForVisitSectionConsult) objectToTest,
+							diagnostician, map);
+		}
+
+	} }; // TEST_CASE_ARRAY
 
 	@Override
 	protected List<CDATestCase> getTestCases() {
@@ -46,16 +66,6 @@ public class ReasonForVisitSectionConsultOperationsTest extends
 	@Override
 	protected EObject getObjectToTest() {
 		return CDTFactory.eINSTANCE.createReasonForVisitSectionConsult();
-	}
-
-	/**
-	 * Test method for
-	 * {@link org.openhealthtools.mdht.uml.cda.cdt.operations.ReasonForVisitSectionConsultOperations#validateReasonForVisitSectionConsultTemplateId(org.openhealthtools.mdht.uml.cda.cdt.ReasonForVisitSectionConsult, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
-	 * .
-	 */
-	@Test
-	public void testValidateReasonForVisitSectionConsultTemplateId() {
-		fail("Not yet implemented");
 	}
 
 	/**
