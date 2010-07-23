2348a8f80b27586f9997056af71fdfb77ae959a3
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:18:47 +0000

    add commented test implementation stub

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/HealthcareProviderOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/HealthcareProviderOperationsTest.java
@@ -23,6 +23,42 @@ import org.openhealthtools.mdht.uml.cda.ihe.operations.HealthcareProvidersPharma
 @SuppressWarnings("nls")
 public class HealthcareProviderOperationsTest extends HealthcareProvidersPharmaciesOperationsTest {
 
+//	protected static final String TEMPLATE_ID = "2.16.840.1.113883.3.88.11.83.11";
+//
+//	private static final CDATestCase TEST_CASE_ARRAY[] = {
+//	// Template ID
+//	// -------------------------------------------------------------
+//	new TemplateIDValidationTest(TEMPLATE_ID) {
+//
+//		@Override
+//		protected boolean validate(final EObject objectToTest,
+//				final BasicDiagnostic diagnostician,
+//				final Map<Object, Object> map) {
+//			return CommentOperations.validateHITSPCommentTemplateId(
+//					(Comment) objectToTest, diagnostician, map);
+//		}
+//	} }; // TEST_CASE_ARRAY
+//
+//	@Override
+//	protected List<CDATestCase> getTestCases() {
+//		// Return a new List because the one returned by Arrays.asList is
+//		// unmodifiable so a sub-class can't append their test cases.
+//		final List<CDATestCase> retValue = super.getTestCases();
+//		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+//		retValue.addAll(super.getTestCases());
+//		return retValue;
+//	}
+//
+//	@Override
+//	protected EObject getObjectToTest() {
+//		return HITSPFactory.eINSTANCE.createComment();
+//	}
+//
+//	@Override
+//	protected EObject getObjectInitToTest() {
+//		return HITSPFactory.eINSTANCE.createComment().init();
+//	}
+	
 	/**
 	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.HealthcareProviderOperations#validateHealthcareProviderTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.HealthcareProvider, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
 	 */
