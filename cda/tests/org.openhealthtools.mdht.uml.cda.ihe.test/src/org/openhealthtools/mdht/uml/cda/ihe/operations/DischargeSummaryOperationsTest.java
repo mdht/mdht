29232566dce69e67266b8dfb5a3ff8255d9b3589
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:36:31 +0000

    implemented test for validateDischargeSummaryTemplateId()

--- cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/DischargeSummaryOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/DischargeSummaryOperationsTest.java
@@ -12,26 +12,72 @@
  */
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary;
+import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
 
 /**
  * This class is a JUnit 4 test case.
  */
 @SuppressWarnings("nls")
-public class DischargeSummaryOperationsTest extends MedicalSummaryOperationsTest{
+public class DischargeSummaryOperationsTest extends
+		MedicalSummaryOperationsTest {
 
-	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateDischargeSummaryTemplateId(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
-	 */
-	@Test
-	public void testValidateDischargeSummaryTemplateId() {
-		fail("Not yet implemented");
+	@SuppressWarnings("hiding")
+	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.1.4";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+
+	// Template ID
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return DischargeSummaryOperations
+					.validateDischargeSummaryTemplateId(
+							(DischargeSummary) objectToTest, diagnostician, map);
+		}
+	} };
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		return retValue;
 	}
 
 	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateDischargeSummaryActiveProblemsSection(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 * @see org.openhealthtools.mdht.uml.cda.operations.MutualExclusionValidationTest#getObjectToTest()
+	 */
+	@Override
+	protected EObject getObjectToTest() {
+		return IHEFactory.eINSTANCE.createDischargeSummary();
+	}
+
+	/**
+	 * @see org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalDocumentOperationsTest#getObjectInitToTest()
+	 */
+	@Override
+	protected EObject getObjectInitToTest() {
+		return IHEFactory.eINSTANCE.createDischargeSummary().init();
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateDischargeSummaryActiveProblemsSection(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
+	 * .
 	 */
 	@Test
 	public void testValidateDischargeSummaryActiveProblemsSection() {
@@ -39,7 +85,9 @@ public class DischargeSummaryOperationsTest extends MedicalSummaryOperationsTest
 	}
 
 	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateMedicalSummaryCode(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 * Test method for
+	 * {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateMedicalSummaryCode(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
+	 * .
 	 */
 	@Test
 	public void testValidateMedicalSummaryCodeDischargeSummaryDiagnosticChainMapOfObjectObject() {
