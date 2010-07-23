315f7f0784b07c10d24d79a2d5e6917dba5c93f1
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:00:55 +0000

    implemented test for validateInsuranceProviderTemplateId()

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/InsuranceProviderOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.hitsp.test/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/InsuranceProviderOperationsTest.java
@@ -12,9 +12,17 @@
  */
 package org.openhealthtools.mdht.uml.cda.hitsp.operations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.hitsp.HITSPFactory;
+import org.openhealthtools.mdht.uml.cda.hitsp.InsuranceProvider;
 import org.openhealthtools.mdht.uml.cda.ihe.operations.CoverageEntryOperationsTest;
 
 /**
@@ -23,12 +31,40 @@ import org.openhealthtools.mdht.uml.cda.ihe.operations.CoverageEntryOperationsTe
 @SuppressWarnings("nls")
 public class InsuranceProviderOperationsTest extends CoverageEntryOperationsTest{
 
-	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.hitsp.operations.InsuranceProviderOperations#validateInsuranceProviderTemplateId(org.openhealthtools.mdht.uml.cda.hitsp.InsuranceProvider, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
-	 */
-	@Test
-	public void testValidateInsuranceProviderTemplateId() {
-		fail("Not yet implemented");
+	protected static final String TEMPLATE_ID = "2.16.840.1.113883.3.88.11.83.5";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return InsuranceProviderOperations.validateInsuranceProviderTemplateId(
+					(InsuranceProvider) objectToTest, diagnostician, map);
+		}
+	} }; // TEST_CASE_ARRAY
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		retValue.addAll(super.getTestCases());
+		return retValue;
 	}
 
+	@Override
+	protected EObject getObjectToTest() {
+		return HITSPFactory.eINSTANCE.createInsuranceProvider();
+	}
+
+	@Override
+	protected EObject getObjectInitToTest() {
+		return HITSPFactory.eINSTANCE.createInsuranceProvider().init();
+	}
+	
 } // InsuranceProviderOperationsTest
