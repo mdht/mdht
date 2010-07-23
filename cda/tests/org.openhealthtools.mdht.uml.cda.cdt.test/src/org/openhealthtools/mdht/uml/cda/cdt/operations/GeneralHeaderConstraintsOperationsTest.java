f9e8f91787d6cc337a86346e258790c8267ddbd5
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 20:14:30 +0000

    implemented test for validateGeneralHeaderConstraintsCode()

--- cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/GeneralHeaderConstraintsOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/GeneralHeaderConstraintsOperationsTest.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.ccd.operations.CCDValidationTest.CodeCCDValidationTest;
 import org.openhealthtools.mdht.uml.cda.cdt.CDTFactory;
 import org.openhealthtools.mdht.uml.cda.cdt.GeneralHeaderConstraints;
 import org.openhealthtools.mdht.uml.cda.operations.ClinicalDocumentOperationsTest;
@@ -35,22 +36,41 @@ public class GeneralHeaderConstraintsOperationsTest extends
 	@SuppressWarnings("hiding")
 	protected static final String TEMPLATE_ID = "2.16.840.1.113883.10.20.3";
 
+	@SuppressWarnings("hiding")
+	protected static final String CODE = "unspecified";
+	@SuppressWarnings("hiding")
+	protected static final String CODE_SYSTEM = "unspecified";
+
 	private static final CDATestCase TEST_CASE_ARRAY[] = {
-	// Template ID
-	// -------------------------------------------------------------
-	new TemplateIDValidationTest(TEMPLATE_ID) {
+			// Template ID
+			// -------------------------------------------------------------
+			new TemplateIDValidationTest(TEMPLATE_ID) {
 
-		@Override
-		protected boolean validate(final EObject objectToTest,
-				final BasicDiagnostic diagnostician,
-				final Map<Object, Object> map) {
-			return GeneralHeaderConstraintsOperations
-					.validateGeneralHeaderConstraintsTemplateId(
-							(GeneralHeaderConstraints) objectToTest,
-							diagnostician, map);
-		}
+				@Override
+				protected boolean validate(final EObject objectToTest,
+						final BasicDiagnostic diagnostician,
+						final Map<Object, Object> map) {
+					return GeneralHeaderConstraintsOperations
+							.validateGeneralHeaderConstraintsTemplateId(
+									(GeneralHeaderConstraints) objectToTest,
+									diagnostician, map);
+				}
 
-	} }; // TEST_CASE_ARRAY
+			},
+
+			// Code
+			// -------------------------------------------------------------
+			new CodeCCDValidationTest(CODE, CODE_SYSTEM) {
+				@Override
+				protected boolean validate(final EObject objectToTest,
+						final BasicDiagnostic diagnostician,
+						final Map<Object, Object> map) {
+					return GeneralHeaderConstraintsOperations
+							.validateGeneralHeaderConstraintsCode(
+									(GeneralHeaderConstraints) objectToTest,
+									diagnostician, map);
+				}
+			} }; // TEST_CASE_ARRAY
 
 	@Override
 	protected List<CDATestCase> getTestCases() {
@@ -106,16 +126,6 @@ public class GeneralHeaderConstraintsOperationsTest extends
 	 */
 	@Test
 	public void testValidateGeneralHeaderConstraintsCopyTimeNotPresent() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link org.openhealthtools.mdht.uml.cda.cdt.operations.GeneralHeaderConstraintsOperations#validateGeneralHeaderConstraintsCode(org.openhealthtools.mdht.uml.cda.cdt.GeneralHeaderConstraints, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
-	 * .
-	 */
-	@Test
-	public void testValidateGeneralHeaderConstraintsCode() {
 		fail("Not yet implemented");
 	}
 
