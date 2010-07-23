5f29ed250787142200510d33d3b04b4f7accc237
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-07-23 17:13:30 +0000

    implemented test for validateImmunizationTemplateId()

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ImmunizationOperationsTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/ImmunizationOperationsTest.java
@@ -12,23 +12,57 @@
  */
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
-import org.junit.Test;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
 import org.openhealthtools.mdht.uml.cda.ccd.operations.MedicationActivityOperationsTest;
+import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
+import org.openhealthtools.mdht.uml.cda.ihe.Immunization;
 
 /**
  * This class is a JUnit 4 test case.
  */
 @SuppressWarnings("nls")
-public class ImmunizationOperationsTest extends MedicationActivityOperationsTest{
+public class ImmunizationOperationsTest extends
+		MedicationActivityOperationsTest {
 
-	/**
-	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.ImmunizationOperations#validateImmunizationTemplateId(org.openhealthtools.mdht.uml.cda.ihe.Immunization, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
-	 */
-	@Test
-	public void testValidateImmunizationTemplateId() {
-		fail("Not yet implemented");
+	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.4.12";
+
+	private static final CDATestCase TEST_CASE_ARRAY[] = {
+	// Template ID
+	// -------------------------------------------------------------
+	new TemplateIDValidationTest(TEMPLATE_ID) {
+
+		@Override
+		protected boolean validate(final EObject objectToTest,
+				final BasicDiagnostic diagnostician,
+				final Map<Object, Object> map) {
+			return ImmunizationOperations.validateImmunizationTemplateId(
+					(Immunization) objectToTest, diagnostician, map);
+		}
+	} }; // TEST_CASE_ARRAY
+
+	@Override
+	protected List<CDATestCase> getTestCases() {
+		// Return a new List because the one returned by Arrays.asList is
+		// unmodifiable so a sub-class can't append their test cases.
+		final List<CDATestCase> retValue = super.getTestCases();
+		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
+		retValue.addAll(super.getTestCases());
+		return retValue;
+	}
+
+	@Override
+	protected EObject getObjectToTest() {
+		return IHEFactory.eINSTANCE.createImmunization();
+	}
+
+	@Override
+	protected EObject getObjectInitToTest() {
+		return IHEFactory.eINSTANCE.createImmunization().init();
 	}
 
 } // ImmunizationOperationsTest
