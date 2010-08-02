392a1b355b0f20234248a588038a6f7882bca706
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-08-02 17:30:58 +0000

    Change action AssignValidationMessage to RemoveValidationMessage.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/actions/RemoveValidationMessagesAction.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/actions/RemoveValidationMessagesAction.java
@@ -1,5 +1,6 @@
 package org.openhealthtools.mdht.uml.cda.ui.actions;
 
+import java.util.Collections;
 import java.util.Iterator;
 
 import org.eclipse.core.commands.ExecutionException;
@@ -9,7 +10,9 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.emf.workspace.AbstractEMFOperation;
@@ -21,18 +24,23 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Constraint;
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.util.UMLSwitch;
 import org.openhealthtools.mdht.uml.cda.core.util.CDAModelUtil;
 import org.openhealthtools.mdht.uml.cda.core.util.CDAProfileUtil;
 import org.openhealthtools.mdht.uml.cda.ui.internal.Logger;
 
-public class AssignValidationMessageAction implements IObjectActionDelegate {
+public class RemoveValidationMessagesAction implements IObjectActionDelegate {
 	
 	protected IWorkbenchPart activePart;
 	protected ISelection currentSelection;
 	protected Element selectedElement;
 	
-	public AssignValidationMessageAction() {
+	public RemoveValidationMessagesAction() {
 		super();
 	}
 
@@ -43,10 +51,10 @@ public class AssignValidationMessageAction implements IObjectActionDelegate {
 		try {
 			TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(selectedElement);
 			
-			IUndoableOperation operation = new AbstractEMFOperation(editingDomain, "Assign Validation Message") {
+			IUndoableOperation operation = new AbstractEMFOperation(editingDomain, "Remove Validation Messages") {
 			    protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) {
 			    	
-			    	CDAModelUtil.assignAllConformanceMessages(selectedElement, false);
+			    	removeAllConformanceMessages(selectedElement);
 					
 			        return Status.OK_STATUS;
 			    }};
@@ -118,4 +126,42 @@ public class AssignValidationMessageAction implements IObjectActionDelegate {
 		return null;
 	}
 
+	private void removeAllConformanceMessages(Element element) {
+		final TreeIterator<EObject> iterator = EcoreUtil.getAllContents(
+				Collections.singletonList(element));
+		while (iterator != null && iterator.hasNext()) {
+			EObject child = iterator.next();
+
+			UMLSwitch<Object> umlSwitch = new UMLSwitch<Object>() {
+
+				public Object caseAssociation(Association association) {
+					CDAModelUtil.setValidationMessage(association, null);
+					return association;
+				}
+
+				public Object caseClass(Class umlClass) {
+					CDAModelUtil.setValidationMessage(umlClass, null);
+					return umlClass;
+				}
+
+				public Object caseProperty(Property property) {
+					CDAModelUtil.setValidationMessage(property, null);
+					
+					Association association = property.getAssociation();
+					if (association != null) {
+						CDAModelUtil.setValidationMessage(association, null);
+					}
+					
+					return property;
+				}
+				
+				public Object caseConstraint(Constraint constraint) {
+					CDAModelUtil.setValidationMessage(constraint, null);
+					return constraint;
+				}
+			};
+			umlSwitch.doSwitch(child);
+		}
+	}
+	
 }
