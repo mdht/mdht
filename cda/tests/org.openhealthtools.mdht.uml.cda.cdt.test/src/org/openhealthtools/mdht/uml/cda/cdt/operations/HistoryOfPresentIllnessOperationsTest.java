017f104718de909a2589a80b40231a9bed6916ef
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-08-13 18:45:30 +0000

    initial check-in

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.cdt.test/src/org/openhealthtools/mdht/uml/cda/cdt/operations/HistoryOfPresentIllnessOperationsTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.cdt.operations;
+
+import static org.junit.Assert.fail;
+
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.cdt.HistoryOfPresentIllness;
+import org.openhealthtools.mdht.uml.cda.operations.SectionOperationsTest;
+
+/**
+ * This class is a JUnit 4 test case.
+ */
+@SuppressWarnings("nls")
+public class HistoryOfPresentIllnessOperationsTest extends SectionOperationsTest {
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.cdt.operations.HistoryOfPresentIllnessOperations#validateHistoryOfPresentIllnessTemplateId(org.openhealthtools.mdht.uml.cda.cdt.HistoryOfPresentIllness, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateHistoryOfPresentIllnessTemplateId() {
+		HistoryOfPresentIllness objectToTest = (HistoryOfPresentIllness) getObjectToTest();
+		HistoryOfPresentIllnessOperations
+				.validateHistoryOfPresentIllnessTemplateId(objectToTest,
+						Diagnostician.INSTANCE
+								.createDefaultDiagnostic(objectToTest), map);
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.cdt.operations.HistoryOfPresentIllnessOperations#validateHistoryOfPresentIllnessCode(org.openhealthtools.mdht.uml.cda.cdt.HistoryOfPresentIllness, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateHistoryOfPresentIllnessCode() {
+		HistoryOfPresentIllness objectToTest = (HistoryOfPresentIllness) getObjectToTest();
+		HistoryOfPresentIllnessOperations
+				.validateHistoryOfPresentIllnessCode(objectToTest,
+						Diagnostician.INSTANCE
+								.createDefaultDiagnostic(objectToTest), map);
+		fail("Not yet implemented");
+	}
+
+} // HistoryOfPresentIllnessOperationsTest
