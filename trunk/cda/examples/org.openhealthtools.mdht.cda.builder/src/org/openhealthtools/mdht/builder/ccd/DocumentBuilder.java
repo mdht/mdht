ea1b646b0533221bfe788a6fc2e9839f41c7ace4
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-08-17 19:20:16 +0000

    Fixed copyright comment

--- trunk/cda/examples/org.openhealthtools.mdht.cda.builder/src/org/openhealthtools/mdht/builder/ccd/DocumentBuilder.java
+++ trunk/cda/examples/org.openhealthtools.mdht.cda.builder/src/org/openhealthtools/mdht/builder/ccd/DocumentBuilder.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
-2 	* Copyright (c) 2010 Sean Muir.
-3 	* All rights reserved. This program and the accompanying materials
-4 	* are made available under the terms of the Eclipse Public License v1.0
-5 	* which accompanies this distribution, and is available at
-6 	* http://www.eclipse.org/legal/epl-v10.html
-7 	*
-8 	* Contributors:
-9 	* Sean Muir (JKM Software) - initial API and implementation
-10 	*
-11 	* $Id$
-12 	*******************************************************************************/
-package org.openhealthtools.mdht.builder.ccd;
+ * Copyright (c) 2010 Sean Muir.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Sean Muir (JKM Software) - initial API and implementation
+ *
+ * $Id$
+ *******************************************************************************/
+ package org.openhealthtools.mdht.builder.ccd;
 
 import org.openhealthtools.mdht.builder.cda.ArrayBuilder;
 import org.openhealthtools.mdht.builder.cda.GenericSectionBuilder;
