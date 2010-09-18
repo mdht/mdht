8ba7f6a5825d670b269e1131b768305acd69cf25
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-09-18 12:45:43 +0000

    More Examples

---
+++ cda/examples/org.openhealthtools.mdht.cda.builder/src/org/openhealthtools/mdht/builder/examples/C32PayersExample.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Sean Muir.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Sean Muir (JKM Software) - initial API and implementation
+ *
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.builder.examples;
+
+import java.io.FileOutputStream;
+
+import org.openhealthtools.mdht.builder.ccd.DocumentBuilder.MedicationsSectionBuilder;
+import org.openhealthtools.mdht.builder.ccd.DocumentBuilder.PayersSectionBuilder;
+import org.openhealthtools.mdht.builder.hitsp.C32DocumentBuilder;
+import org.openhealthtools.mdht.builder.hitsp.DocumentBuilder;
+import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
+import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
+
+/**
+ * C32Example is an example implementation of the MDHT DocumentBuilder based on
+ * the GOF BuilderPattern. see http://en.wikipedia.org/wiki/Builder_pattern
+ * 
+ * The DocumentBuilder orchestrates the various CDA Builder components in order
+ * to build a complete CDA document. The DocumentBuilder provides default
+ * behavior requiring only the need to override the builder you need to created
+ * the document while ignoring others.
+ * 
+ * 
+ * This example uses C32DocumentBuilder which creates the minimum structures to
+ * create valid (no errors) C32 V2.5
+ * 
+ */
+
+public class C32PayersExample {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Define and override various builders
+		 */
+		DocumentBuilder exampleHITSPC32 = new C32DocumentBuilder() {
+
+			@Override
+			public PayersSectionBuilder getPayersSectionBuilder() {
+				return new PayersSectionBuilder()
+				{
+					
+				};
+			}
+
+		
+
+		
+
+		};
+
+		try {
+
+			System.out.println("Start C32 Document Build Example");
+
+			ClinicalDocument clinicalDocument = exampleHITSPC32.buildDocument();
+
+			CDAUtil.save(clinicalDocument, new FileOutputStream("/home/eclipse/heliosworkspaceG/org.openhealthtools.mdht.cda.builder/resource/ExampleC32Payers.xml"));
+
+			System.out.println("Completed C32 Document Build Example");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
