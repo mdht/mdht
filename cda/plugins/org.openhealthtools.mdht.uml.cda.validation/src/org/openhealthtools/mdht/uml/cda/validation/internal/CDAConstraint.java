b8f1f843e2bcede593f72d2534b271fda94e881a
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-09-28 20:56:38 +0000

    Initial version of CDA Model Validation plugin

---
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.validation/src/org/openhealthtools/mdht/uml/cda/validation/internal/CDAConstraint.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Sean Muir
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sean Muir (JKM Software) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+
+package org.openhealthtools.mdht.uml.cda.validation.internal;
+
+/**
+ * 
+ * $Id: $
+ */
+public interface CDAConstraint {
+
+	public static final String CLASSES_GROUP = Activator.PLUGIN_ID + ".classes.";
+	public static final String OCL_GROUP = Activator.PLUGIN_ID + ".ocl.";
+	public static final String ASSOCIATIONS_GROUP = Activator.PLUGIN_ID + ".associations.";
+
+
+}
