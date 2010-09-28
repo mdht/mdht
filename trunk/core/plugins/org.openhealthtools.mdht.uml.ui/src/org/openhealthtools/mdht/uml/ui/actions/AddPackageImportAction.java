2ff8b455673e1b642bafde6fde972e2d0cd35335
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-09-28 23:14:13 +0000

    Add PackageImport and ElementImport

---
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.ui/src/org/openhealthtools/mdht/uml/ui/actions/AddPackageImportAction.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (c) 2010 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.ui.actions;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.IUndoableOperation;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.workspace.AbstractEMFOperation;
+import org.eclipse.emf.workspace.IWorkspaceCommandStack;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.part.ISetSelectionTarget;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageImport;
+import org.eclipse.uml2.uml.UMLFactory;
+import org.openhealthtools.mdht.uml.common.ui.dialogs.DialogLaunchUtil;
+import org.openhealthtools.mdht.uml.ui.internal.Logger;
+import org.openhealthtools.mdht.uml.ui.internal.l10n.UML2UIMessages;
+
+public class AddPackageImportAction extends UML2AbstractAction {
+
+	public AddPackageImportAction() {
+		super();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		try {
+			final Element element = getSelectedElement();
+			if (element != null) {
+				IUndoableOperation operation = new AbstractEMFOperation(
+						editingDomain, UML2UIMessages.AddUMLPackageImport_operation_title) {
+				    protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) {
+						if (Package.class.isInstance(element)) {
+							Package umlPackage = (Package) element;
+							
+							// prompt for element
+							Package element = DialogLaunchUtil.choosePackage(
+									umlPackage.eResource().getResourceSet(), activePart.getSite().getShell());
+							if (element == null) {
+								return Status.CANCEL_STATUS;
+							}
+							
+							PackageImport packageImport = UMLFactory.eINSTANCE.createPackageImport();
+							packageImport.setImportingNamespace(umlPackage);
+							packageImport.setImportedPackage(element);
+
+							if (activePart instanceof ISetSelectionTarget) {
+								((ISetSelectionTarget)activePart).selectReveal(new StructuredSelection(umlPackage));
+								((ISetSelectionTarget)activePart).selectReveal(new StructuredSelection(packageImport));
+							}
+
+					        return Status.OK_STATUS;
+						}
+						
+						return Status.CANCEL_STATUS;
+				    }};
+
+			    try {
+					IWorkspaceCommandStack commandStack = (IWorkspaceCommandStack) editingDomain.getCommandStack();
+					operation.addContext(commandStack.getDefaultUndoContext());
+			        commandStack.getOperationHistory().execute(operation, new NullProgressMonitor(), activePart);
+
+			    } catch (ExecutionException ee) {
+			        Logger.logException(ee);
+			    }
+			}
+
+		} catch (Exception e) {
+			throw new RuntimeException(e.getCause());
+		}
+	}
+
+}
