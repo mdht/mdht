91f468b9c5afe9821db692a866ecd210b6cf9245
daford <daford@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-10-06 13:00:23 +0000

    Initial check-in

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/EncounterActivityOperationsTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.ihe.operations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class
+ */
+@SuppressWarnings("nls")
+public class EncounterActivityOperationsTest extends
+		EncounterEntryOperationsTest {
+
+	/**
+	 * Test method for
+	 * {@link org.openhealthtools.mdht.uml.cda.ihe.operations.EncounterActivityOperations#validateEncountersActivityTemplateId(org.openhealthtools.mdht.uml.cda.ihe.EncounterActivity, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
+	 * .
+	 */
+	@Test
+	public void testValidateEncountersActivityTemplateId() {
+		fail("Not yet implemented");
+	}
+
+} // EncounterActivityOperationsTest
