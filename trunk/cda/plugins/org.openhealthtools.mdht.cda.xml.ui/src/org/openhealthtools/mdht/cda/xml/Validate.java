c02170e3e544b910e41f9877445679e479e1e3f2
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2010-11-16 20:53:10 +0000

    Fixed os path issue

--- trunk/cda/plugins/org.openhealthtools.mdht.cda.xml.ui/src/org/openhealthtools/mdht/cda/xml/Validate.java
+++ trunk/cda/plugins/org.openhealthtools.mdht.cda.xml.ui/src/org/openhealthtools/mdht/cda/xml/Validate.java
@@ -2,17 +2,10 @@ package org.openhealthtools.mdht.cda.xml;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-import java.util.TimerTask;
-import java.util.zip.ZipFile;
 
 import org.apache.xerces.parsers.SAXParser;
 import org.eclipse.emf.common.util.Diagnostic;
@@ -33,48 +26,11 @@ public class Validate {
 
 		ClinicalDocument clinicalDocument;
 		
-//		System.out.println("CCCCCCCCCCCCCCCCCCCCCCccc");
-		
 		CDAUtil.loadPackages();
 		
-		final String PATH_SEPARATOR = System.getProperty("path.separator");
-
-		final String JAVA_CLASSPATH = System.getProperty("java.class.path");
-
-		StringTokenizer st = new StringTokenizer(JAVA_CLASSPATH, PATH_SEPARATOR);
-
-		while (st.hasMoreTokens()) {
-			
-			
-			
-			String path = st.nextToken();
-			
-//			System.out.println("PPPPPPPPPPPPPP"+path);
-			
-//			if (path.endsWith(".jar") || (path.endsWith(".zip"))) {
-//				try {
-////					processModelPlugin(new ZipFile(path));
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//					// If there is an issue loading the plugin jar - we let
-//					// normal processing continue
-//				}
-//			}
-		}
-		
-//		System.out.println("ddddddddddddddddddddCccc");
-
 		try {
 			
-//			System.out.println(args[0]);
-//			System.out.println(args[1]);
-//			System.out.println(args[2]);
-			
 			URI cdaURI = URI.createURI(args[0]);
-
-			URI output = URI.createURI(args[1]);
-			
-			URI lock = URI.createURI(args[2]);
 
 			if (cdaURI.isFile()) {
 
@@ -82,7 +38,7 @@ public class Validate {
 
 				inputSource.setEncoding("UTF-8");
 
-				OutputStream fout = new FileOutputStream(output.toFileString());
+				OutputStream fout = new FileOutputStream(args[1]);
 				OutputStream bout = new BufferedOutputStream(fout);
 				final OutputStreamWriter out = new OutputStreamWriter(bout);
 
@@ -100,8 +56,6 @@ public class Validate {
 				CDAUtil.validate(clinicalDocument, new BasicValidationHandler() {
 
 					public void handleError(Diagnostic diagnostic) {
-
-//						System.out.println(diagnostic.getMessage());
 
 						String path = "";
 						if (diagnostic.getData().size() > 0 && diagnostic.getData().get(0) instanceof EObject) {
@@ -128,8 +82,6 @@ public class Validate {
 					}
 
 					public void handleWarning(Diagnostic diagnostic) {
-
-//						System.out.println(diagnostic.getMessage());
 						
 						String path = "";
 						if (diagnostic.getData().size() > 0 && diagnostic.getData().get(0) instanceof EObject) {
@@ -155,13 +107,9 @@ public class Validate {
 				out.flush();
 				out.close();
 				
-//				System.out.println("foooo");
-
-				File lockFile = new File(lock.toFileString());
+				File lockFile = new File(args[2]);
 				
 				lockFile.delete();
-				
-//				lock.to
 
 			}
 
