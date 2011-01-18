fbf27c273932c641707eb2940e4fe8c2c49e6094
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-01-18 20:54:56 +0000

    add imported genmodels logic

--- trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.wizards/src/org/openhealthtools/mdht/uml/cda/wizards/NewC32PageOne.java
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.wizards/src/org/openhealthtools/mdht/uml/cda/wizards/NewC32PageOne.java
@@ -7,10 +7,8 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.uml2.uml.Type;
 
 
