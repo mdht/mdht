fcdc27f2af945bb30625eb1943af112c41b5564f
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-02-05 17:05:55 +0000

    Initial version of consolidated generated test project

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.consol.test/src/org/openhealthtools/mdht/uml/cda/consol/operations/AllCDTOperationsTests.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2010 IBM Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.consol.operations;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This class represents a suite of JUnit 4 test cases.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ GeneralHeaderConstraintsOperationsTest.class })
+	public class AllCDTOperationsTests {
+	// Nothing
+} // AllCDTOperationsTests
