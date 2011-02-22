ad7cf4a1c7d8091b41d0ee9e4b62584825ea3d8d
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-02-22 17:25:18 +0000

    refactored xml generate to add classpath of cda.core when running generate from workspace

--- trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.core/src/org/openhealthtools/mdht/uml/cda/core/internal/generate/Generate.java
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.core/src/org/openhealthtools/mdht/uml/cda/core/internal/generate/Generate.java
@@ -1,4 +1,4 @@
-package org.openhealthtools.mdht.uml.cda.ui.internal.generate;
+package org.openhealthtools.mdht.uml.cda.core.internal.generate;
 
 import java.io.PrintWriter;
 import java.util.Map;
@@ -18,8 +18,8 @@ import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.UMLPlugin;
 import org.eclipse.uml2.uml.resource.UMLResource;
 import org.openhealthtools.mdht.uml.cda.core.profile.CDAPackage;
+import org.openhealthtools.mdht.uml.cda.core.util.InstanceGenerator;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
-//import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 import org.openhealthtools.mdht.uml.common.util.UMLUtil;
 
 public class Generate {
