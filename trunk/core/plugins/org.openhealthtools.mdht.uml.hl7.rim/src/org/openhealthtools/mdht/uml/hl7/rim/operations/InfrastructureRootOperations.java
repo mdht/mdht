a3320a5173bdbba6e8cbc1f9c116f431be8307f3
leswestberg <leswestberg@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-02-25 21:32:58 +0000

    UserStory: artf2548    Description: Fixed an issue when using oclIsUndefined() against variables that are typed as Enumerations from the vocab model.  The issue is that the variable will always return that it is defined - whether it was set or not.   So we had to change our tactics and check this against the "IsSet" flags in java instead.  This checks in more of the fix for the "RIM" issues related to this user story.  It adds a new methods specific to ClassCode, TypeCode, MoodCode, ContextControlCode, and DeterminerCode.

--- trunk/core/plugins/org.openhealthtools.mdht.uml.hl7.rim/src/org/openhealthtools/mdht/uml/hl7/rim/operations/InfrastructureRootOperations.java
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.hl7.rim/src/org/openhealthtools/mdht/uml/hl7/rim/operations/InfrastructureRootOperations.java
@@ -7,10 +7,15 @@
 package org.openhealthtools.mdht.uml.hl7.rim.operations;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.ecore.OCL;
+import org.eclipse.ocl.expressions.OCLExpression;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CS;
 import org.openhealthtools.mdht.uml.hl7.datatypes.II;
 import org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot;
+import org.openhealthtools.mdht.uml.hl7.rim.RIMPackage;
 import org.openhealthtools.mdht.uml.hl7.vocab.NullFlavor;
 
 /**
@@ -29,12 +34,26 @@ import org.openhealthtools.mdht.uml.hl7.vocab.NullFlavor;
  *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isNullFlavorUndefined() <em>Is Null Flavor Undefined</em>}</li>
  *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#hasContent() <em>Has Content</em>}</li>
  *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isDefined(java.lang.String) <em>Is Defined</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isClassCodeDefined() <em>Is Class Code Defined</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isTypeCodeDefined() <em>Is Type Code Defined</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isContextControlCodeDefined() <em>Is Context Control Code Defined</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isDeterminerCodeDefined() <em>Is Determiner Code Defined</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.hl7.rim.InfrastructureRoot#isMoodCodeDefined() <em>Is Mood Code Defined</em>}</li>
  * </ul>
  * </p>
  *
  * @generated
  */
 public class InfrastructureRootOperations {
+	/**
+	 * The cached environment for evaluating OCL expressions.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	protected static final OCL EOCL_ENV = OCL.newInstance();
+
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -127,6 +146,221 @@ public class InfrastructureRootOperations {
 	 */
 	public static  boolean isDefined(InfrastructureRoot infrastructureRoot, String featureName) {
 		return (infrastructureRoot.eIsSet(infrastructureRoot.eClass().getEStructuralFeature(featureName)));
+	}
+
+	/**
+	 * The cached OCL expression body for the '{@link #isClassCodeDefined(InfrastructureRoot) <em>Is Class Code Defined</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isClassCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String IS_CLASS_CODE_DEFINED__EOCL_EXP = "self.isDefined('classCode')";
+	/**
+	 * The cached OCL query for the '{@link #isClassCodeDefined(InfrastructureRoot) <em>Is Class Code Defined</em>}' query operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isClassCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static OCLExpression<EClassifier> IS_CLASS_CODE_DEFINED__EOCL_QRY;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * self.isDefined('classCode')
+	 * @param infrastructureRoot The receiving '<em><b>Infrastructure Root</b></em>' model object.
+	 * <!-- end-model-doc -->
+	 * @generated
+	 */
+	public static  boolean isClassCodeDefined(InfrastructureRoot infrastructureRoot) {
+		if (IS_CLASS_CODE_DEFINED__EOCL_QRY == null) {
+			OCL.Helper helper = EOCL_ENV.createOCLHelper();
+			helper.setOperationContext(RIMPackage.Literals.INFRASTRUCTURE_ROOT, RIMPackage.Literals.INFRASTRUCTURE_ROOT.getEAllOperations().get(8));
+			try {
+				IS_CLASS_CODE_DEFINED__EOCL_QRY = helper.createQuery(IS_CLASS_CODE_DEFINED__EOCL_EXP);
+			}
+			catch (ParserException pe) {
+				throw new UnsupportedOperationException(pe.getLocalizedMessage());
+			}
+		}
+		OCL.Query query = EOCL_ENV.createQuery(IS_CLASS_CODE_DEFINED__EOCL_QRY);
+		return ((Boolean) query.evaluate(infrastructureRoot)).booleanValue();
+	}
+
+	/**
+	 * The cached OCL expression body for the '{@link #isTypeCodeDefined(InfrastructureRoot) <em>Is Type Code Defined</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isTypeCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String IS_TYPE_CODE_DEFINED__EOCL_EXP = "self.isDefined('typeCode')";
+	/**
+	 * The cached OCL query for the '{@link #isTypeCodeDefined(InfrastructureRoot) <em>Is Type Code Defined</em>}' query operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isTypeCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static OCLExpression<EClassifier> IS_TYPE_CODE_DEFINED__EOCL_QRY;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * self.isDefined('typeCode')
+	 * @param infrastructureRoot The receiving '<em><b>Infrastructure Root</b></em>' model object.
+	 * <!-- end-model-doc -->
+	 * @generated
+	 */
+	public static  boolean isTypeCodeDefined(InfrastructureRoot infrastructureRoot) {
+		if (IS_TYPE_CODE_DEFINED__EOCL_QRY == null) {
+			OCL.Helper helper = EOCL_ENV.createOCLHelper();
+			helper.setOperationContext(RIMPackage.Literals.INFRASTRUCTURE_ROOT, RIMPackage.Literals.INFRASTRUCTURE_ROOT.getEAllOperations().get(9));
+			try {
+				IS_TYPE_CODE_DEFINED__EOCL_QRY = helper.createQuery(IS_TYPE_CODE_DEFINED__EOCL_EXP);
+			}
+			catch (ParserException pe) {
+				throw new UnsupportedOperationException(pe.getLocalizedMessage());
+			}
+		}
+		OCL.Query query = EOCL_ENV.createQuery(IS_TYPE_CODE_DEFINED__EOCL_QRY);
+		return ((Boolean) query.evaluate(infrastructureRoot)).booleanValue();
+	}
+
+	/**
+	 * The cached OCL expression body for the '{@link #isContextControlCodeDefined(InfrastructureRoot) <em>Is Context Control Code Defined</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isContextControlCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String IS_CONTEXT_CONTROL_CODE_DEFINED__EOCL_EXP = "self.isDefined('contextControlCode')";
+	/**
+	 * The cached OCL query for the '{@link #isContextControlCodeDefined(InfrastructureRoot) <em>Is Context Control Code Defined</em>}' query operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isContextControlCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static OCLExpression<EClassifier> IS_CONTEXT_CONTROL_CODE_DEFINED__EOCL_QRY;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * self.isDefined('contextControlCode')
+	 * @param infrastructureRoot The receiving '<em><b>Infrastructure Root</b></em>' model object.
+	 * <!-- end-model-doc -->
+	 * @generated
+	 */
+	public static  boolean isContextControlCodeDefined(InfrastructureRoot infrastructureRoot) {
+		if (IS_CONTEXT_CONTROL_CODE_DEFINED__EOCL_QRY == null) {
+			OCL.Helper helper = EOCL_ENV.createOCLHelper();
+			helper.setOperationContext(RIMPackage.Literals.INFRASTRUCTURE_ROOT, RIMPackage.Literals.INFRASTRUCTURE_ROOT.getEAllOperations().get(10));
+			try {
+				IS_CONTEXT_CONTROL_CODE_DEFINED__EOCL_QRY = helper.createQuery(IS_CONTEXT_CONTROL_CODE_DEFINED__EOCL_EXP);
+			}
+			catch (ParserException pe) {
+				throw new UnsupportedOperationException(pe.getLocalizedMessage());
+			}
+		}
+		OCL.Query query = EOCL_ENV.createQuery(IS_CONTEXT_CONTROL_CODE_DEFINED__EOCL_QRY);
+		return ((Boolean) query.evaluate(infrastructureRoot)).booleanValue();
+	}
+
+	/**
+	 * The cached OCL expression body for the '{@link #isDeterminerCodeDefined(InfrastructureRoot) <em>Is Determiner Code Defined</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isDeterminerCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String IS_DETERMINER_CODE_DEFINED__EOCL_EXP = "self.isDefined('determinerCode')";
+	/**
+	 * The cached OCL query for the '{@link #isDeterminerCodeDefined(InfrastructureRoot) <em>Is Determiner Code Defined</em>}' query operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isDeterminerCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static OCLExpression<EClassifier> IS_DETERMINER_CODE_DEFINED__EOCL_QRY;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * self.isDefined('determinerCode')
+	 * @param infrastructureRoot The receiving '<em><b>Infrastructure Root</b></em>' model object.
+	 * <!-- end-model-doc -->
+	 * @generated
+	 */
+	public static  boolean isDeterminerCodeDefined(InfrastructureRoot infrastructureRoot) {
+		if (IS_DETERMINER_CODE_DEFINED__EOCL_QRY == null) {
+			OCL.Helper helper = EOCL_ENV.createOCLHelper();
+			helper.setOperationContext(RIMPackage.Literals.INFRASTRUCTURE_ROOT, RIMPackage.Literals.INFRASTRUCTURE_ROOT.getEAllOperations().get(11));
+			try {
+				IS_DETERMINER_CODE_DEFINED__EOCL_QRY = helper.createQuery(IS_DETERMINER_CODE_DEFINED__EOCL_EXP);
+			}
+			catch (ParserException pe) {
+				throw new UnsupportedOperationException(pe.getLocalizedMessage());
+			}
+		}
+		OCL.Query query = EOCL_ENV.createQuery(IS_DETERMINER_CODE_DEFINED__EOCL_QRY);
+		return ((Boolean) query.evaluate(infrastructureRoot)).booleanValue();
+	}
+
+	/**
+	 * The cached OCL expression body for the '{@link #isMoodCodeDefined(InfrastructureRoot) <em>Is Mood Code Defined</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isMoodCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String IS_MOOD_CODE_DEFINED__EOCL_EXP = "self.isDefined('moodCode')";
+	/**
+	 * The cached OCL query for the '{@link #isMoodCodeDefined(InfrastructureRoot) <em>Is Mood Code Defined</em>}' query operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #isMoodCodeDefined(InfrastructureRoot)
+	 * @generated
+	 * @ordered
+	 */
+	protected static OCLExpression<EClassifier> IS_MOOD_CODE_DEFINED__EOCL_QRY;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * self.isDefined('moodCode')
+	 * @param infrastructureRoot The receiving '<em><b>Infrastructure Root</b></em>' model object.
+	 * <!-- end-model-doc -->
+	 * @generated
+	 */
+	public static  boolean isMoodCodeDefined(InfrastructureRoot infrastructureRoot) {
+		if (IS_MOOD_CODE_DEFINED__EOCL_QRY == null) {
+			OCL.Helper helper = EOCL_ENV.createOCLHelper();
+			helper.setOperationContext(RIMPackage.Literals.INFRASTRUCTURE_ROOT, RIMPackage.Literals.INFRASTRUCTURE_ROOT.getEAllOperations().get(12));
+			try {
+				IS_MOOD_CODE_DEFINED__EOCL_QRY = helper.createQuery(IS_MOOD_CODE_DEFINED__EOCL_EXP);
+			}
+			catch (ParserException pe) {
+				throw new UnsupportedOperationException(pe.getLocalizedMessage());
+			}
+		}
+		OCL.Query query = EOCL_ENV.createQuery(IS_MOOD_CODE_DEFINED__EOCL_QRY);
+		return ((Boolean) query.evaluate(infrastructureRoot)).booleanValue();
 	}
 
 } // InfrastructureRootOperations
