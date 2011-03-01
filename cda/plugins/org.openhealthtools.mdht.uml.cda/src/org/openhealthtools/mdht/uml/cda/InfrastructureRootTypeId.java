f1141c4bd8fd6f5692f8afaf72c7b730e5e3dd59
leswestberg <leswestberg@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-03-01 20:55:29 +0000

    UserStory:artf2534  Author:Westberg Comments: Updated types within cda.uml to use the new uid and st datatype so that it can verify that instances of this field are in line with the regular expressions published in the normative specification.  Tested this both in valid and invalid cases using XML validation and making sure that the regular expression rules are being run and giving errors.

--- cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/InfrastructureRootTypeId.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/InfrastructureRootTypeId.java
@@ -30,9 +30,9 @@ import org.openhealthtools.mdht.uml.hl7.datatypes.II;
  *
  * @see org.openhealthtools.mdht.uml.cda.CDAPackage#getInfrastructureRootTypeId()
  * @model features="redefinedRoot redefinedExtension" 
- *        redefinedRootDefault="2.16.840.1.113883.1.3" redefinedRootUnsettable="true" redefinedRootDataType="org.eclipse.emf.ecore.EString" redefinedRootRequired="true" redefinedRootOrdered="false" redefinedRootSuppressedGetVisibility="true" redefinedRootSuppressedSetVisibility="true" redefinedRootSuppressedIsSetVisibility="true" redefinedRootSuppressedUnsetVisibility="true"
+ *        redefinedRootDefault="2.16.840.1.113883.1.3" redefinedRootUnsettable="true" redefinedRootDataType="org.openhealthtools.mdht.uml.hl7.datatypes.uid" redefinedRootRequired="true" redefinedRootOrdered="false" redefinedRootSuppressedGetVisibility="true" redefinedRootSuppressedSetVisibility="true" redefinedRootSuppressedIsSetVisibility="true" redefinedRootSuppressedUnsetVisibility="true"
  *        redefinedRootExtendedMetaData="name='root' kind='attribute'"
- *        redefinedExtensionDataType="org.eclipse.emf.ecore.EString" redefinedExtensionRequired="true" redefinedExtensionOrdered="false" redefinedExtensionSuppressedGetVisibility="true" redefinedExtensionSuppressedSetVisibility="true"
+ *        redefinedExtensionDataType="org.openhealthtools.mdht.uml.hl7.datatypes.stType" redefinedExtensionRequired="true" redefinedExtensionOrdered="false" redefinedExtensionSuppressedGetVisibility="true" redefinedExtensionSuppressedSetVisibility="true"
  *        redefinedExtensionExtendedMetaData="name='extension' kind='attribute'"
  * @generated
  */
@@ -50,4 +50,17 @@ public interface InfrastructureRootTypeId extends II {
 	 * @generated
 	 */
 	boolean validateRedefinedRootroot(DiagnosticChain diagnostics, Map<Object, Object> context);
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * not self.extension.oclIsUndefined()
+	 * @param diagnostics The chain of diagnostics to which problems are to be appended.
+	 * @param context The cache of context-specific information.
+	 * <!-- end-model-doc -->
+	 * @model annotation="http://www.eclipse.org/uml2/1.1.0/GenModel body='not self.extension.oclIsUndefined()'"
+	 * @generated
+	 */
+	boolean validateRedefinedExtension(DiagnosticChain diagnostics, Map<Object, Object> context);
 } // InfrastructureRootTypeId
--- cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/operations/InfrastructureRootTypeIdOperations.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda/src/org/openhealthtools/mdht/uml/cda/operations/InfrastructureRootTypeIdOperations.java
@@ -28,6 +28,7 @@ import org.openhealthtools.mdht.uml.hl7.datatypes.operations.ANYOperations;
  * The following operations are supported:
  * <ul>
  *   <li>{@link org.openhealthtools.mdht.uml.cda.InfrastructureRootTypeId#validateRedefinedRootroot(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Redefined Rootroot</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.InfrastructureRootTypeId#validateRedefinedExtension(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Redefined Extension</em>}</li>
  * </ul>
  * </p>
  *
@@ -102,6 +103,63 @@ public class InfrastructureRootTypeIdOperations extends ANYOperations {
 						 CDAValidator.DIAGNOSTIC_SOURCE,
 						 CDAValidator.INFRASTRUCTURE_ROOT_TYPE_ID__REDEFINED_ROOTROOT,
 						 org.eclipse.emf.ecore.plugin.EcorePlugin.INSTANCE.getString("_UI_GenericInvariant_diagnostic", new Object[] { "validateRedefinedRootroot", org.eclipse.emf.ecore.util.EObjectValidator.getObjectLabel(infrastructureRootTypeId, context) }),
+						 new Object [] { infrastructureRootTypeId }));
+			}
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * The cached OCL expression body for the '{@link #validateRedefinedExtension(InfrastructureRootTypeId, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Redefined Extension</em>}' operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #validateRedefinedExtension(InfrastructureRootTypeId, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String VALIDATE_REDEFINED_EXTENSION__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP = "not self.extension.oclIsUndefined()";
+
+	/**
+	 * The cached OCL invariant for the '{@link #validateRedefinedExtension(InfrastructureRootTypeId, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Redefined Extension</em>}' invariant operation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #validateRedefinedExtension(InfrastructureRootTypeId, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)
+	 * @generated
+	 * @ordered
+	 */
+	protected static Constraint VALIDATE_REDEFINED_EXTENSION__DIAGNOSTIC_CHAIN_MAP__EOCL_INV;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * not self.extension.oclIsUndefined()
+	 * @param infrastructureRootTypeId The receiving '<em><b>Infrastructure Root Type Id</b></em>' model object.
+	 * @param diagnostics The chain of diagnostics to which problems are to be appended.
+	 * @param context The cache of context-specific information.
+	 * <!-- end-model-doc -->
+	 * @generated
+	 */
+	public static  boolean validateRedefinedExtension(InfrastructureRootTypeId infrastructureRootTypeId, DiagnosticChain diagnostics, Map<Object, Object> context) {
+		if (VALIDATE_REDEFINED_EXTENSION__DIAGNOSTIC_CHAIN_MAP__EOCL_INV == null) {
+			OCL.Helper helper = EOCL_ENV.createOCLHelper();
+			helper.setContext(CDAPackage.Literals.INFRASTRUCTURE_ROOT_TYPE_ID);
+			try {
+				VALIDATE_REDEFINED_EXTENSION__DIAGNOSTIC_CHAIN_MAP__EOCL_INV = helper.createInvariant(VALIDATE_REDEFINED_EXTENSION__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP);
+			}
+			catch (ParserException pe) {
+				throw new UnsupportedOperationException(pe.getLocalizedMessage());
+			}
+		}
+		if (!EOCL_ENV.createQuery(VALIDATE_REDEFINED_EXTENSION__DIAGNOSTIC_CHAIN_MAP__EOCL_INV).check(infrastructureRootTypeId)) {
+			if (diagnostics != null) {
+				diagnostics.add
+					(new BasicDiagnostic
+						(Diagnostic.ERROR,
+						 CDAValidator.DIAGNOSTIC_SOURCE,
+						 CDAValidator.INFRASTRUCTURE_ROOT_TYPE_ID__REDEFINED_EXTENSION,
+						 org.eclipse.emf.ecore.plugin.EcorePlugin.INSTANCE.getString("_UI_GenericInvariant_diagnostic", new Object[] { "validateRedefinedExtension", org.eclipse.emf.ecore.util.EObjectValidator.getObjectLabel(infrastructureRootTypeId, context) }),
 						 new Object [] { infrastructureRootTypeId }));
 			}
 			return false;
