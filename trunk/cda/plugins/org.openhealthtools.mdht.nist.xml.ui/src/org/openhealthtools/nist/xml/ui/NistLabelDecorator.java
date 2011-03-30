e1ed3b1e5f43c4b4dfd0b4f58908b78bff3d6b25
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-03-30 19:40:39 +0000

    Updated to use embedded jars

---
+++ trunk/cda/plugins/org.openhealthtools.mdht.nist.xml.ui/src/org/openhealthtools/nist/xml/ui/NistLabelDecorator.java
@@ -0,0 +1,45 @@
+package org.openhealthtools.nist.xml.ui;
+
+import org.eclipse.jface.viewers.ILabelDecorator;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+
+public class NistLabelDecorator implements ILabelDecorator {
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Image decorateImage(Image image, Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String decorateText(String text, Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
