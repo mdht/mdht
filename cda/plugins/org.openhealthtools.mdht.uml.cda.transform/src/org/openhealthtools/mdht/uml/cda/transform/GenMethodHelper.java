cd40f19d6536f9d55a1e29222cd02243298e0f64
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-05-10 19:11:37 +0000

    Commit Kenn's patch for artf2652.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/GenMethodHelper.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/GenMethodHelper.java
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     Kenn Hussey - using annotations instead of expressions for Java bodies
  *     
  * $Id$
  *******************************************************************************/
@@ -14,6 +15,7 @@ package org.openhealthtools.mdht.uml.cda.transform;
 
 import org.eclipse.emf.common.util.BasicEList.UnmodifiableEList;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.Constraint;
@@ -29,17 +31,41 @@ import org.openhealthtools.mdht.uml.cda.core.util.CDAModelUtil;
 public class GenMethodHelper {
 	public static final String LF = System.getProperty("line.separator");
 
+	protected static String capitalize(String name) {
+		return name.substring(0, 1).toUpperCase() + name.substring(1);
+	}
+
+	protected static String pluralize(String name) {
+		if (name.endsWith("y")) {
+			return name.substring(0, name.length() - 1) + "ies";
+		}
+		if (name.endsWith("ia")) {
+			return name;
+		}
+		return name + "s";
+	}
+
 	protected EcoreTransformerOptions transformerOptions;
 
 	public GenMethodHelper(EcoreTransformerOptions options) {
 		this.transformerOptions = options;
 	}
 
-	public Operation genPropertyGetterOperation(Classifier classifier, Property property, Classifier domainType) {
-		if (property.getAssociation() != null) {
-			return genAssociationGetterOperation(classifier, property, domainType);
-		}
+	protected void addBodyAnnotation(Operation operation, String body) {
+		EcoreUtil.setAnnotation(operation, "http://www.eclipse.org/emf/2002/GenModel", "body", body);
+	}
 
+	protected void addBodyExpression(Operation operation, String language, String body) {
+		Constraint bodyConstraint = operation.createBodyCondition("body");
+		bodyConstraint.getConstrainedElements().add(operation);
+
+		OpaqueExpression bodyExpression = (OpaqueExpression) bodyConstraint.createSpecification(
+			null, null, UMLPackage.eINSTANCE.getOpaqueExpression());
+		bodyExpression.getLanguages().add(language);
+		bodyExpression.getBodies().add(body);
+	}
+
+	public Operation genAddOperation(Classifier classifier, Property property, Classifier domainType) {
 		Operation operation = null;
 		String businessName = null;
 		if (transformerOptions.isUseBusinessNames()) {
@@ -48,29 +74,18 @@ public class GenMethodHelper {
 			businessName = normalizeCodeName(property.getName());
 		}
 
-		String operationName = "get" + ((property.getUpper() == 1)
-				? capitalize(businessName)
-				: capitalize(pluralize(businessName)));
-		StringBuffer operationBody = new StringBuffer();
+		String operationName = "add" + capitalize(businessName);
 
+		String[] paramNamesArray = { "value" };
+		EList<String> parmNames = new UnmodifiableEList<String>(1, paramNamesArray);
+		Type[] paramTypesArray = { domainType };
+		EList<Type> parmTypes = new UnmodifiableEList<Type>(1, paramTypesArray);
+
+		// "fluent" API style, returns self
 		if (classifier instanceof Interface) {
-			operation = ((Interface) classifier).createOwnedOperation(operationName, null, null, domainType);
+			operation = ((Interface) classifier).createOwnedOperation(operationName, parmNames, parmTypes, classifier);
 		} else if (classifier instanceof Class) {
-			operation = ((Class) classifier).createOwnedOperation(operationName, null, null, domainType);
-		}
-
-		if (operation != null) {
-			operation.setIsQuery(true); // make this a query method
-			operation.setUpper(property.getUpper());
-
-			Property cdaProperty = CDAModelUtil.getCDAProperty(property);
-			if (cdaProperty != null) {
-				operationBody.append("self." + cdaProperty.getName());
-				operationBody.append(".oclAsType(" + domainType.getQualifiedName() + ")");
-
-				// create body constraint to implement the operation
-				addBodyExpression(operation, "OCL", operationBody.toString());
-			}
+			operation = ((Class) classifier).createOwnedOperation(operationName, parmNames, parmTypes, classifier);
 		}
 
 		return operation;
@@ -231,16 +246,20 @@ public class GenMethodHelper {
 				operationBody.append("return value;");
 			}
 
-			// create body constraint to implement the operation
+			// create body annotation to implement the operation
 			if (operationBody.length() > 0) {
-				addBodyExpression(operation, "Java", operationBody.toString());
+				addBodyAnnotation(operation, operationBody.toString());
 			}
 		}
 
 		return operation;
 	}
 
-	public Operation genAddOperation(Classifier classifier, Property property, Classifier domainType) {
+	public Operation genPropertyGetterOperation(Classifier classifier, Property property, Classifier domainType) {
+		if (property.getAssociation() != null) {
+			return genAssociationGetterOperation(classifier, property, domainType);
+		}
+
 		Operation operation = null;
 		String businessName = null;
 		if (transformerOptions.isUseBusinessNames()) {
@@ -249,18 +268,29 @@ public class GenMethodHelper {
 			businessName = normalizeCodeName(property.getName());
 		}
 
-		String operationName = "add" + capitalize(businessName);
+		String operationName = "get" + ((property.getUpper() == 1)
+				? capitalize(businessName)
+				: capitalize(pluralize(businessName)));
+		StringBuffer operationBody = new StringBuffer();
 
-		String[] paramNamesArray = { "value" };
-		EList<String> parmNames = new UnmodifiableEList<String>(1, paramNamesArray);
-		Type[] paramTypesArray = { domainType };
-		EList<Type> parmTypes = new UnmodifiableEList<Type>(1, paramTypesArray);
-
-		// "fluent" API style, returns self
 		if (classifier instanceof Interface) {
-			operation = ((Interface) classifier).createOwnedOperation(operationName, parmNames, parmTypes, classifier);
+			operation = ((Interface) classifier).createOwnedOperation(operationName, null, null, domainType);
 		} else if (classifier instanceof Class) {
-			operation = ((Class) classifier).createOwnedOperation(operationName, parmNames, parmTypes, classifier);
+			operation = ((Class) classifier).createOwnedOperation(operationName, null, null, domainType);
+		}
+
+		if (operation != null) {
+			operation.setIsQuery(true); // make this a query method
+			operation.setUpper(property.getUpper());
+
+			Property cdaProperty = CDAModelUtil.getCDAProperty(property);
+			if (cdaProperty != null) {
+				operationBody.append("self." + cdaProperty.getName());
+				operationBody.append(".oclAsType(" + domainType.getQualifiedName() + ")");
+
+				// create body constraint to implement the operation
+				addBodyExpression(operation, "OCL", operationBody.toString());
+			}
 		}
 
 		return operation;
@@ -336,26 +366,12 @@ public class GenMethodHelper {
 
 		operationBody.append("return this;");
 
-		// create body constraint to implement the operation
+		// create body annotation to implement the operation
 		if (operationBody.length() > 0) {
-			addBodyExpression(operation, "Java", operationBody.toString());
+			addBodyAnnotation(operation, operationBody.toString());
 		}
 
 		return operation;
-	}
-
-	protected static String pluralize(String name) {
-		if (name.endsWith("y")) {
-			return name.substring(0, name.length() - 1) + "ies";
-		}
-		if (name.endsWith("ia")) {
-			return name;
-		}
-		return name + "s";
-	}
-
-	protected static String capitalize(String name) {
-		return name.substring(0, 1).toUpperCase() + name.substring(1);
 	}
 
 	protected String normalizeCodeName(String name) {
@@ -365,16 +381,6 @@ public class GenMethodHelper {
 			result += part.substring(0, 1).toUpperCase() + part.substring(1);
 		}
 		return result;
-	}
-
-	protected void addBodyExpression(Operation operation, String language, String body) {
-		Constraint bodyConstraint = operation.createBodyCondition("body");
-		bodyConstraint.getConstrainedElements().add(operation);
-
-		OpaqueExpression bodyExpression = (OpaqueExpression) bodyConstraint.createSpecification(
-			null, null, UMLPackage.eINSTANCE.getOpaqueExpression());
-		bodyExpression.getLanguages().add(language);
-		bodyExpression.getBodies().add(body);
 	}
 
 }
