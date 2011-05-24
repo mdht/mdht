18b3155480052410d68db53eb019e39797d67816
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-05-24 17:14:31 +0000

    Code formatting and clean-up. Fix copyrights.

--- trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.ui.rsm/src/org/openhealthtools/mdht/uml/hdf/ui/rsm/preferences/PreferenceConstants.java
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.ui.rsm/src/org/openhealthtools/mdht/uml/hdf/ui/rsm/preferences/PreferenceConstants.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Sean Muir
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sean Muir (JKM Software) - initial API and implementation
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.hdf.ui.rsm.preferences;
 
 /**
@@ -12,5 +22,5 @@ public class PreferenceConstants {
 	public static final String P_CHOICE = "choicePreference";
 
 	public static final String P_STRING = "stringPreference";
-	
+
 }
--- trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.ui.rsm/src/org/openhealthtools/mdht/uml/hdf/ui/rsm/preferences/PreferenceInitializer.java
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.hdf.ui.rsm/src/org/openhealthtools/mdht/uml/hdf/ui/rsm/preferences/PreferenceInitializer.java
@@ -1,8 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Sean Muir
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sean Muir (JKM Software) - initial API and implementation
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.hdf.ui.rsm.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
-
 import org.openhealthtools.mdht.uml.hdf.ui.rsm.internal.Activator;
 
 /**
@@ -15,12 +24,12 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 * 
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
 	 */
+	@Override
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 		store.setDefault(PreferenceConstants.P_BOOLEAN, true);
 		store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
-		store.setDefault(PreferenceConstants.P_STRING,
-				"Default value");
+		store.setDefault(PreferenceConstants.P_STRING, "Default value");
 	}
 
 }
