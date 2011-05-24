994af3b6ad385e356ed7ab94b6f3355c8f505242
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-05-24 14:10:08 +0000

    Code formatting and clean-up. Fix copyrights.

--- trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.ui.rsm/src/org/openhealthtools/mdht/uml/cda/ui/rsm/providers/RSMCustomUMLParserProvider.java
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.ui.rsm/src/org/openhealthtools/mdht/uml/cda/ui/rsm/providers/RSMCustomUMLParserProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 David A Carlson.
+ * Copyright (c) 2006, 2009 David A Carlson
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,6 @@
  *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.ui.rsm.providers;
 
-import org.eclipse.gmf.runtime.common.ui.services.parser.IParser;
 
 /**
  * This class provides custom edit parsers for HL7 class diagrams.
@@ -20,13 +19,13 @@ import org.eclipse.gmf.runtime.common.ui.services.parser.IParser;
  * 
  * 
  */
-public class RSMCustomUMLParserProvider extends org.openhealthtools.mdht.uml.cda.ui.providers.CustomUMLParserProvider
-{
+public class RSMCustomUMLParserProvider extends org.openhealthtools.mdht.uml.cda.ui.providers.CustomUMLParserProvider {
 	// RSM Visual Property ID for UML Properties is actually empty string ""
 	final static String PROPERTYVISUALID = "";
-	
+
+	@Override
 	protected IParser getParser(String visualID) {
-	
+
 		IParser parser = null;
 		if (PROPERTYVISUALID.equals(visualID)) {
 			parser = getCustomPropertyParser();
