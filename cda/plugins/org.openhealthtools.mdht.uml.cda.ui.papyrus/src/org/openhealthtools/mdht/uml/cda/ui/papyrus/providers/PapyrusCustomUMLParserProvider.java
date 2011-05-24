12ac273e12d80c31e1d2d69ef20526e07be0c5b6
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-05-24 14:10:08 +0000

    Code formatting and clean-up. Fix copyrights.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.ui.papyrus/src/org/openhealthtools/mdht/uml/cda/ui/papyrus/providers/PapyrusCustomUMLParserProvider.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.ui.papyrus/src/org/openhealthtools/mdht/uml/cda/ui/papyrus/providers/PapyrusCustomUMLParserProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 David A Carlson.
+ * Copyright (c) 2006, 2009 David A Carlson
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,19 +15,19 @@ package org.openhealthtools.mdht.uml.cda.ui.papyrus.providers;
 import org.eclipse.gmf.runtime.common.ui.services.parser.IParser;
 
 /**
-* This class provides custom edit parsers for HL7 class diagrams.
+ * This class provides custom edit parsers for HL7 class diagrams.
  * It applies only to models with the HDF profile applied and within Papyrus environment.
  * 
- * Similar extension in CDA 
+ * Similar extension in CDA
  * 
  * 
  */
-public class PapyrusCustomUMLParserProvider extends org.openhealthtools.mdht.uml.cda.ui.providers.CustomUMLParserProvider
-{
+public class PapyrusCustomUMLParserProvider extends
+		org.openhealthtools.mdht.uml.cda.ui.providers.CustomUMLParserProvider {
 	final static String PROPERTYVISUALID = "3012";
-	
+
 	protected IParser getParser(String visualID) {
-	
+
 		IParser parser = null;
 		if (PROPERTYVISUALID.equals(visualID)) {
 			parser = getCustomPropertyParser();
