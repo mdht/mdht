75da2b82da00b57f80d6bc72af5bc7b182fcdd7a
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-05-25 12:53:03 +0000

    Add new test case

--- cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/DischargeSummaryOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/DischargeSummaryOperationsTest.java
@@ -10,11 +10,8 @@
  *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.BasicDiagnostic;
@@ -29,74 +26,126 @@ import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
 @SuppressWarnings("nls")
 public class DischargeSummaryOperationsTest extends MedicalSummaryOperationsTest {
 
-	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.1.4";
+	public static class OperationsForOCL extends DischargeSummaryOperations {
+		public String getOCLValue(String fieldName) {
 
-	private static final CDATestCase TEST_CASE_ARRAY[] = {
+			String oclValue = null;
 
-	// Template ID
-	new TemplateIDValidationTest(TEMPLATE_ID) {
-
-		@Override
-		protected boolean validate(final EObject objectToTest, final BasicDiagnostic diagnostician,
-				final Map<Object, Object> map) {
-			return DischargeSummaryOperations.validateDischargeSummaryTemplateId(
-				(DischargeSummary) objectToTest, diagnostician, map);
+			try {
+				oclValue = (String) this.getClass().getSuperclass().getDeclaredField(fieldName).get(this);
+			} catch (Exception e) {
+				oclValue = "NO OCL FOUND FOR PROPERTY " + fieldName;
+			}
+			return oclValue;
 		}
-	} };
-
-	@Override
-	protected List<CDATestCase> getTestCases() {
-		// Return a new List because the one returned by Arrays.asList is
-		// unmodifiable so a sub-class can't append their test cases.
-		final List<CDATestCase> retValue = super.getTestCases();
-		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
-		return retValue;
 	}
 
-	/**
-	 * @see org.openhealthtools.mdht.uml.cda.operations.MutualExclusionValidationTest#getObjectToTest()
-	 */
-	@Override
-	protected EObject getObjectToTest() {
-		return IHEFactory.eINSTANCE.createDischargeSummary();
+	private static OperationsForOCL operationsForOCL = new OperationsForOCL();
+
+	public class ObjectFactory implements TestObjectFactory<DischargeSummary> {
+		public DischargeSummary create() {
+			return IHEFactory.eINSTANCE.createDischargeSummary();
+		}
 	}
 
-	/**
-	 * @see org.openhealthtools.mdht.uml.cda.ihe.operations.MedicalDocumentOperationsTest#getObjectInitToTest()
-	 */
-	@Override
-	protected EObject getObjectInitToTest() {
-		return IHEFactory.eINSTANCE.createDischargeSummary().init();
-	}
+	ObjectFactory objectFactory = new ObjectFactory();
 
 	/**
-	 * Not a real test, needed for EMMA to report 100% method coverage.
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateDischargeSummaryTemplateId(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
 	 */
-	@Override
-	@SuppressWarnings("unused")
 	@Test
-	public void testConstructor() {
-		DischargeSummaryOperations obj = new DischargeSummaryOperations();
-		assertTrue(true);
-	} // testConstructor
+	public void testValidateDischargeSummaryTemplateId() {
+		OperationsTestCase<DischargeSummary> testCase = new OperationsTestCase<DischargeSummary>(
+			"ValidateDischargeSummaryTemplateId",
+			operationsForOCL.getOCLValue("VALIDATE_DISCHARGE_SUMMARY_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(DischargeSummary target) {
+
+			}
+
+			@Override
+			protected void updateToPass(DischargeSummary target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return DischargeSummaryOperations.validateDischargeSummaryTemplateId(
+					(DischargeSummary) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
+	}
 
 	/**
-	 * Test method for
-	 * {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateDischargeSummaryActiveProblemsSection(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
-	 * .
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateDischargeSummaryActiveProblemsSection(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
 	 */
 	@Test
 	public void testValidateDischargeSummaryActiveProblemsSection() {
-		fail("Not yet implemented");
+
+		OperationsTestCase<DischargeSummary> testCase = new OperationsTestCase<DischargeSummary>(
+			"ValidateDischargeSummaryActiveProblemsSection",
+			operationsForOCL.getOCLValue("VALIDATE_DISCHARGE_SUMMARY_ACTIVE_PROBLEMS_SECTION__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(DischargeSummary target) {
+
+			}
+
+			@Override
+			protected void updateToPass(DischargeSummary target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return DischargeSummaryOperations.validateDischargeSummaryActiveProblemsSection(
+					(DischargeSummary) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
+	}
+
+	@Test
+	public void testValidateGeneralHeaderConstraintsCodeDischargeSummaryDiagnosticChainMapOfObjectObject() {
+		OperationsTestCase<DischargeSummary> testCase = new OperationsTestCase<DischargeSummary>(
+			"testValidateGeneralHeaderConstraintsCodeDischargeSummary",
+			operationsForOCL.getOCLValue("VALIDATE_GENERAL_HEADER_CONSTRAINTS_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(DischargeSummary target) {
+
+			}
+
+			@Override
+			protected void updateToPass(DischargeSummary target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return DischargeSummaryOperations.validateGeneralHeaderConstraintsCode(
+					(DischargeSummary) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
 	}
 
 	/**
-	 * Test method for
-	 * {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#validateMedicalSummaryCode(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}
-	 * .
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.DischargeSummaryOperations#getActiveProblemsSection(org.openhealthtools.mdht.uml.cda.ihe.DischargeSummary)}.
 	 */
 	@Test
-	public void testValidateMedicalSummaryCodeDischargeSummaryDiagnosticChainMapOfObjectObject() {
+	public void testGetActiveProblemsSection() {
 		fail("Not yet implemented");
 	}
 
--- cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/VitalSignObservationOperationsTest.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ihe.test/src/org/openhealthtools/mdht/uml/cda/ihe/operations/VitalSignObservationOperationsTest.java
@@ -10,31 +10,14 @@
  *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.ihe.operations;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.BasicDiagnostic;
-import org.eclipse.emf.common.util.BasicEList;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.util.Diagnostician;
 import org.junit.Test;
-import org.openhealthtools.mdht.uml.cda.CDAFactory;
-import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
-import org.openhealthtools.mdht.uml.cda.Section;
-import org.openhealthtools.mdht.uml.cda.ccd.ResultObservation;
-import org.openhealthtools.mdht.uml.cda.ccd.operations.ResultObservationOperations;
 import org.openhealthtools.mdht.uml.cda.ccd.operations.ResultObservationOperationsTest;
 import org.openhealthtools.mdht.uml.cda.ihe.IHEFactory;
 import org.openhealthtools.mdht.uml.cda.ihe.VitalSignObservation;
-import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
-import org.openhealthtools.mdht.uml.hl7.datatypes.CD;
-import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
-import org.openhealthtools.mdht.uml.hl7.datatypes.PQ;
 
 /**
  * This class is a JUnit4 test case.
@@ -42,128 +25,183 @@ import org.openhealthtools.mdht.uml.hl7.datatypes.PQ;
 @SuppressWarnings("nls")
 public class VitalSignObservationOperationsTest extends ResultObservationOperationsTest {
 
-	protected static final String TEMPLATE_ID = "1.3.6.1.4.1.19376.1.5.3.1.4.13.2";
+	public static class OperationsForOCL extends VitalSignObservationOperations {
+		public String getOCLValue(String fieldName) {
 
-	protected static final String CODE = "11535-2"; // Not in OCL
+			String oclValue = null;
 
-	protected static final String CODE_SYSTEM = "2.16.840.1.113883.6.1";
-
-	private static final String TARGET_SITE_CODE = "whatever";
-
-	private static final String TARGET_SITE_CODE_CODE_SYSTEM = "whatever";
-
-	private static final CDATestCase TEST_CASE_ARRAY[] = {
-			// Template ID
-			// -------------------------------------------------------------
-			new TemplateIDValidationTest(TEMPLATE_ID) {
-
-				@Override
-				protected boolean validate(final EObject objectToTest, final BasicDiagnostic diagnostician,
-						final Map<Object, Object> map) {
-					return VitalSignObservationOperations.validateVitalSignObservationTemplateId(
-						(VitalSignObservation) objectToTest, diagnostician, map);
-				}
-
-			},
-
-			// Code
-			// -------------------------------------------------------------
-			new CodeCCDValidationTest(CODE, CODE_SYSTEM) {
-				@Override
-				protected boolean validate(final EObject objectToTest, final BasicDiagnostic diagnostician,
-						final Map<Object, Object> map) {
-					return VitalSignObservationOperations.validateResultObservationCode(
-						(VitalSignObservation) objectToTest, diagnostician, map);
-				}
-			},
-
-			// value
-			// -------------------------------------------------------------
-			new ObservationValueCCDValidationTest(CODE) {
-
-				@SuppressWarnings("synthetic-access")
-				@Override
-				public void doTest(final EObject objectToTest, final BasicDiagnostic diagnostician,
-						final Map<Object, Object> map) {
-					try {
-						validateExpectPass(objectToTest, diagnostician, map);
-						doSet(objectToTest);
-						validateExpectPass(objectToTest, diagnostician, map);
-					} catch (final UnsupportedOperationException uoe) {
-						fail(CDAValidationTest.createUnsupportedOperationFailureMessage(featureName, uoe));
-					}
-				}
-
-				@Override
-				protected boolean validate(final EObject objectToTest, final BasicDiagnostic diagnostician,
-						final Map<Object, Object> map) {
-					return VitalSignObservationOperations.validateVitalSignObservationValue(
-						(VitalSignObservation) objectToTest, diagnostician, map);
-				}
-
-				@Override
-				protected Object getValueToSet() {
-					final EList<PQ> retValue = new BasicEList<PQ>();
-					retValue.add(DatatypesFactory.eINSTANCE.createPQ());
-					return retValue;
-				}
-			},
-
-			// Information Source
-			// -------------------------------------------------------------
-			new InformationSourceCCDValidationTest() {
-				@Override
-				protected boolean validate(final EObject objectToTest, final BasicDiagnostic diagnostician,
-						final Map<Object, Object> map) {
-					return ResultObservationOperations.validateResultObservationInformationSource(
-						(ResultObservation) objectToTest, diagnostician, map);
-				}
+			try {
+				oclValue = (String) this.getClass().getSuperclass().getDeclaredField(fieldName).get(this);
+			} catch (Exception e) {
+				oclValue = "NO OCL FOUND FOR PROPERTY " + fieldName;
 			}
-
-	}; // TEST_CASE_ARRAY
-
-	@Override
-	protected List<CDATestCase> getTestCases() {
-		// Return a new List because the one returned by Arrays.asList is
-		// unmodifiable so a sub-class can't append their test cases.
-		final List<CDATestCase> retValue = super.getTestCases();
-		retValue.addAll(Arrays.asList(TEST_CASE_ARRAY));
-		return retValue;
+			return oclValue;
+		}
 	}
 
-	@Override
-	protected EObject getObjectToTest() {
-		ClinicalDocument clinicalDocument = CDAFactory.eINSTANCE.createClinicalDocument();
-		Section section = CDAFactory.eINSTANCE.createSection();
-		clinicalDocument.addSection(section);
-		VitalSignObservation vitalSignObservation = IHEFactory.eINSTANCE.createVitalSignObservation();
-		section.addObservation(vitalSignObservation);
-		return vitalSignObservation;
-		// return IHEFactory.eINSTANCE.createVitalSignObservation();
+	private static OperationsForOCL operationsForOCL = new OperationsForOCL();
+
+	public class ObjectFactory implements TestObjectFactory<VitalSignObservation> {
+		public VitalSignObservation create() {
+			return IHEFactory.eINSTANCE.createVitalSignObservation();
+		}
+	}
+
+	ObjectFactory objectFactory = new ObjectFactory();
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.VitalSignObservationOperations#validateVitalSignObservationTemplateId(org.openhealthtools.mdht.uml.cda.ihe.VitalSignObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateVitalSignObservationTemplateId() {
+		OperationsTestCase<VitalSignObservation> testCase = new OperationsTestCase<VitalSignObservation>(
+			"ValidateVitalSignObservationTemplateId",
+			operationsForOCL.getOCLValue("VALIDATE_VITAL_SIGN_OBSERVATION_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(VitalSignObservation target) {
+
+			}
+
+			@Override
+			protected void updateToPass(VitalSignObservation target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return VitalSignObservationOperations.validateVitalSignObservationTemplateId(
+					(VitalSignObservation) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
 	}
 
 	/**
-	 * JUnit test
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.VitalSignObservationOperations#validateVitalSignObservationValue(org.openhealthtools.mdht.uml.cda.ihe.VitalSignObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
 	 */
 	@Test
-	public void testVitalSignsObservationTargetSiteCode() {
-		final VitalSignObservation vso = (VitalSignObservation) getObjectToTest();
-		final BasicDiagnostic diagnostician = Diagnostician.INSTANCE.createDefaultDiagnostic(vso);
-		try {
-			boolean isValid = VitalSignObservationOperations.validateVitalSignObservationTargetSiteCode(
-				vso, diagnostician, map);
-			assertTrue(createAssertionFailureMessage(diagnostician), !isValid);
+	public void testValidateVitalSignObservationValue() {
+		OperationsTestCase<VitalSignObservation> testCase = new OperationsTestCase<VitalSignObservation>(
+			"ValidateVitalSignObservationValue",
+			operationsForOCL.getOCLValue("VALIDATE_VITAL_SIGN_OBSERVATION_VALUE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
 
-			final CD targetSiteCode = DatatypesFactory.eINSTANCE.createCE(
-				TARGET_SITE_CODE, TARGET_SITE_CODE_CODE_SYSTEM);
-			vso.getTargetSiteCodes().add(targetSiteCode);
+			@Override
+			protected void updateToFail(VitalSignObservation target) {
 
-			isValid = VitalSignObservationOperations.validateVitalSignObservationTargetSiteCode(vso, diagnostician, map);
-			assertTrue(createAssertionFailureMessage(diagnostician), isValid);
+			}
 
-		} catch (final UnsupportedOperationException uoe) {
-			fail(createUnsupportedOperationFailureMessage("TargetSiteCode", uoe));
-		}
+			@Override
+			protected void updateToPass(VitalSignObservation target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return VitalSignObservationOperations.validateVitalSignObservationValue(
+					(VitalSignObservation) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.VitalSignObservationOperations#validateVitalSignObservationInterpretationCode(org.openhealthtools.mdht.uml.cda.ihe.VitalSignObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateVitalSignObservationInterpretationCode() {
+		OperationsTestCase<VitalSignObservation> testCase = new OperationsTestCase<VitalSignObservation>(
+			"ValidateVitalSignObservationInterpretationCode",
+			operationsForOCL.getOCLValue("VALIDATE_VITAL_SIGN_OBSERVATION_INTERPRETATION_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(VitalSignObservation target) {
+
+			}
+
+			@Override
+			protected void updateToPass(VitalSignObservation target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return VitalSignObservationOperations.validateVitalSignObservationInterpretationCode(
+					(VitalSignObservation) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.VitalSignObservationOperations#validateVitalSignObservationMethodCode(org.openhealthtools.mdht.uml.cda.ihe.VitalSignObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateVitalSignObservationMethodCode() {
+		OperationsTestCase<VitalSignObservation> testCase = new OperationsTestCase<VitalSignObservation>(
+			"ValidateVitalSignObservationMethodCode",
+			operationsForOCL.getOCLValue("VALIDATE_VITAL_SIGN_OBSERVATION_METHOD_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(VitalSignObservation target) {
+
+			}
+
+			@Override
+			protected void updateToPass(VitalSignObservation target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return VitalSignObservationOperations.validateVitalSignObservationMethodCode(
+					(VitalSignObservation) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
+	}
+
+	/**
+	 * Test method for {@link org.openhealthtools.mdht.uml.cda.ihe.operations.VitalSignObservationOperations#validateVitalSignObservationTargetSiteCode(org.openhealthtools.mdht.uml.cda.ihe.VitalSignObservation, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map)}.
+	 */
+	@Test
+	public void testValidateVitalSignObservationTargetSiteCode() {
+		OperationsTestCase<VitalSignObservation> testCase = new OperationsTestCase<VitalSignObservation>(
+			"ValidateVitalSignObservationTargetSiteCode",
+			operationsForOCL.getOCLValue("VALIDATE_VITAL_SIGN_OBSERVATION_VALUE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(VitalSignObservation target) {
+
+			}
+
+			@Override
+			protected void updateToPass(VitalSignObservation target) {
+				target.init();
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+				return VitalSignObservationOperations.validateVitalSignObservationTargetSiteCode(
+					(VitalSignObservation) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		testCase.doValidationTest();
 	}
 
 } // VitalSignObservationOperationsTest
