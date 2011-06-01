668cb89fb9497b8abba52f4ef359acc3c9a0c2ec
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-06-01 21:53:06 +0000

    Commit Kenn Hussey's patch for artf2651.

--- trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/GenDomainInterface.java
+++ trunk/cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/GenDomainInterface.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 David A Carlson.
+ * Copyright (c) 2011 David A Carlson and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  * 
  * Contributors:
  *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     Kenn Hussey - eliminating duplicate comments from classes
  *     
  * $Id$
  *******************************************************************************/
@@ -37,6 +38,7 @@ public class GenDomainInterface extends TransformAbstract {
 		// copy comments for use in Javadoc
 		for (Comment comment : umlClass.getOwnedComments()) {
 			Comment clone = EcoreUtil.copy(comment);
+			clone.getAnnotatedElements().clear();
 			clone.getAnnotatedElements().add(domainInterface);
 			domainInterface.getOwnedComments().add(clone);
 		}
