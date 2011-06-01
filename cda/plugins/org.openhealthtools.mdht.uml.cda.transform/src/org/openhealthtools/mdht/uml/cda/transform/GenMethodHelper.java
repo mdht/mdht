d4954b5ba2d1f9d9da860fe7638193b5c2f4b0dd
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-06-01 21:53:06 +0000

    Commit Kenn Hussey's patch for artf2651.

--- cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/GenMethodHelper.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.transform/src/org/openhealthtools/mdht/uml/cda/transform/GenMethodHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 David A Carlson.
+ * Copyright (c) 2011 David A Carlson, IBM Corporation, and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *     David A Carlson (XMLmodeling.com) - initial API and implementation
  *     Kenn Hussey - using annotations instead of expressions for Java bodies
+ *     Kenn Hussey - fixing problems with generating facade for HITSP
  *     
  * $Id$
  *******************************************************************************/
@@ -27,6 +28,7 @@ import org.eclipse.uml2.uml.Stereotype;
 import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.openhealthtools.mdht.uml.cda.core.util.CDAModelUtil;
+import org.openhealthtools.mdht.uml.common.util.UMLUtil;
 
 public class GenMethodHelper {
 	public static final String LF = System.getProperty("line.separator");
@@ -36,6 +38,15 @@ public class GenMethodHelper {
 	}
 
 	protected static String pluralize(String name) {
+		if (name.endsWith("us")) {
+			return name.substring(0, name.length() - 2) + "i";
+		}
+		if (name.endsWith("ss")) {
+			return name + "es";
+		}
+		if (name.endsWith("s")) {
+			return name;
+		}
 		if (name.endsWith("y")) {
 			return name.substring(0, name.length() - 1) + "ies";
 		}
@@ -107,9 +118,9 @@ public class GenMethodHelper {
 		}
 
 		String operationName = "get";
-		// if (!UMLUtil.getRedefinedProperties(sourceProperty).isEmpty()) {
-		// operationName += CDAModelUtil.getModelPrefix(sourceProperty);
-		// }
+		if (!UMLUtil.getRedefinedProperties(sourceProperty).isEmpty()) {
+			operationName += CDAModelUtil.getModelPrefix(sourceProperty);
+		}
 		operationName += ((sourceProperty.getUpper() == 1)
 				? capitalize(businessName)
 				: capitalize(pluralize(businessName)));
@@ -120,9 +131,13 @@ public class GenMethodHelper {
 		String domainTypeQName = domainType.getQualifiedName();
 
 		Class cdaTargetClass = CDAModelUtil.getCDAClass(targetClass);
-		String cdaTargetName = cdaTargetClass.getName();
+		String cdaTargetName = cdaTargetClass == null
+				? targetClass.getName()
+				: cdaTargetClass.getName();
 		String cdaTargetLowerName = cdaTargetName.substring(0, 1).toLowerCase() + cdaTargetName.substring(1);
-		String cdaTargetQName = cdaTargetClass.getQualifiedName();
+		String cdaTargetQName = cdaTargetClass == null
+				? targetClass.getQualifiedName()
+				: cdaTargetClass.getQualifiedName();
 
 		StringBuffer operationBody = new StringBuffer();
 
@@ -214,7 +229,7 @@ public class GenMethodHelper {
 				operationBody.append("section.init();" + LF);
 
 				operationBody.append("addSection((org.openhealthtools.mdht.uml.cda.Section)section);" + LF);
-				operationBody.append("return (" + domainType.getName() + ")section;");
+				operationBody.append("return section;");
 			} else if ((CDAModelUtil.isSection(sourceClass) || CDAModelUtil.isClinicalStatement(sourceClass)) &&
 					CDAModelUtil.isClinicalStatement(cdaTargetClass)) {
 				operationBody.append(domainType.getName() + " clinicalStatement = " + "(" + domainType.getName() +
@@ -242,6 +257,16 @@ public class GenMethodHelper {
 					operationBody.append("set" + capitalize(cdaProperty.getName()) + "(value);" + LF);
 				} else {
 					operationBody.append("get" + capitalize(pluralize(cdaProperty.getName())) + "().add(value);" + LF);
+				}
+				operationBody.append("return value;");
+			} else {
+				operationBody.append(domainType.getName() + " value = " + "(" + domainType.getName() + ") eObject;" +
+						LF);
+
+				if (property.getUpper() == 1) {
+					operationBody.append("set" + capitalize(property.getName()) + "(value);" + LF);
+				} else {
+					operationBody.append("get" + capitalize(pluralize(property.getName())) + "().add(value);" + LF);
 				}
 				operationBody.append("return value;");
 			}
@@ -323,45 +348,33 @@ public class GenMethodHelper {
 		// create instance of appropriate domainType
 		Class sourceClass = property.getClass_();
 		Class cdaTargetClass = CDAModelUtil.getCDAClass((Classifier) property.getType());
-		String nsURI = null;
+		Property cdaProperty = CDAModelUtil.getCDAProperty(property);
 
-		if (CDAModelUtil.isDatatypeModel(property.getType())) {
-			operationBody.append("org.eclipse.emf.ecore.EPackage ePackage = org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesPackage.eINSTANCE;" +
-					LF);
-			operationBody.append("org.eclipse.emf.ecore.EFactory eFactory = org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory.eINSTANCE;" +
-					LF);
-		} else if (CDAModelUtil.isCDAModel(property.getType())) {
-			operationBody.append("org.eclipse.emf.ecore.EPackage ePackage = org.openhealthtools.mdht.uml.cda.CDAPackage.eINSTANCE;" +
-					LF);
-			operationBody.append("org.eclipse.emf.ecore.EFactory eFactory = org.openhealthtools.mdht.uml.cda.CDAFactory.eINSTANCE;" +
-					LF);
-		} else {
-			Stereotype ePackage = property.getType().getNearestPackage().getAppliedStereotype("Ecore::EPackage");
-			if (ePackage != null) {
-				nsURI = (String) property.getType().getNearestPackage().getValue(ePackage, "nsURI");
-			} else {
-				return null;
-			}
-
-			operationBody.append("org.eclipse.emf.ecore.EPackage ePackage = org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.getEPackage(\"" +
-					nsURI + "\");" + LF);
-			operationBody.append("org.eclipse.emf.ecore.EFactory eFactory = org.eclipse.emf.ecore.EPackage.Registry.INSTANCE.getEFactory(\"" +
-					nsURI + "\");" + LF);
-		}
-
-		operationBody.append("org.eclipse.emf.ecore.EClass eClass = (org.eclipse.emf.ecore.EClass) ePackage.getEClassifier(\"" +
-				property.getType().getName() + "\");" + LF);
-		operationBody.append("org.eclipse.emf.ecore.EObject eObject = eFactory.create(eClass);" + LF);
-
-		// TODO add to classifier
-
-		if (CDAModelUtil.isClinicalStatement(sourceClass) && CDAModelUtil.isClinicalStatement(cdaTargetClass)) {
-			operationBody.append(domainType.getName() + " clinicalStatement = " + "(" + domainType.getName() +
-					") eObject;" + LF);
-			operationBody.append("clinicalStatement.init();" + LF);
-
+		if ((CDAModelUtil.isClinicalDocument(sourceClass) || CDAModelUtil.isSection(sourceClass)) &&
+				CDAModelUtil.isSection(cdaTargetClass)) {
+			operationBody.append("addSection((org.openhealthtools.mdht.uml.cda.Section)value);" + LF);
+		} else if ((CDAModelUtil.isSection(sourceClass) || CDAModelUtil.isClinicalStatement(sourceClass)) &&
+				CDAModelUtil.isClinicalStatement(cdaTargetClass)) {
 			operationBody.append("add" + cdaTargetClass.getName() + "((org.openhealthtools.mdht.uml.cda." +
-					cdaTargetClass.getName() + ")clinicalStatement);" + LF);
+					cdaTargetClass.getName() + ")value);" + LF);
+		} else if (CDAModelUtil.isDatatypeModel(property.getType())) {
+			if (cdaProperty.getUpper() == 1) {
+				operationBody.append("set" + capitalize(cdaProperty.getName()) + "(value);" + LF);
+			} else {
+				operationBody.append("get" + capitalize(pluralize(cdaProperty.getName())) + "().add(value);" + LF);
+			}
+		} else if (CDAModelUtil.isCDAModel(property.getType())) {
+			if (cdaProperty.getUpper() == 1) {
+				operationBody.append("set" + capitalize(cdaProperty.getName()) + "(value);" + LF);
+			} else {
+				operationBody.append("get" + capitalize(pluralize(cdaProperty.getName())) + "().add(value);" + LF);
+			}
+		} else {
+			if (property.getUpper() == 1) {
+				operationBody.append("set" + capitalize(property.getName()) + "(value);" + LF);
+			} else {
+				operationBody.append("get" + capitalize(pluralize(property.getName())) + "().add(value);" + LF);
+			}
 		}
 
 		operationBody.append("return this;");
