d5c8d80695f31fabbbcaf159cc422166041233be
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-06-08 15:47:28 +0000

    initial version of CDT tests

---
+++ cda/tests/org.openhealthtools.mdht.uml.cda.cdt.tests/src/org/openhealthtools/mdht/uml/cda/cdt/tests/CDTTests.java
@@ -0,0 +1,44 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.cdt.tests;
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * <!-- begin-user-doc --> A test suite for the '<em><b>cdt</b></em>' package.
+ * <!-- end-user-doc -->
+ * 		
+
+ * @generated
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		GeneralHeaderConstraintsTest.class, HistoryAndPhysicalTest.class, HistoryOfPresentIllnessTest.class,
+		PastMedicalHistorySectionTest.class, ReviewOfSystemsSectionIHETest.class, PhysicalExaminationSectionTest.class,
+		VitalSignsSectionTest.class, GeneralStatusSectionTest.class, DiagnosticFindingsTest.class,
+		ConsultationNoteTest.class, PastMedicalHistorySectionConsultTest.class, ReviewOfSystemsSectionTest.class,
+		ReasonForReferralSectionTest.class, ReasonForVisitSectionConsultTest.class, LevelOneConformanceTest.class,
+		LevelTwoConformanceTest.class, LevelThreeConformanceTest.class, AssessmentSectionTest.class,
+		PlanSectionTest.class, AssessmentAndPlanSectionTest.class, ReasonForVisitSectionTest.class,
+		ChiefComplaintSectionTest.class, ReasonForVisitAndChiefComplaintSectionTest.class, ProgressNoteTest.class,
+		AssessmentAndPlanSectionProcNoteTest.class, AssessmentSectionProcNoteTest.class,
+		ChiefComplaintSectionProcNoteTest.class, ObjectiveSectionTest.class, SubjectiveSectionTest.class,
+		CDTRegistryDelegateTest.class })
+public class CDTTests {
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(CDTTests.class);
+	}
+
+} // CDTTests
