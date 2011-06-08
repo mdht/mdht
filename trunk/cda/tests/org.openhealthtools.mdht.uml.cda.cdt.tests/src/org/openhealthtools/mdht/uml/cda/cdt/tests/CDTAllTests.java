9815442126706094390bdd717044c4f340c7c5a3
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-06-08 15:47:28 +0000

    initial version of CDT tests

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.cdt.tests/src/org/openhealthtools/mdht/uml/cda/cdt/tests/CDTAllTests.java
@@ -0,0 +1,46 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.cdt.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc --> A test suite for the '<em><b>CDT</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class CDTAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new CDTAllTests("CDT Tests");
+		suite.addTest(CDTTests.suite());
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CDTAllTests(String name) {
+		super(name);
+	}
+
+} // CDTAllTests
