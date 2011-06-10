5894ff92889d18e6df8a8309bbc41c79f8318803
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-06-10 18:04:04 +0000

    Code formatting and clean up. Fix copyrights.

--- cda/tests/org.openhealthtools.mdht.uml.cda.ccd.tests/src/org/openhealthtools/mdht/uml/cda/ccd/tests/CCDExample.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.ccd.tests/src/org/openhealthtools/mdht/uml/cda/ccd/tests/CCDExample.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/*******************************************************************************
+ * Copyright (c) 2011 Sean Muir and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *     Sean Muir (JKM Software) - initial API and implementation
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.ccd.tests;
 
 import java.io.File;
