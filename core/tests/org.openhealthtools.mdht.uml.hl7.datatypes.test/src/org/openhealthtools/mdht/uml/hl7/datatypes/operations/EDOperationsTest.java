3b33c6c79c4bad59f936a7f9def2782785ba7616
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-11-21 18:24:14 +0000

    Added matches test

--- core/tests/org.openhealthtools.mdht.uml.hl7.datatypes.test/src/org/openhealthtools/mdht/uml/hl7/datatypes/operations/EDOperationsTest.java
+++ core/tests/org.openhealthtools.mdht.uml.hl7.datatypes.test/src/org/openhealthtools/mdht/uml/hl7/datatypes/operations/EDOperationsTest.java
@@ -12,6 +12,7 @@
  */
 package org.openhealthtools.mdht.uml.hl7.datatypes.operations;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.eclipse.emf.ecore.EObject;
@@ -42,7 +43,52 @@ public class EDOperationsTest extends DatatypesOperationsTest {
 		EDOperations obj = new EDOperations();
 		assertTrue(true);
 	} // testConstructor
-	
+
+	@Test
+	public final void testMatches() {
+		@SuppressWarnings("unused")
+		ED ed = (ED) getObjectToTest();
+
+		ed.addText("true");
+
+		assertTrue(EDOperations.matches(ed, "[Tt]rue"));
+
+		ed = (ED) getObjectToTest();
+
+		ed.addText("false");
+
+		assertFalse(EDOperations.matches(ed, "[Tt]rue"));
+
+		ed = (ED) getObjectToTest();
+
+		ed.addText("1.3.6.1.4.1.19376.1.5.3.1.1.11.2.3.2");
+
+		assertTrue(EDOperations.matches(ed, "([0-2])(.([1-9][0-9]*|0))+"));
+
+		ed = (ED) getObjectToTest();
+
+		ed.addText("cfbf1712-5a5f-4948-bb97-da2bad618223");
+
+		assertTrue(EDOperations.matches(ed, "([A-Fa-f0-9]{8})(\\-[A-Fa-f0-9]{4}){3}(\\-[A-Fa-f0-9]{12})"));
+
+		ed.setLanguage("true");
+		assertTrue(ANYOperations.matches(ed, "language", "[Tt]rue"));
+
+		ed.setLanguage("1.3.6.1.4.1.19376.1.5.3.1.1.11.2.3.2");
+		assertTrue(ANYOperations.matches(ed, "language", "([0-2])(.([1-9][0-9]*|0))+"));
+
+		ed.setLanguage("cfbf1712-5a5f-4948-bb97-da2bad618223");
+		assertTrue(ANYOperations.matches(ed, "language", "([A-Fa-f0-9]{8})(\\-[A-Fa-f0-9]{4}){3}(\\-[A-Fa-f0-9]{12})"));
+
+		ed.setLanguage("shouldbefalse");
+		assertFalse(ANYOperations.matches(ed, "language", "[Tt]rue"));
+
+		assertFalse(ANYOperations.matches(ed, "language", "([0-2])(.([1-9][0-9]*|0))+"));
+
+		assertFalse(ANYOperations.matches(ed, "language", "([A-Fa-f0-9]{8})(\\-[A-Fa-f0-9]{4}){3}(\\-[A-Fa-f0-9]{12})"));
+
+	}
+
 	/**
 	 * test addText and getText
 	 */
