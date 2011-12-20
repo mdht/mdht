c989df51a347e5d166319038652a9d047f1d8077
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2011-12-20 20:00:30 +0000

    Artifact artf3002 : Errors when deleting template class from a controlled resource

--- core/plugins/org.openhealthtools.mdht.uml.ui.navigator/src/org/openhealthtools/mdht/uml/ui/navigator/actions/DeleteUMLAction.java
+++ core/plugins/org.openhealthtools.mdht.uml.ui.navigator/src/org/openhealthtools/mdht/uml/ui/navigator/actions/DeleteUMLAction.java
@@ -16,56 +16,99 @@ import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.edit.command.DeleteCommand;
-import org.eclipse.emf.edit.command.RemoveCommand;
+import org.eclipse.emf.common.command.IdentityCommand;
+import org.eclipse.emf.common.command.UnexecutableCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.emf.edit.ui.action.DeleteAction;
+import org.eclipse.emf.edit.domain.IEditingDomainProvider;
+import org.eclipse.emf.edit.ui.EMFEditUIPlugin;
+import org.eclipse.emf.edit.ui.action.CommandActionHandler;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.uml2.common.edit.command.ChangeCommand;
 import org.eclipse.uml2.uml.Association;
 import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.Property;
+import org.openhealthtools.mdht.uml.common.ui.saveable.ModelDocument;
+import org.openhealthtools.mdht.uml.common.ui.saveable.ModelManager;
 
-public class DeleteUMLAction extends DeleteAction {
+public class DeleteUMLAction extends CommandActionHandler {
 
-	public DeleteUMLAction(EditingDomain domain, boolean removeAllReferences) {
-		super(domain, removeAllReferences);
-	}
+	protected List<Element> elements;
 
 	public DeleteUMLAction(EditingDomain domain) {
-		super(domain);
+		super(domain, EMFEditUIPlugin.INSTANCE.getString("_UI_Delete_menu_item"));
 	}
 
-	public DeleteUMLAction(boolean removeAllReferences) {
-		super(removeAllReferences);
+	public DeleteUMLAction(EditingDomain domain, String label) {
+		super(domain, label);
 	}
 
-	public DeleteUMLAction() {
-		super();
+	public void setActiveWorkbenchPart(IWorkbenchPart workbenchPart) {
+		if (workbenchPart instanceof IEditingDomainProvider) {
+			domain = ((IEditingDomainProvider) workbenchPart).getEditingDomain();
+		}
 	}
 
 	@Override
 	public Command createCommand(Collection<?> selection) {
-		List<Object> elements = new ArrayList<Object>();
+		elements = new ArrayList<Element>();
 		for (Object object : selection) {
-			if (object instanceof Class) {
-				Class umlClass = (Class) object;
-				for (Property property : umlClass.getOwnedAttributes()) {
-					if (property.getAssociation() != null) {
-						elements.add(property.getAssociation());
+			if (object instanceof Element && ((Element) object).getOwner() != null) {
+				if (object instanceof Class) {
+					Class umlClass = (Class) object;
+					for (Property property : umlClass.getOwnedAttributes()) {
+						if (property.getAssociation() != null) {
+							elements.add(property.getAssociation());
+						}
 					}
+					elements.add(umlClass);
+				} else if (object instanceof Association) {
+					Association association = (Association) object;
+					elements.addAll(association.getMemberEnds());
+					elements.add(association);
+				} else {
+					elements.add((Element) object);
 				}
-				elements.add(umlClass);
-			} else if (object instanceof Association) {
-				Association association = (Association) object;
-				elements.addAll(association.getMemberEnds());
-				elements.add(association);
-			} else {
-				elements.add(object);
 			}
 		}
 
-		return removeAllReferences
-				? DeleteCommand.create(domain, elements)
-				: RemoveCommand.create(domain, elements);
+		if (elements.size() > 0) {
+			return IdentityCommand.INSTANCE;
+		}
+
+		return UnexecutableCommand.INSTANCE;
+	}
+
+	@Override
+	public void run() {
+
+		if (command != UnexecutableCommand.INSTANCE) {
+			String label = elements.get(0) instanceof NamedElement &&
+					((NamedElement) elements.get(0)).getName() != null
+					? "Delete " + ((NamedElement) elements.get(0)).getName()
+					: "Delete Element";
+			if (elements.size() > 1) {
+				label += " (+" + (elements.size() - 1) + ")";
+			}
+
+			domain.getCommandStack().execute(new ChangeCommand(domain, new Runnable() {
+
+				public void run() {
+					for (Element element : elements) {
+						if (element.eResource() != null) {
+							// mark resource dirty so that it can be saved
+							ModelDocument sourceDocument = ModelManager.getManager().manage(element.eResource());
+							if (sourceDocument != null) {
+								sourceDocument.setDirty(true);
+							}
+						}
+
+						element.destroy();
+					}
+				}
+			}, label));
+		}
 	}
 
 }
