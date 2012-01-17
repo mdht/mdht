745a5fff1ed11d976fc4474693ecd9263a640991
rama.ramakrishnan <rama.ramakrishnan@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-01-17 04:18:34 +0000

    Created the tests  project for ToC with consolidated cda sections

---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.consoltoc.tests/src/org/openhealthtools/mdht/uml/cda/consoltoc/tests/ConsoltocTests.java
@@ -0,0 +1,41 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.consoltoc.tests;
+
+
+import junit.framework.JUnit4TestAdapter;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>consoltoc</b></em>' package.
+ * <!-- end-user-doc -->
+ * 		
+
+ * @generated
+ */
+ @RunWith(Suite.class)
+@Suite.SuiteClasses({
+  	DischargeSummaryTest.class
+  	,ConsultationRequestTest.class
+  	,ConsultationSummaryTest.class
+  	,DischargeInstructionsTest.class
+})
+public class ConsoltocTests {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter( ConsoltocTests.class);
+	}
+	 
+
+} //ConsoltocTests
---
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.consoltoc.tests/src/org/openhealthtools/mdht/uml/cda/consoltoc/tests/DischargeSummaryTest.java
@@ -0,0 +1,669 @@
+
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.consoltoc.tests;
+
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.consoltoc.ConsoltocFactory;
+import org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary;
+import org.openhealthtools.mdht.uml.cda.consoltoc.operations.DischargeSummaryOperations;
+import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A static utility class that provides operations related to '<em><b>Discharge Summary</b></em>' model objects.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following operations are supported:
+ * <ul>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getAllergiesSectionEntriesOptional() <em>Get Allergies Section Entries Optional</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getHospitalCourseSection() <em>Get Hospital Course Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getHospitalDischargeDiagnosisSection() <em>Get Hospital Discharge Diagnosis Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getHospitalDischargeMedicationsSectionEntriesOptional() <em>Get Hospital Discharge Medications Section Entries Optional</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getPlanOfCareSection() <em>Get Plan Of Care Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getProblemSectionEntriesOptional() <em>Get Problem Section Entries Optional</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getChiefComplaintSection() <em>Get Chief Complaint Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getChiefComplaintAndReasonForVisitSection() <em>Get Chief Complaint And Reason For Visit Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getDischargeDietSection() <em>Get Discharge Diet Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getFamilyHistorySection() <em>Get Family History Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getFunctionalStatusSection() <em>Get Functional Status Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getHistoryOfPresentIllnessSection() <em>Get History Of Present Illness Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getHospitalDischargePhysicalSection() <em>Get Hospital Discharge Physical Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getHospitalDischargeStudiesSummarySection() <em>Get Hospital Discharge Studies Summary Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getProceduresSectionEntriesOptional() <em>Get Procedures Section Entries Optional</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getReasonForVisitSection() <em>Get Reason For Visit Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getReviewOfSystemsSection() <em>Get Review Of Systems Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getSocialHistorySection() <em>Get Social History Section</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getVitalSignsSectionEntriesOptional() <em>Get Vital Signs Section Entries Optional</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#getImmunizationsSectionEntriesOptional() <em>Get Immunizations Section Entries Optional</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consoltoc.DischargeSummary#validateGeneralHeaderConstraintsTemplateId(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate General Header Constraints Template Id</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+ 
+public class DischargeSummaryTest extends  CDAValidationTest {
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetAllergiesSectionEntriesOptional() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getAllergiesSectionEntriesOptional();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetHospitalCourseSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getHospitalCourseSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetHospitalDischargeDiagnosisSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getHospitalDischargeDiagnosisSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetHospitalDischargeMedicationsSectionEntriesOptional() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getHospitalDischargeMedicationsSectionEntriesOptional();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetPlanOfCareSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getPlanOfCareSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetProblemSectionEntriesOptional() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getProblemSectionEntriesOptional();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetChiefComplaintSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getChiefComplaintSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetChiefComplaintAndReasonForVisitSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getChiefComplaintAndReasonForVisitSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetDischargeDietSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getDischargeDietSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetFamilyHistorySection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getFamilyHistorySection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetFunctionalStatusSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getFunctionalStatusSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetHistoryOfPresentIllnessSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getHistoryOfPresentIllnessSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetHospitalDischargePhysicalSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getHospitalDischargePhysicalSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetHospitalDischargeStudiesSummarySection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getHospitalDischargeStudiesSummarySection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetProceduresSectionEntriesOptional() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getProceduresSectionEntriesOptional();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetReasonForVisitSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getReasonForVisitSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetReviewOfSystemsSection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getReviewOfSystemsSection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetSocialHistorySection() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getSocialHistorySection();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetVitalSignsSectionEntriesOptional() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getVitalSignsSectionEntriesOptional();
+
+
+
+}
+
+
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testGetImmunizationsSectionEntriesOptional() {
+
+
+DischargeSummary target = objectFactory.create();
+target.getImmunizationsSectionEntriesOptional();
+
+
+
+}
+
+
+
+
+/**
+*
+* @generated
+*/
+@Test
+
+ 
+									
+public void testValidateGeneralHeaderConstraintsTemplateId() {
+			OperationsTestCase<DischargeSummary> validateGeneralHeaderConstraintsTemplateIdTestCase = new OperationsTestCase<DischargeSummary>(
+			"validateGeneralHeaderConstraintsTemplateId",
+			operationsForOCL.getOCLValue("VALIDATE_GENERAL_HEADER_CONSTRAINTS_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP")
+			,objectFactory) {
+
+			@Override
+			protected void updateToFail(DischargeSummary target) {
+
+			}
+
+			@Override
+			protected void updateToPass(DischargeSummary target) {
+				target.init();
+				
+				
+				
+				
+				
+
+				
+				
+
+	
+			
+
+	
+		
+		
+			
+				
+		
+			
+				
+			
+				
+				
+				
+				
+				
+				
+				
+			
+			
+					
+			
+				
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+			
+			
+			
+				return DischargeSummaryOperations.validateGeneralHeaderConstraintsTemplateId(
+					(DischargeSummary) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateGeneralHeaderConstraintsTemplateIdTestCase.doValidationTest();
+}		
+
+
+
+
+/**
+*
+* @generated
+*/
+	private static class OperationsForOCL extends DischargeSummaryOperations {
+		public String getOCLValue(String fieldName) {
+
+			String oclValue = null;
+
+			try {
+				oclValue = (String) this.getClass().getSuperclass().getDeclaredField(fieldName).get(this);
+			} catch (Exception e) {
+				oclValue = "NO OCL FOUND FOR PROPERTY " + fieldName;
+			}
+			return oclValue;
+		}
+	}
+	
+/**
+*
+* @generated
+*/
+	private static class ObjectFactory implements TestObjectFactory<DischargeSummary> {
+		@Override
+		public DischargeSummary create() {		
+			return ConsoltocFactory.eINSTANCE.createDischargeSummary();
+		}
+	}
+
+
+/**
+*
+* @generated
+*/
+	private static OperationsForOCL operationsForOCL = new OperationsForOCL();
+
+/**
+*
+* @generated
+*/
+	private static ObjectFactory objectFactory = new ObjectFactory();
+
+
+		/**
+* Tests Operations Constructor for 100% coverage
+* @generated
+*/	
+	private static class ConstructorTestClass extends DischargeSummaryOperations {};
+	
+		/**
+* Tests Operations Constructor for 100% coverage
+* @generated
+*/	
+		@Test
+	public void testConstructor() {
+	        @SuppressWarnings("unused")
+			ConstructorTestClass constructorTestClass = new ConstructorTestClass();		
+	} // testConstructor
+	
+	
+	
+	/**
+*
+* @generated
+*/
+	@Override
+	protected EObject getObjectToTest() {		
+		return null;
+	}
+	
+	
+
+	
+	
+} // DischargeSummaryOperations
