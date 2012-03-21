af13a324ed75f2effabf3803d860c39f211094c3
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-03-21 17:36:50 +0000

    Initial import, extracted and refactored from uml.cda.transform

---
+++ core/plugins/org.openhealthtools.mdht.uml.transform.ui/src/org/openhealthtools/mdht/uml/transform/ui/actions/FlattenModelAction.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2012 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.transform.ui.actions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.IUndoableOperation;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.workspace.AbstractEMFOperation;
+import org.eclipse.emf.workspace.IWorkspaceCommandStack;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.uml2.uml.Element;
+import org.openhealthtools.mdht.uml.transform.FlattenTransformer;
+import org.openhealthtools.mdht.uml.transform.TransformerOptions;
+
+public class FlattenModelAction extends UML2AbstractAction {
+
+	public FlattenModelAction() {
+		super();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		boolean includeBaseModel = true;
+		boolean useBusinessNames = true;
+		String outputModelPath = null;
+		String vocabModelPath = null;
+
+		try {
+			final List<Element> elements = getSelectedElements();
+			if (elements.isEmpty()) {
+				return;
+			}
+
+			TransformerOptions options = new TransformerOptions();
+			options.setIncludeBaseModel(includeBaseModel);
+			options.setUseBusinessNames(useBusinessNames);
+
+			options.setOutputModelPath(outputModelPath);
+			options.setVocabModelPath(vocabModelPath);
+
+			final FlattenTransformer transformer = new FlattenTransformer(options);
+
+			IUndoableOperation operation = new AbstractEMFOperation(editingDomain, "Flatten Model") {
+				@Override
+				protected IStatus doExecute(IProgressMonitor monitor, IAdaptable info) {
+
+					transformer.initialize(elements.get(0).getNearestPackage());
+					for (Element element : elements) {
+						transformer.transformModelElement(element);
+					}
+
+					transformer.saveResources();
+
+					return Status.OK_STATUS;
+				}
+			};
+
+			try {
+				IWorkspaceCommandStack commandStack = (IWorkspaceCommandStack) editingDomain.getCommandStack();
+				operation.addContext(commandStack.getDefaultUndoContext());
+				commandStack.getOperationHistory().execute(operation, new NullProgressMonitor(), activePart);
+
+			} catch (ExecutionException ee) {
+				throw new RuntimeException(ee.getCause());
+			}
+
+			if (transformer.getFlattenedPackage() != null) {
+				Map<String, String> saveOptions = new HashMap<String, String>();
+				transformer.getFlattenedPackage().eResource().save(saveOptions);
+			}
+			if (transformer.getFlattenedVocabPackage() != null) {
+				Map<String, String> saveOptions = new HashMap<String, String>();
+				transformer.getFlattenedVocabPackage().eResource().save(saveOptions);
+			}
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
---
+++ core/plugins/org.openhealthtools.mdht.uml.transform.ui/src/org/openhealthtools/mdht/uml/transform/ui/actions/UML2AbstractAction.java
@@ -0,0 +1,216 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2009 David A Carlson.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ * $Id$
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.transform.ui.actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.openhealthtools.mdht.uml.common.ui.util.EditingDomainUtil;
+
+public abstract class UML2AbstractAction implements IObjectActionDelegate, IViewActionDelegate, IEditorActionDelegate {
+
+	protected IWorkbenchPart activePart;
+
+	protected ISelection currentSelection;
+
+	protected TransactionalEditingDomain editingDomain;
+
+	public UML2AbstractAction() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IViewActionDelegate#init(org.eclipse.ui.IViewPart)
+	 */
+	public void init(IViewPart view) {
+		activePart = view;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
+	 */
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		activePart = targetEditor;
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		activePart = targetPart;
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		currentSelection = selection;
+
+		editingDomain = EditingDomainUtil.getEditingDomain(((IStructuredSelection) selection).toList());
+		action.setEnabled(editingDomain != null);
+	}
+
+	protected boolean isReadOnly() {
+		if (editingDomain != null) {
+			for (Object selected : ((IStructuredSelection) currentSelection).toArray()) {
+				Element element = getElementFromSelection(selected);
+				if (element != null && editingDomain.isReadOnly(element.eResource())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	protected View getSelectedView() {
+		if (editingDomain != null) {
+			for (Object selected : ((IStructuredSelection) currentSelection).toArray()) {
+				View view = (View) ((IAdaptable) selected).getAdapter(View.class);
+
+				if (view != null) {
+					return view;
+				}
+			}
+		}
+
+		return null;
+	}
+
+	protected List<Element> getSelectedElements() {
+		List<Element> elements = new ArrayList<Element>();
+		for (Object selected : ((IStructuredSelection) currentSelection).toArray()) {
+			Element element = getElementFromSelection(selected);
+			if (element != null) {
+				elements.add(element);
+			}
+		}
+
+		return elements;
+	}
+
+	protected Element getSelectedElement() {
+		for (Object selected : ((IStructuredSelection) currentSelection).toArray()) {
+			// only the first selection is used
+			return getElementFromSelection(selected);
+		}
+
+		return null;
+	}
+
+	private Element getElementFromSelection(Object selection) {
+		Element element = null;
+
+		EObject eObject = null;
+		if (selection instanceof IAdaptable) {
+			// Try to adapt to View first, since Notation OK
+			eObject = (EObject) ((IAdaptable) selection).getAdapter(View.class);
+
+			if (eObject == null) {
+				eObject = (EObject) ((IAdaptable) selection).getAdapter(EObject.class);
+			}
+		} else if (selection instanceof EObject) {
+			eObject = (EObject) selection;
+		}
+
+		if (View.class.isInstance(eObject)) {
+			eObject = ((View) eObject).getElement();
+		}
+
+		if (Element.class.isInstance(eObject)) {
+			element = (Element) eObject;
+		}
+
+		return element;
+	}
+
+	/**
+	 * Find next unused property name, using 'name' as the base.
+	 */
+	protected String getUniqueMemberName(Classifier owner, String name) {
+		int seqNo = 2;
+		String uniqueName = name;
+
+		while (null != owner.getOwnedMember(uniqueName, false, UMLPackage.eINSTANCE.getProperty())) {
+			uniqueName = name + String.valueOf(seqNo++);
+		}
+
+		return uniqueName;
+	}
+
+	/**
+	 * Find next unused type name, using 'name' as the base.
+	 */
+	protected String getUniqueTypeName(Package owner, String name) {
+		int seqNo = 1;
+		String uniqueName;
+
+		do {
+			uniqueName = name + String.valueOf(seqNo++);
+		} while (null != owner.getOwnedType(uniqueName));
+
+		return uniqueName;
+	}
+
+	/**
+	 * Find next unused type name, using 'name' as the base.
+	 */
+	protected String getUniqueTypeName(Class owner, String name) {
+		int seqNo = 1;
+		String uniqueName;
+
+		do {
+			uniqueName = name + String.valueOf(seqNo++);
+		} while (null != owner.getNestedClassifier(uniqueName));
+
+		return uniqueName;
+	}
+
+	/**
+	 * Find next unused package name, using 'name' as the base.
+	 */
+	protected String getUniquePackageName(Package owner, String name) {
+		int seqNo = 1;
+		String uniqueName;
+
+		do {
+			uniqueName = name + String.valueOf(seqNo++);
+		} while (null != owner.getNestedPackage(uniqueName));
+
+		return uniqueName;
+	}
+
+}
