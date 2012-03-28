3ac8de895358cb28487799bdefec3a4a5a6bc4e5
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-03-28 01:20:40 +0000

    regen after some Clean up of rule ids and cardinality

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.consol.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/AuthorizationActivityTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.consol.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/AuthorizationActivityTest.java
@@ -11,10 +11,14 @@ import java.util.Map;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.CDAFactory;
+import org.openhealthtools.mdht.uml.cda.EntryRelationship;
 import org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity;
 import org.openhealthtools.mdht.uml.cda.consol.ConsolFactory;
 import org.openhealthtools.mdht.uml.cda.consol.operations.AuthorizationActivityOperations;
 import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
+import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
+import org.openhealthtools.mdht.uml.hl7.vocab.x_ActRelationshipEntryRelationship;
 
 /**
  * <!-- begin-user-doc -->
@@ -25,6 +29,11 @@ import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
  * The following operations are supported:
  * <ul>
  *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity#validateAuthorizationActivityTemplateId(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Authorization Activity Template Id</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity#validateAuthorizationActivityClassCode(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Authorization Activity Class Code</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity#validateAuthorizationActivityId(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Authorization Activity Id</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity#validateAuthorizationActivityMoodCode(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Authorization Activity Mood Code</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity#validateAuthorizationActivityEntryRelationship(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Authorization Activity Entry Relationship</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AuthorizationActivity#validateAuthorizationActivityEntryRelationshipTypeCode(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Authorization Activity Entry Relationship Type Code</em>}</li>
  * </ul>
  * </p>
  *
@@ -65,6 +74,183 @@ public class AuthorizationActivityTest extends CDAValidationTest {
 		};
 
 		validateAuthorizationActivityTemplateIdTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAuthorizationActivityClassCode() {
+		OperationsTestCase<AuthorizationActivity> validateAuthorizationActivityClassCodeTestCase = new OperationsTestCase<AuthorizationActivity>(
+			"validateAuthorizationActivityClassCode",
+			operationsForOCL.getOCLValue("VALIDATE_AUTHORIZATION_ACTIVITY_CLASS_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AuthorizationActivity target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AuthorizationActivity target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AuthorizationActivityOperations.validateAuthorizationActivityClassCode(
+					(AuthorizationActivity) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAuthorizationActivityClassCodeTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated not
+	*/
+	@Test
+	public void testValidateAuthorizationActivityId() {
+		OperationsTestCase<AuthorizationActivity> validateAuthorizationActivityIdTestCase = new OperationsTestCase<AuthorizationActivity>(
+			"validateAuthorizationActivityId",
+			operationsForOCL.getOCLValue("VALIDATE_AUTHORIZATION_ACTIVITY_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AuthorizationActivity target) {
+				target.init();
+			}
+
+			@Override
+			protected void updateToPass(AuthorizationActivity target) {
+
+				target.getIds().add(DatatypesFactory.eINSTANCE.createII());
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AuthorizationActivityOperations.validateAuthorizationActivityId(
+					(AuthorizationActivity) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAuthorizationActivityIdTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAuthorizationActivityMoodCode() {
+		OperationsTestCase<AuthorizationActivity> validateAuthorizationActivityMoodCodeTestCase = new OperationsTestCase<AuthorizationActivity>(
+			"validateAuthorizationActivityMoodCode",
+			operationsForOCL.getOCLValue("VALIDATE_AUTHORIZATION_ACTIVITY_MOOD_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AuthorizationActivity target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AuthorizationActivity target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AuthorizationActivityOperations.validateAuthorizationActivityMoodCode(
+					(AuthorizationActivity) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAuthorizationActivityMoodCodeTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated not
+	*/
+	@Test
+	public void testValidateAuthorizationActivityEntryRelationship() {
+		OperationsTestCase<AuthorizationActivity> validateAuthorizationActivityEntryRelationshipTestCase = new OperationsTestCase<AuthorizationActivity>(
+			"validateAuthorizationActivityEntryRelationship",
+			operationsForOCL.getOCLValue("VALIDATE_AUTHORIZATION_ACTIVITY_ENTRY_RELATIONSHIP__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AuthorizationActivity target) {
+				target.init();
+			}
+
+			@Override
+			protected void updateToPass(AuthorizationActivity target) {
+
+				EntryRelationship er = CDAFactory.eINSTANCE.createEntryRelationship();
+
+				target.getEntryRelationships().add(er);
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AuthorizationActivityOperations.validateAuthorizationActivityEntryRelationship(
+					(AuthorizationActivity) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAuthorizationActivityEntryRelationshipTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated not
+	*/
+	@Test
+	public void testValidateAuthorizationActivityEntryRelationshipTypeCode() {
+		OperationsTestCase<AuthorizationActivity> validateAuthorizationActivityEntryRelationshipTypeCodeTestCase = new OperationsTestCase<AuthorizationActivity>(
+			"validateAuthorizationActivityEntryRelationshipTypeCode",
+			operationsForOCL.getOCLValue("VALIDATE_AUTHORIZATION_ACTIVITY_ENTRY_RELATIONSHIP_TYPE_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AuthorizationActivity target) {
+				target.init();
+				EntryRelationship er = CDAFactory.eINSTANCE.createEntryRelationship();
+
+				target.getEntryRelationships().add(er);
+			}
+
+			@Override
+			protected void updateToPass(AuthorizationActivity target) {
+				target.getEntryRelationships().clear();
+				EntryRelationship er = CDAFactory.eINSTANCE.createEntryRelationship();
+				er.setTypeCode(x_ActRelationshipEntryRelationship.SUBJ);
+				target.getEntryRelationships().add(er);
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AuthorizationActivityOperations.validateAuthorizationActivityEntryRelationshipTypeCode(
+					(AuthorizationActivity) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAuthorizationActivityEntryRelationshipTypeCodeTestCase.doValidationTest();
 	}
 
 	/**
