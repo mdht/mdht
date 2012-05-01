022edbbc4e27207d0d6833693fb9ebf9bcfac8be
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-05-01 20:31:47 +0000

    General repository clean-up for 1.1.0 release including model transformation, code generation and fix copyrights.

--- cda/tests/org.openhealthtools.mdht.uml.cda.consoltoc.tests/src/org/openhealthtools/mdht/uml/cda/consoltoc/tests/Main.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.consoltoc.tests/src/org/openhealthtools/mdht/uml/cda/consoltoc/tests/Main.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 ramakrishnanr.
+ * Copyright (c) 2012 Rama Ramakrishnan and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
- *     ramakrishnanr - initial API and implementation
- *     
+ *     Rama Ramakrishnan (Agilex Corporation) - initial API and implementation
  *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.consoltoc.tests;
 
