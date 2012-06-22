3900acae832fee8db624130ced92dc96f3bcef68
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-06-22 18:52:39 +0000

    changed to use org.dita.dost plugin for clean build

--- cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/builder/CDABuilder.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.ui/src/org/openhealthtools/mdht/uml/cda/ui/builder/CDABuilder.java
@@ -14,16 +14,10 @@ package org.openhealthtools.mdht.uml.cda.ui.builder;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.management.ManagementFactory;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -34,11 +28,10 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.dita.dost.util.DitaUtil;
 import org.eclipse.ant.core.AntCorePlugin;
 import org.eclipse.ant.core.IAntClasspathEntry;
 import org.eclipse.ant.internal.core.AntClasspathEntry;
-import org.eclipse.ant.internal.launching.launchConfigurations.AntHomeClasspathEntry;
-import org.eclipse.ant.internal.launching.launchConfigurations.ContributedClasspathEntriesEntry;
 import org.eclipse.ant.launching.IAntLaunchConstants;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -51,11 +44,9 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -79,16 +70,11 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
-import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
-import org.eclipse.jdt.launching.IVMInstall;
-import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.uml2.codegen.ecore.genmodel.GenModelFactory;
 import org.eclipse.uml2.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.ecore.importer.UMLImporter;
-import org.openhealthtools.mdht.uml.cda.ui.internal.Activator;
 import org.openhealthtools.mdht.uml.cda.ui.util.CDAUIUtil;
-import org.osgi.framework.Bundle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
@@ -437,152 +423,14 @@ public class CDABuilder extends IncrementalProjectBuilder {
 
 	}
 
-	public static void runPublishDita(IProject project, IProgressMonitor monitor) throws IOException, CoreException,
+	private static void runPublishDita(IProject project, IProgressMonitor monitor) throws IOException, CoreException,
 			URISyntaxException {
-
-		IFolder ditaFolder = project.getFolder("dita");
 
 		IFile ditaMapFile = CDAUIUtil.getProjectFile(project, CDAUIUtil.DITA_PATH, "book.ditamap");
 
-		if (ditaMapFile == null) {
-			return;
+		if (ditaMapFile != null) {
+			DitaUtil.publish(ditaMapFile, "pdf");
 		}
-		StringBuffer jvmArguments = new StringBuffer();
-		for (String arg : ManagementFactory.getRuntimeMXBean().getInputArguments()) {
-			if (arg.startsWith("-X")) {
-				jvmArguments.append(arg);
-				jvmArguments.append(" ");
-			}
-		}
-
-		jvmArguments.append("-Dfile.encoding=UTF-8 ");
-
-		String[] segments = ditaMapFile.getName().split("\\.");
-
-		String ditaMapFileRoot = segments[0];
-
-		// Assume pdf target ,developer-pdf
-		String antTargets = "standards-pdf,developer-pdf";
-
-		if (ditaMapFile.getName().endsWith("-web.ditamap")) {
-			antTargets = "standards-eclipsehelp";
-		}
-
-		Bundle bundle = Platform.getBundle("org.openhealthtools.mdht.uml.cda.dita");
-
-		Path path = new Path("META-INF/MANIFEST.MF");
-
-		URL fileURL = FileLocator.find(bundle, path, null);
-
-		Path ditadirPath = new Path("DITA-OT");
-
-		URL ditadirURL = FileLocator.find(bundle, ditadirPath, null);
-
-		ditadirURL = FileLocator.toFileURL(ditadirURL);
-
-		InputStream in = fileURL.openStream();
-
-		Manifest ditaPluginManifest = new Manifest(in);
-
-		Attributes attributes = ditaPluginManifest.getMainAttributes();
-
-		String ditaClassPath = attributes.getValue("Bundle-ClassPath");
-
-		List<String> classpath = new ArrayList<String>();
-
-		for (String classPath : ditaClassPath.split(",")) {
-			if (".".equals(classPath)) {
-				URL url = FileLocator.find(bundle, new Path(""), null);
-				url = FileLocator.toFileURL(url);
-				IRuntimeClasspathEntry pluginEntry = JavaRuntime.newRuntimeContainerClasspathEntry(
-					new Path(url.getPath()), IRuntimeClasspathEntry.USER_CLASSES);
-				classpath.add(pluginEntry.getMemento());
-			} else {
-				URL url = FileLocator.find(bundle, new Path(classPath), null);
-				url = FileLocator.toFileURL(url);
-				IRuntimeClasspathEntry toolsEntry = JavaRuntime.newArchiveRuntimeClasspathEntry(new Path(url.getPath()));
-				toolsEntry.setClasspathProperty(IRuntimeClasspathEntry.USER_CLASSES);
-				classpath.add(toolsEntry.getMemento());
-			}
-		}
-
-		ContributedClasspathEntriesEntry ccee = new ContributedClasspathEntriesEntry();
-
-		AntHomeClasspathEntry ace = new AntHomeClasspathEntry();
-
-		classpath.add(ace.getMemento());
-
-		classpath.add(ccee.getMemento());
-
-		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
-
-		ILaunchConfigurationType type = launchManager.getLaunchConfigurationType(IAntLaunchConstants.ID_ANT_LAUNCH_CONFIGURATION_TYPE);
-
-		URL ditaPublishBuildFileURL = fileURL = FileLocator.find(bundle, new Path("dita-publish.xml"), null);
-
-		ditaPublishBuildFileURL = FileLocator.toFileURL(ditaPublishBuildFileURL);
-
-		String name = launchManager.generateLaunchConfigurationName("Publish CDA");
-
-		ILaunchConfigurationWorkingCopy workingCopy = type.newInstance(null, name);
-
-		workingCopy.setAttribute("org.eclipse.ui.externaltools.ATTR_LOCATION", ditaPublishBuildFileURL.getPath());
-
-		IVMInstall jre = JavaRuntime.getDefaultVMInstall();
-
-		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_JRE_CONTAINER_PATH, jre.getName());
-
-		workingCopy.setAttribute(
-			IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS,
-			"-Djavax.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl");
-
-		Map<String, String> antProperties = new HashMap<String, String>();
-
-		antProperties.put("dita.dir", ditadirURL.toURI().getRawPath());
-		antProperties.put("ditaMapFile", ditaMapFile.getLocation().toOSString());
-		antProperties.put("outputLocation", project.getLocation().toOSString());
-		antProperties.put("pdf.output", Activator.getDefault().getStateLocation().append("pdf").toOSString());
-		antProperties.put("ditaMapFileRoot", ditaMapFileRoot);
-
-		String fileName = getFileNameFromMap(ditaMapFile.getLocation().toOSString());
-		if (fileName == null) {
-			fileName = project.getName();
-		}
-
-		antProperties.put("fileName", fileName);
-		antProperties.put("args.debug", "no");
-		antProperties.put("ditaFilePath", ditaFolder.getLocation().toOSString());
-		antProperties.put("tempFilePath", Activator.getDefault().getStateLocation().append("temp").toOSString());
-		antProperties.put("docProject", project.getLocation().toOSString());
-
-		String pdfFileLocation = ditaMapFile.getName();
-		pdfFileLocation = pdfFileLocation.replaceFirst(".ditamap", ".pdf");
-		antProperties.put("pdflocation", pdfFileLocation);
-
-		workingCopy.setAttribute("process_factory_id", "org.eclipse.ant.ui.remoteAntProcessFactory");
-		workingCopy.setAttribute(
-			IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME,
-			"org.eclipse.ant.internal.ui.antsupport.InternalAntRunner");
-
-		workingCopy.setAttribute(
-			org.eclipse.core.externaltools.internal.IExternalToolConstants.ATTR_WORKING_DIRECTORY,
-			project.getLocation().toOSString());
-		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, jvmArguments.toString());
-		workingCopy.setAttribute(org.eclipse.ant.launching.IAntLaunchConstants.ATTR_ANT_TARGETS, antTargets);
-		workingCopy.setAttribute(IAntLaunchConstants.ATTR_ANT_PROPERTIES, antProperties);
-		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH, classpath);
-		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_DEFAULT_CLASSPATH, false);
-		workingCopy.setAttribute(IDebugUIConstants.ATTR_CONSOLE_PROCESS, false);
-		workingCopy.setAttribute(
-			IJavaLaunchConfigurationConstants.ATTR_CLASSPATH_PROVIDER, "org.eclipse.ant.ui.AntClasspathProvider");
-		workingCopy.setAttribute(IDebugUIConstants.ATTR_LAUNCH_IN_BACKGROUND, false);
-
-		workingCopy.setAttribute(DebugPlugin.ATTR_CONSOLE_ENCODING, "UTF-8");
-
-		workingCopy.migrate();
-
-		workingCopy.launch(ILaunchManager.RUN_MODE, monitor, false, true);
-
 	}
 
 	public static void runTransformation(IProject project, IProgressMonitor monitor) {
