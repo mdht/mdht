de436c4944738d8552b2c909619490130ec936db
dcarlson <dcarlson@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-11-06 15:10:23 +0000

    artf3423 : Model validation does not include implicit property redefinitions

---
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.validation/src/org/openhealthtools/mdht/uml/cda/validation/internal/classifiers/PropertyRedefinitionConstraints.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2012 David A Carlson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *     
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.validation.internal.classifiers;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.Property;
+import org.openhealthtools.mdht.uml.validation.provider.AbstractMultiConstraint;
+
+/**
+ * Constraints on implicit property redefinition.
+ */
+public class PropertyRedefinitionConstraints extends AbstractMultiConstraint {
+
+	public PropertyRedefinitionConstraints() {
+		super();
+	}
+
+	public IStatus validateImplicitPropertyRedefinitionIsValid(IValidationContext context) {
+		IStatus result = null;
+
+		Property property = (Property) context.getTarget();
+		// Property is owned by a Class
+		// Does not have explicit redefinition (no duplication of UML2 validation)
+		if (property.getClass_() != null && property.getRedefinedElements().isEmpty()) {
+			boolean ok = true;
+			Property parentProperty = null;
+
+			for (Classifier parent : property.getClass_().allParents()) {
+				parentProperty = parent.getAttribute(property.getName(), null);
+				if (parentProperty != null && !parentProperty.isConsistentWith(property)) {
+					ok = false;
+					break;
+				}
+			}
+
+			if (!ok) {
+				// Property "{0}" is not a valid implicit redefinition of Property "{1}".
+				result = context.createFailureStatus(property.getQualifiedName(), parentProperty.getQualifiedName());
+			}
+		}
+
+		return (result == null)
+				? context.createSuccessStatus()
+				: result;
+	}
+
+}
