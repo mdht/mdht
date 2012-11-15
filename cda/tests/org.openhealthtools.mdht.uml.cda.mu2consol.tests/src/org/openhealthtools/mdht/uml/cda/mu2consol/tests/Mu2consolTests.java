7f8e62d604fa2097843c693251ef32d8f345000b
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-11-15 17:58:37 +0000

    Rerun generate

--- cda/tests/org.openhealthtools.mdht.uml.cda.mu2consol.tests/src/org/openhealthtools/mdht/uml/cda/mu2consol/tests/Mu2consolTests.java
+++ cda/tests/org.openhealthtools.mdht.uml.cda.mu2consol.tests/src/org/openhealthtools/mdht/uml/cda/mu2consol/tests/Mu2consolTests.java
@@ -28,9 +28,9 @@ import org.junit.runners.Suite;
 public class Mu2consolTests {
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
+	* <!-- begin-user-doc --> <!-- end-user-doc -->
+	* @generated
+	*/
 	public static junit.framework.Test suite() {
 		return new JUnit4TestAdapter(Mu2consolTests.class);
 	}
