905a14d16a492e38b7be59fc0d9d5aa78a41e9f0
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2012-12-14 17:53:59 +0000

    Fix copyrights.

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.mu2consol.tests/src/org/openhealthtools/mdht/uml/cda/mu2consol/tests/Mu2consolTests.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.mu2consol.tests/src/org/openhealthtools/mdht/uml/cda/mu2consol/tests/Mu2consolTests.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/*******************************************************************************
+ * Copyright (c) 2012 Rama Ramakrishnan and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *    Rama Ramakrishnan (Agilex Corporation) - initial API and implementation
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.mu2consol.tests;
 
 import junit.framework.JUnit4TestAdapter;
