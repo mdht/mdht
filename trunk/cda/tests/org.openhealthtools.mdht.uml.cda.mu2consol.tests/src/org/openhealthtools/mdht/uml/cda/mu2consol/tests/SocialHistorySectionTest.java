79a54866aa0534c053a3d18a3beaa7975fb20b41
jtimm <jtimm@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2013-08-06 16:17:04 +0000

    Applied and tested all patches for [artf3560] and [artf3590].

--- trunk/cda/tests/org.openhealthtools.mdht.uml.cda.mu2consol.tests/src/org/openhealthtools/mdht/uml/cda/mu2consol/tests/SocialHistorySectionTest.java
+++ trunk/cda/tests/org.openhealthtools.mdht.uml.cda.mu2consol.tests/src/org/openhealthtools/mdht/uml/cda/mu2consol/tests/SocialHistorySectionTest.java
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Rama Ramakrishnan (Agilex Corporation) - initial API and implementation
+ *    Dan Brown (Audacious Inquiry) - additional testing code
  *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.mu2consol.tests;
 
@@ -15,11 +16,14 @@ import java.util.Map;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.Observation;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolFactory;
 import org.openhealthtools.mdht.uml.cda.mu2consol.Mu2consolFactory;
 import org.openhealthtools.mdht.uml.cda.mu2consol.SmokingStatusObservation;
 import org.openhealthtools.mdht.uml.cda.mu2consol.SocialHistorySection;
 import org.openhealthtools.mdht.uml.cda.mu2consol.operations.SocialHistorySectionOperations;
 import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
+import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
 
 /**
  * <!-- begin-user-doc --> A static utility class that provides operations
@@ -42,7 +46,7 @@ public class SocialHistorySectionTest extends CDAValidationTest {
 
 	/**
 	*
-	* @generated not
+	* @generated NOT
 	*/
 	@Test
 	public void testValidateMU2CONSOLSocialHistorySectionSmokingStatusObservation() {
@@ -52,22 +56,68 @@ public class SocialHistorySectionTest extends CDAValidationTest {
 			objectFactory) {
 
 			@Override
+			public void addFailTests() {
+
+				addFailTest(new FailTest() {
+
+					// empty fail
+					@Override
+					public void updateToFail(SocialHistorySection target) {
+					}
+
+				});
+
+				addFailTest(new FailTest() {
+
+					// TEST 4
+					// Contains 0 entry with 0 Smoking status observation templates
+					@Override
+					public void updateToFail(SocialHistorySection target) {
+						target.init();
+					}
+
+				});
+
+			}
+
+			@Override
+			public void addPassTests() {
+
+				addPassTest(new PassTest() {
+
+					@Override
+					public void updateToPass(SocialHistorySection target) {
+						// TEST 1
+						// Contains 3 entry elements, where the 1st of which, contains a Smoking status observation template.
+						SmokingStatusObservation smokStatObs = Mu2consolFactory.eINSTANCE.createSmokingStatusObservation().init();
+						target.addObservation(smokStatObs);
+						Observation socialHistObs = ConsolFactory.eINSTANCE.createSocialHistoryObservation().init();
+						target.addObservation(socialHistObs);
+						Observation socialHistObs2 = ConsolFactory.eINSTANCE.createSocialHistoryObservation().init();
+						target.addObservation(socialHistObs2);
+					}
+
+				});
+
+				addPassTest(new PassTest() {
+
+					@Override
+					public void updateToPass(SocialHistorySection target) {
+						// TEST 2
+						// Contains 1 entry with a Smoking status observation template.
+						SmokingStatusObservation sObs = Mu2consolFactory.eINSTANCE.createSmokingStatusObservation().init();
+						target.addObservation(sObs);
+					}
+
+				});
+
+			}
+
+			@Override
 			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
 
 				return SocialHistorySectionOperations.validateMU2CONSOLSocialHistorySectionSmokingStatusObservation(
 					(SocialHistorySection) objectToTest, diagnostician, map);
-			}
-
-			@Override
-			protected void updateToFail(SocialHistorySection target) {
-
-			}
-
-			@Override
-			protected void updateToPass(SocialHistorySection target) {
-				SmokingStatusObservation sObs = Mu2consolFactory.eINSTANCE.createSmokingStatusObservation().init();
-				target.addObservation(sObs);
-
 			}
 
 		};
@@ -89,7 +139,7 @@ public class SocialHistorySectionTest extends CDAValidationTest {
 
 	/**
 	 * 
-	 * @generated
+	 * @generated NOT
 	 */
 	@Test
 	public void testValidateSocialHistorySectionTemplateId() {
@@ -99,8 +149,29 @@ public class SocialHistorySectionTest extends CDAValidationTest {
 			objectFactory) {
 
 			@Override
-			protected void updateToFail(SocialHistorySection target) {
+			public void addFailTests() {
 
+				addFailTest(new FailTest() {
+
+					// empty fail
+					@Override
+					public void updateToFail(SocialHistorySection target) {
+					}
+
+				});
+
+				addFailTest(new FailTest() {
+
+					// Contains an invalid template ID
+					@Override
+					public void updateToFail(SocialHistorySection target) {
+						target.init();
+						target.getTemplateIds().clear();
+						target.getTemplateIds().add(
+							DatatypesFactory.eINSTANCE.createII("2.16.840.1.113883.XX.XX.XX.X.XX"));
+					}
+
+				});
 			}
 
 			@Override
