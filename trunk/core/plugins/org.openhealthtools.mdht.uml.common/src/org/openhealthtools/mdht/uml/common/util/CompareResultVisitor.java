304c117324b6b282a44aeef095935f3a30b5d4f0
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2014-04-25 14:15:42 +0000

    Added initial version of model compare

---
+++ trunk/core/plugins/org.openhealthtools.mdht.uml.common/src/org/openhealthtools/mdht/uml/common/util/CompareResultVisitor.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sean Muir, JKM Software LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Sean Muir - initial API and implementation
+ *     
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.common.util;
+
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.NamedElement;
+
+public interface CompareResultVisitor {
+
+	public abstract void startElement(NamedElement element);
+
+	public abstract void addedElement(NamedElement owner, Element element);
+
+	public abstract void deletedElement(NamedElement owner, Element element);
+
+	public abstract void changedElement(NamedElement owner, Element originalElement, Element updatedElement);
+
+	public abstract void endElement(NamedElement element);
+
+}
