03e2c25fedfb2dd2428fbac14984eaee4f924a39
seanmuir <seanmuir@d2ad734f-166c-0410-b8fe-fe8d3035ea49>
2014-07-28 16:49:15 +0000

    Reload and Regen to kepler

--- core/plugins/org.openhealthtools.mdht.uml.hl7.rim/src/org/openhealthtools/mdht/uml/hl7/rim/Act.java
+++ core/plugins/org.openhealthtools.mdht.uml.hl7.rim/src/org/openhealthtools/mdht/uml/hl7/rim/Act.java
@@ -75,7 +75,7 @@ public interface Act extends InfrastructureRoot {
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @model kind="operation" required="true" ordered="false"
+	 * @model kind="operation" dataType="org.eclipse.uml2.types.Boolean" required="true" ordered="false"
 	 *        annotation="http://www.eclipse.org/uml2/1.1.0/GenModel body='self.isDefined(\'classCode\')'"
 	 * @generated
 	 */
@@ -84,7 +84,7 @@ public interface Act extends InfrastructureRoot {
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @model kind="operation" required="true" ordered="false"
+	 * @model kind="operation" dataType="org.eclipse.uml2.types.Boolean" required="true" ordered="false"
 	 *        annotation="http://www.eclipse.org/uml2/1.1.0/GenModel body='self.isDefined(\'moodCode\')'"
 	 * @generated
 	 */
