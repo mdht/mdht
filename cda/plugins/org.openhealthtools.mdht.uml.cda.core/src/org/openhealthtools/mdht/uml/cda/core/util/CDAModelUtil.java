3bf49b6613a4acfeaef16288854342a02fcce944
Sean Muir <sean.w.muir@gmail.com>
2015-04-25 19:58:48 -0400

    Added inline utilities

--- cda/plugins/org.openhealthtools.mdht.uml.cda.core/src/org/openhealthtools/mdht/uml/cda/core/util/CDAModelUtil.java
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.core/src/org/openhealthtools/mdht/uml/cda/core/util/CDAModelUtil.java
@@ -61,6 +61,7 @@ import org.eclipse.uml2.uml.util.UMLSwitch;
 import org.openhealthtools.mdht.uml.cda.core.profile.ActRelationship;
 import org.openhealthtools.mdht.uml.cda.core.profile.EntryRelationship;
 import org.openhealthtools.mdht.uml.cda.core.profile.EntryRelationshipKind;
+import org.openhealthtools.mdht.uml.cda.core.profile.Inline;
 import org.openhealthtools.mdht.uml.cda.core.profile.SeverityKind;
 import org.openhealthtools.mdht.uml.cda.core.profile.Validation;
 import org.openhealthtools.mdht.uml.common.util.NamedElementUtil;
@@ -2414,13 +2415,13 @@ public class CDAModelUtil {
 	}
 
 	public static boolean isInlineClass(Class _class) {
+		Inline inline = CDAProfileUtil.getInline(_class);
 
-		if (_class.getOwner() instanceof Class) {
+		if (inline != null) {
 			return true;
 		}
 
-		Stereotype stereotype = CDAProfileUtil.getAppliedCDAStereotype(_class, ICDAProfileConstants.INLINE);
-		if (stereotype != null) {
+		if (_class.getOwner() instanceof Class) {
 			return true;
 		}
 
@@ -2432,6 +2433,43 @@ public class CDAModelUtil {
 
 		return false;
 
+	}
+
+	public static String getInlineFilter(Class inlineClass) {
+
+		Inline inline = CDAProfileUtil.getInline(inlineClass);
+
+		if (inline != null) {
+			return inline.getFilter() != null
+					? inline.getFilter()
+					: "";
+
+		}
+
+		String filter = "";
+		for (Comment comment : inlineClass.getOwnedComments()) {
+			if (comment.getBody().startsWith("INLINE&")) {
+				String[] temp = comment.getBody().split("&");
+				if (temp.length == 2) {
+					filter = String.format("->select(%s)", temp[1]);
+				}
+				break;
+			}
+		}
+
+		if ("".equals(filter)) {
+			// search hierarchy
+			for (Classifier next : inlineClass.getGenerals()) {
+				if (next instanceof Class) {
+					filter = getInlineFilter((Class) next);
+					if (!"".equals(filter)) {
+						break;
+					}
+				}
+			}
+		}
+
+		return filter;
 	}
 
 	public static boolean isPublishSeperately(Class _class) {
