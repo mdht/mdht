2276166825316932ca70b2640bb50c015af160ef
SarpKaya <sarp.kaya@nehta.gov.au>
2015-07-21 15:58:48 +1000

    CDATemplate Conformance message builder added

---
+++ cda/plugins/org.openhealthtools.mdht.uml.cda.core/src/org/openhealthtools/mdht/uml/cda/core/util/CDATemplateComputeBuilder.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Sean Muir and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sarp Kaya (NEHTA)  - initial implementation
+ *
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.core.util;
+
+public abstract class CDATemplateComputeBuilder {
+
+	private String templateVersion;
+
+	private boolean markup = false;
+
+	private boolean cardinalityAfterElement = false;
+
+	private String templateId;
+
+	private String ruleIds;
+
+	public CDATemplateComputeBuilder setTemplateVersion(String templateVersion) {
+		this.templateVersion = templateVersion;
+		return this;
+	}
+
+	public CDATemplateComputeBuilder setTemplateId(String templateId) {
+		this.templateId = templateId;
+		return this;
+	}
+
+	public CDATemplateComputeBuilder setRuleIds(String ruleIds) {
+		this.ruleIds = ruleIds;
+		return this;
+	}
+
+	public CDATemplateComputeBuilder setRequireMarkup(Boolean markup) {
+		this.markup = markup;
+		return this;
+	}
+
+	public CDATemplateComputeBuilder setCardinalityAfterElement(Boolean cardinalityAfterElement) {
+		this.cardinalityAfterElement = cardinalityAfterElement;
+		return this;
+	}
+
+	public CDATemplateComputeBuilder compute() {
+		return this;
+	}
+
+	public abstract String addMultiplicity();
+
+}
