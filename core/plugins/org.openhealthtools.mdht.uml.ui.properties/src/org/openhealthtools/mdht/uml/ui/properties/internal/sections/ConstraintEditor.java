66cc10bb01c55f53cc7f4d5c1c50160aad3c926e
Sean Muir <sean.w.muir@gmail.com>
2015-09-01 17:36:02 -0400

    interface for constraint editor

---
+++ core/plugins/org.openhealthtools.mdht.uml.ui.properties/src/org/openhealthtools/mdht/uml/ui/properties/internal/sections/ConstraintEditor.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2015 seanmuir.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     seanmuir - initial API and implementation
+ *
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.ui.properties.internal.sections;
+
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * @author seanmuir
+ *
+ */
+public interface ConstraintEditor {
+
+	void setText(Text text);
+
+}
